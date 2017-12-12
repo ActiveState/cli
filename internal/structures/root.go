3eecbe40a2376a6121b3f8b1881335e94bd5ca2a
Nathan Rijksen <n.rijksen@gmail.com>
2017-12-12 12:28:28 -0500

    Specced out the project structure

--- internal/structures/root.go
+++ internal/structures/root.go
@@ -1,12 +1,14 @@
 package structures
 
 type Project struct {
-	Name      string     `json:"name"`
-	Languages []Language `json:"languages"`
+	Name     string    `json:"name"`
+	Owner    string    `json:"owner"`
+	Runtimes []Runtime `json:"runtimes"`
 }
 
-type Language struct {
+type Runtime struct {
 	Name     string    `json:"name"`
+	Version  int       `json:"version"`
 	Packages []Package `json:"packages"`
 }
 
