3cdab228e8315baf088550604ff6ba53704b77d7
mitchell <mitchell-as@users.noreply.github.com>
2018-01-30 17:51:52 -0500

    activate: Clone the given URL into an optionally given directory.

--- state/activate/activate_test.go
+++ state/activate/activate_test.go
@@ -9,6 +9,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Little hack to ensure we are ran before the init function in activate.go
+var _ = func() (_ struct{}) {
+	config.Init()
+	locale.Init()
+	return
+}()
+
 func TestMain(m *testing.M) {
 	config.Init()
 	locale.Init()
@@ -16,13 +23,62 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
+var cloneURL = "https://github.com/golang/playground"
+
 func TestExecute(t *testing.T) {
 	assert := assert.New(t)
 
+	path, cd = "", false // clear command line options
 	Cc := Command.GetCobraCmd()
 	Cc.SetArgs([]string{"activate"})
-
 	Command.Execute()
-
 	assert.Equal(true, true, "Execute didn't panic")
+}
+
+func TestExecuteURL(t *testing.T) {
+	assert := assert.New(t)
+
+	path, cd = "", false // clear command line options
+	origCWD, _ := os.Getwd()
+	Command.GetCobraCmd().SetArgs([]string{cloneURL})
+	Command.Execute()
+	assert.DirExists("playground", "git cloned into directory 'playground'")
+	currentCWD, _ := os.Getwd()
+	assert.Equal(origCWD, currentCWD, "The current directory did not change")
+	os.RemoveAll("playground")
+}
+
+func TestExecuteURLPath(t *testing.T) {
+	assert := assert.New(t)
+
+	path, cd = "", false // clear command line options
+	origCWD, _ := os.Getwd()
+	Command.GetCobraCmd().SetArgs([]string{cloneURL, "--path", "play"})
+	Command.Execute()
+	assert.DirExists("play", "git cloned into directory 'play'")
+	currentCWD, _ := os.Getwd()
+	assert.Equal(origCWD, currentCWD, "The current directory did not change")
+	os.RemoveAll("play")
+
+	path, cd = "", false // clear command line options
+	Command.GetCobraCmd().SetArgs([]string{"--path", "play", cloneURL})
+	Command.Execute()
+	assert.DirExists("play", "git cloned into directory 'play'")
+	currentCWD, _ = os.Getwd()
+	assert.Equal(origCWD, currentCWD, "The current directory did not change")
+	os.RemoveAll("play")
+}
+
+func TestExecuteCD(t *testing.T) {
+	assert := assert.New(t)
+
+	path, cd = "", false // clear command line options
+	origCWD, _ := os.Getwd()
+	Command.GetCobraCmd().SetArgs([]string{cloneURL, "--cd"})
+	Command.Execute()
+	currentCWD, _ := os.Getwd()
+	assert.NotEqual(origCWD, currentCWD, "The current directory changed")
+	os.Chdir("..")
+	assert.DirExists("playground", "git cloned into directory 'playground'")
+	os.RemoveAll("playground")
 }
