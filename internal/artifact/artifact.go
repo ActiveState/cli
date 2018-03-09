ea424821fb5e6bba445698f74047a4ae235a6e20
Nathan Rijksen <n.rijksen@gmail.com>
2018-03-09 14:40:12 -0500

    Documented artifact struct

--- internal/artifact/artifact.go
+++ internal/artifact/artifact.go
@@ -1,5 +1,6 @@
 package artifact
 
+// Artifact reflects the spec described here https://docs.google.com/document/d/1HprLsYXiKBeKfUvRrXpgyD_aodMnf6ZyuwgqpZu5ii4
 type Artifact struct {
 	Name     string
 	Type     string
