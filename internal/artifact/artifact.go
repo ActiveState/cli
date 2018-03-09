d5a74162b15ff45b76ef75dd19908332de7c30c6
Nathan Rijksen <n.rijksen@gmail.com>
2018-03-09 16:10:06 -0500

    Expand on artifact comment

--- internal/artifact/artifact.go
+++ internal/artifact/artifact.go
@@ -1,6 +1,8 @@
 package artifact
 
-// Artifact reflects the spec described here https://docs.google.com/document/d/1HprLsYXiKBeKfUvRrXpgyD_aodMnf6ZyuwgqpZu5ii4
+// Artifact is used to describe the contents of an artifact, this information can then be used to set up distributions
+// An artifact can be a package, a language, a clib, etc. It is a component that makes up a distribution.
+// It reflects the spec described here https://docs.google.com/document/d/1HprLsYXiKBeKfUvRrXpgyD_aodMnf6ZyuwgqpZu5ii4
 type Artifact struct {
 	Name     string
 	Type     string
