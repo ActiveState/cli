33e181bbd5d3bd3903751ebc69a2439cfa5fc065
mitchell <mitchell-as@users.noreply.github.com>
2018-05-08 12:17:37 -0400

    Added state organizations command.

---
+++ state/organizations/organizations.go
@@ -0,0 +1,44 @@
+package organizations
+
+import (
+	"github.com/ActiveState/cli/internal/api"
+	clientOrgs "github.com/ActiveState/cli/internal/api/client/organizations"
+	"github.com/ActiveState/cli/internal/failures"
+	"github.com/ActiveState/cli/internal/locale"
+	"github.com/ActiveState/cli/internal/logging"
+	"github.com/ActiveState/cli/internal/print"
+	"github.com/ActiveState/cli/pkg/cmdlets/commands"
+	"github.com/bndr/gotabulate"
+	"github.com/spf13/cobra"
+)
+
+// Command is the organization command's definition.
+var Command = &commands.Command{
+	Name:        "organizations",
+	Description: "organizations_description",
+	Run:         Execute,
+}
+
+// Execute the organizations command.
+func Execute(cmd *cobra.Command, args []string) {
+	params := clientOrgs.NewListOrganizationsParams()
+	memberOnly := true
+	params.SetMemberOnly(&memberOnly)
+	orgs, err := api.Client.Organizations.ListOrganizations(params, nil)
+	if err != nil {
+		logging.Errorf("Unable to list member organizations: %s", err)
+		failures.Handle(err, locale.T("organizations_err"))
+		return
+	}
+
+	rows := [][]interface{}{}
+	for _, org := range orgs.Payload {
+		rows = append(rows, []interface{}{org.Name})
+	}
+
+	t := gotabulate.Create(rows)
+	t.SetHeaders([]string{locale.T("organization_name")})
+	t.SetAlign("left")
+
+	print.Line(t.Render("simple"))
+}
---
+++ state/organizations/organizations_test.go
@@ -0,0 +1,33 @@
+package organizations
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/ActiveState/cli/internal/api"
+	"github.com/ActiveState/cli/internal/environment"
+	"github.com/ActiveState/cli/internal/testhelpers/httpmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func setup(t *testing.T) {
+	root, err := environment.GetRootPath()
+	assert.NoError(t, err, "Should detect root path")
+	os.Chdir(filepath.Join(root, "test"))
+
+	Cc := Command.GetCobraCmd()
+	Cc.SetArgs([]string{})
+}
+
+func TestOrganizations(t *testing.T) {
+	setup(t)
+
+	httpmock.Activate(api.Prefix)
+	defer httpmock.DeActivate()
+
+	httpmock.Register("GET", "/organizations")
+
+	err := Command.Execute()
+	assert.NoError(t, err, "Executed without error")
+}
