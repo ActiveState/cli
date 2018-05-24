7f50cbffb3a6387328a6ba0a3cd2e36022e3433c
mitchell <mitchell-as@users.noreply.github.com>
2018-05-24 14:16:02 -0400

    More test coverage for state new.

--- state/new/new_test.go
+++ state/new/new_test.go
@@ -49,3 +49,31 @@ func TestNew(t *testing.T) {
 
 	os.RemoveAll(tmpdir)
 }
+
+func TestNewBadPath(t *testing.T) {
+	Flags.Path, Flags.Owner, Flags.Version, Args.Name = "", "", "", "" // reset
+	invalidPath := ":invalid:path:"
+	Cc := Command.GetCobraCmd()
+	Cc.SetArgs([]string{"test-name", "-p", invalidPath, "-o", "test-owner", "-v", "1.0"})
+	err := Command.Execute()
+	assert.NoError(t, err, "Executed without error")
+	_, err = os.Stat(filepath.Join(invalidPath, constants.ConfigFileName))
+	assert.Error(t, err, "Project was not created")
+}
+
+func TestNewBadVersion(t *testing.T) {
+	Flags.Path, Flags.Owner, Flags.Version, Args.Name = "", "", "", "" // reset
+	tmpdir, err := ioutil.TempDir("", "cli-new-test")
+	assert.NoError(t, err, "Created temp directory")
+	cwd, err := os.Getwd()
+	assert.NoError(t, err, "Fetched cwd")
+	os.Chdir(tmpdir)
+	Cc := Command.GetCobraCmd()
+	Cc.SetArgs([]string{"test-name", "-o", "test-owner", "-v", "badVersion"})
+	err = Command.Execute()
+	assert.NoError(t, err, "Executed without error")
+	_, err = os.Stat(filepath.Join(tmpdir, constants.ConfigFileName))
+	assert.Error(t, err, "Project was not created")
+	os.Chdir(cwd) // restore
+	os.RemoveAll(tmpdir)
+}
