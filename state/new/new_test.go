3af9a84f0e4b7336747af5da2bb75e646baa8c43
mitchell <mitchell-as@users.noreply.github.com>
2018-05-24 17:50:07 -0400

    Break up one of the unit tests for state new.

--- state/new/new_test.go
+++ state/new/new_test.go
@@ -10,13 +10,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNew(t *testing.T) {
+func TestNewInEmptyDir(t *testing.T) {
 	tmpdir, err := ioutil.TempDir("", "cli-new-test")
 	assert.NoError(t, err, "Created temp directory")
 	cwd, err := os.Getwd()
 	assert.NoError(t, err, "Fetched cwd")
 
-	// Test creating project in empty directory.
 	err = os.Chdir(tmpdir)
 	assert.NoError(t, err, "Switched to tempdir")
 	Cc := Command.GetCobraCmd()
@@ -29,9 +28,18 @@ func TestNew(t *testing.T) {
 	assert.NoError(t, err, "Renamed config file so later tests cannot reference it")
 	os.Chdir(cwd) // restore
 
-	// Test creating project in non-empty directory.
-	os.Chdir(tmpdir)
+	os.RemoveAll(tmpdir)
+}
+
+func TestNewInNonEmptyDir(t *testing.T) {
 	Flags.Path, Flags.Owner, Flags.Version, Args.Name = "", "", "", "" // reset
+	tmpdir, _ := ioutil.TempDir("", "cli-new-test")
+	cwd, _ := os.Getwd()
+	err := ioutil.WriteFile(filepath.Join(tmpdir, "foo.txt"), []byte(""), 0666)
+	assert.NoError(t, err, "Wrote dummy file")
+
+	os.Chdir(tmpdir)
+	Cc := Command.GetCobraCmd()
 	Cc.SetArgs([]string{"test-name", "-o", "test-owner", "-v", "1.0"})
 	err = Command.Execute()
 	assert.NoError(t, err, "Executed without error")
@@ -39,18 +47,29 @@ func TestNew(t *testing.T) {
 	assert.NoError(t, err, "Project was created in sub-directory")
 	os.Chdir(cwd) // restore
 
-	// Test creating project in existing directory.
+	os.RemoveAll(tmpdir)
+}
+
+func TestNewWithPathToExistingDir(t *testing.T) {
 	Flags.Path, Flags.Owner, Flags.Version, Args.Name = "", "", "", "" // reset
+	tmpdir, _ := ioutil.TempDir("", "cli-new-test")
+	cwd, _ := os.Getwd()
+	err := ioutil.WriteFile(filepath.Join(tmpdir, "foo.txt"), []byte(""), 0666)
+	assert.NoError(t, err, "Wrote dummy file")
+
+	os.Chdir(tmpdir)
+	Cc := Command.GetCobraCmd()
 	Cc.SetArgs([]string{"test-name", "-p", tmpdir, "-o", "test-owner", "-v", "1.0"})
 	err = Command.Execute()
 	assert.NoError(t, err, "Executed without error")
 	_, err = os.Stat(filepath.Join(tmpdir, constants.ConfigFileName))
 	assert.Error(t, err, "Project was not created in existing directory")
+	os.Chdir(cwd) // restore
 
 	os.RemoveAll(tmpdir)
 }
 
-func TestNewBadPath(t *testing.T) {
+func TestNewWithBadPath(t *testing.T) {
 	Flags.Path, Flags.Owner, Flags.Version, Args.Name = "", "", "", "" // reset
 	invalidPath := ":invalid:path:"
 	Cc := Command.GetCobraCmd()
@@ -61,19 +80,19 @@ func TestNewBadPath(t *testing.T) {
 	assert.Error(t, err, "Project was not created")
 }
 
-func TestNewBadVersion(t *testing.T) {
+func TestNewWithBadVersion(t *testing.T) {
 	Flags.Path, Flags.Owner, Flags.Version, Args.Name = "", "", "", "" // reset
-	tmpdir, err := ioutil.TempDir("", "cli-new-test")
-	assert.NoError(t, err, "Created temp directory")
-	cwd, err := os.Getwd()
-	assert.NoError(t, err, "Fetched cwd")
+	tmpdir, _ := ioutil.TempDir("", "cli-new-test")
+	cwd, _ := os.Getwd()
+
 	os.Chdir(tmpdir)
 	Cc := Command.GetCobraCmd()
 	Cc.SetArgs([]string{"test-name", "-o", "test-owner", "-v", "badVersion"})
-	err = Command.Execute()
+	err := Command.Execute()
 	assert.NoError(t, err, "Executed without error")
 	_, err = os.Stat(filepath.Join(tmpdir, constants.ConfigFileName))
 	assert.Error(t, err, "Project was not created")
 	os.Chdir(cwd) // restore
+
 	os.RemoveAll(tmpdir)
 }
