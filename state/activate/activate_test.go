a67a7f2fb18a07b6dd8186596ba678d5803ef4f3
Nathan Rijksen <n.rijksen@gmail.com>
2018-07-17 10:45:30 -0400

    Reset failures after executing command

--- state/activate/activate_test.go
+++ state/activate/activate_test.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ActiveState/cli/internal/config"
 	"github.com/ActiveState/cli/internal/environment"
 
 	"github.com/ActiveState/cli/internal/failures"
@@ -21,29 +20,7 @@ func init() {
 	}
 }
 
-func setup(t *testing.T) {
-	cwd, err := environment.GetRootPath()
-	assert.NoError(t, err, "Should fetch cwd")
-	testDir := filepath.Join(cwd, "state", "activate", "testdata")
-	os.Mkdir(testDir, os.ModePerm) // For now there is nothing in the testdata dir so it's not cloned.  Don't care if it errors out.
-	err = os.Chdir(testDir)
-	assert.NoError(t, err, "Should change dir")
-}
-
-func demolish(t *testing.T) {
-	root, err := environment.GetRootPath()
-	assert.NoError(t, err, "Should fetch cwd")
-	os.Chdir(root)
-	os.RemoveAll(filepath.Join(root, "state", "activate", "testdata"))
-
-	datadir := config.GetDataDir()
-	os.RemoveAll(filepath.Join(datadir, "virtual"))
-	os.RemoveAll(filepath.Join(datadir, "packages"))
-	os.RemoveAll(filepath.Join(datadir, "languages"))
-	os.RemoveAll(filepath.Join(datadir, "artifacts"))
-}
 func TestExecute(t *testing.T) {
-	setup(t)
 	assert := assert.New(t)
 
 	cwd, _ := os.Getwd() // store
@@ -57,7 +34,6 @@ func TestExecute(t *testing.T) {
 
 	err = os.Chdir(cwd)
 	assert.Nil(err, "Changed back to original cwd")
-	demolish(t)
 }
 
 func TestExecuteGitClone(t *testing.T) {
@@ -100,6 +76,7 @@ func TestExecuteGitClone(t *testing.T) {
 	Command.Execute()
 
 	assert.NoError(t, failures.Handled(), "No failure occurred")
+	failures.ResetHandled()
 
 	newCwd, _ := os.Getwd()
 	assert.Equal(t, "repo2", filepath.Base(newCwd), "The cloned repository exists and was changed into")
