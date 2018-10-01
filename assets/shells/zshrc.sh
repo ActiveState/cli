98e1e756d8bced598a089dcb008dd5f6582ca9e6
Carey Hoffman <cgchoffman@gmail.com>
2018-10-01 16:51:53 -0700

    Fix state activate in zedshell

--- assets/shells/zshrc.sh
+++ assets/shells/zshrc.sh
@@ -1,10 +1,9 @@
-source ~/.zshrc
-export PROMPT_COMMAND="echo -e \"\033[1mActive state: {{.Project.Owner}}/{{.Project.Name}}\\033[0m\"
-"
+if [ -f ~/.zshrc ]; then source ~/.zshrc; fi
+export PROMPT_COMMAND="echo -e \"\033[1mActive state: {{.Project.Owner}}/{{.Project.Name}}\\033[0m\""
 precmd() { eval "$PROMPT_COMMAND" }
 {{range $K, $V := .Env}}
-export {{$K}}={{$V}}
+export {{$K}}="{{$V}}"
 {{end}}
-cd {{.WD}}
+cd "{{.WD}}"
 
 {{.UserScripts}}
