630947362ad276ff450149c4dbdbeb901c0480f9
Carey Hoffman <cgchoffman@gmail.com>
2018-10-01 17:02:49 -0700

    Revert prompt command edit

--- assets/shells/zshrc.sh
+++ assets/shells/zshrc.sh
@@ -1,5 +1,6 @@
 if [ -f ~/.zshrc ]; then source ~/.zshrc; fi
-export PROMPT_COMMAND="echo -e \"\033[1mActive state: {{.Project.Owner}}/{{.Project.Name}}\\033[0m\""
+export PROMPT_COMMAND="echo -e \"\033[1mActive state: {{.Project.Owner}}/{{.Project.Name}}\\033[0m\"
+"
 precmd() { eval "$PROMPT_COMMAND" }
 {{range $K, $V := .Env}}
 export {{$K}}="{{$V}}"
