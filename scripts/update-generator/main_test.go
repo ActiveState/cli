c769013f0b99383b76e2248967ac53c90e75323b
Nathan Rijksen <n.rijksen@gmail.com>
2019-02-14 16:56:35 -0500

    Fix failing update-generator tests

--- scripts/update-generator/main_test.go
+++ scripts/update-generator/main_test.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/ActiveState/cli/internal/constants"
+
 	"github.com/ActiveState/cli/internal/environment"
 
 	"github.com/stretchr/testify/assert"
@@ -23,10 +25,17 @@ func TestCreateUpdate(t *testing.T) {
 	version = "1.0"
 	genDir = dir
 
+	exitCode := -1
+	exit = func(code int) {
+		exitCode = code
+	}
+
 	os.Chdir(environment.GetRootPathUnsafe())
 	run()
 
-	assert.FileExists(t, filepath.Join(dir, defaultPlatform+".json"), "Should create update bits")
-	assert.FileExists(t, filepath.Join(dir, "1.0", defaultPlatform+".json"), "Should create update bits")
-	assert.FileExists(t, filepath.Join(dir, "1.0", defaultPlatform+".gz"), "Should create update bits")
+	assert.Equal(t, -1, exitCode, "exit was not called")
+
+	assert.FileExists(t, filepath.Join(dir, constants.BranchName, defaultPlatform+".json"), "Should create update bits")
+	assert.FileExists(t, filepath.Join(dir, constants.BranchName, "1.0", defaultPlatform+".json"), "Should create update bits")
+	assert.FileExists(t, filepath.Join(dir, constants.BranchName, "1.0", defaultPlatform+".gz"), "Should create update bits")
 }
