c9666dd5fb2b19e56950e1aded72fc839a09d4b1
Nathan Rijksen <n.rijksen@gmail.com>
2019-02-14 17:18:08 -0500

    Fix selfupdate tests

--- state/selfupdate/selfupdate_test.go
+++ state/selfupdate/selfupdate_test.go
@@ -1,14 +1,24 @@
 package selfupdate
 
 import (
+	"os"
 	"testing"
 
+	"github.com/ActiveState/cli/internal/constants"
+	"github.com/ActiveState/cli/internal/testhelpers/httpmock"
+
 	"github.com/ActiveState/cli/internal/failures"
+	"github.com/ActiveState/cli/internal/testhelpers/updatemocks"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestExecute(t *testing.T) {
 	assert := assert.New(t)
+
+	httpmock.Activate(constants.APIUpdateURL)
+	defer httpmock.DeActivate()
+
+	updatemocks.MockUpdater(t, os.Args[0], "1.2.3-123")
 
 	Cc := Command.GetCobraCmd()
 	Cc.SetArgs([]string{"self-update"})
