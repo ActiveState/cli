e2675bb16faa1b1adac06642b4a36fb56aaf14f8
Nathan Rijksen <n.rijksen@gmail.com>
2019-02-15 16:37:27 -0500

    Add threadsafe warning

--- internal/testhelpers/exiter/exiter.go
+++ internal/testhelpers/exiter/exiter.go
@@ -6,6 +6,7 @@ var returnedExitCode int
 
 // WaitForExit will call the supplied function and return the exit code that occurs during its invokation, or -1 if no
 // exit was called. This requires you to use exiter.Exit as your exit function.
+// WARNING - this is not threadsafe!
 func WaitForExit(f func()) (exitCode int) {
 	returnedExitCode = -1
 	defer func() {
