e4032f9a4ac22762b7c04a990ec182101082240c
Nathan Rijksen <n.rijksen@gmail.com>
2019-03-01 12:50:15 -0500

    Added generate-locale script, which adds locale keys to en-us.yaml based on git diff

---
+++ scripts/locale-generator.py
@@ -0,0 +1,16 @@
+import subprocess
+import re
+import os
+
+os.chdir(os.path.join(os.path.dirname(__file__), ".."))
+result = subprocess.check_output(['git', 'diff'])
+matches = re.findall(re.compile(r"^\+.*locale.Tr?\(.(\w+)", re.M), result.decode('utf-8'))
+
+with open('locale/en-us.yaml', 'r') as file:
+    contents = file.read()
+
+with open('locale/en-us.yaml', 'a') as file:
+    for match in matches:
+        if not re.search(re.compile("^%s:" % match, re.M), contents):
+            print("Adding: %s" % match)
+            file.write("\n%s:\n  other: TODO" % match)
