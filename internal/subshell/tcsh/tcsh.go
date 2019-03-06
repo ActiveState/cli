442477fda6d9db0ae88b47ef37a678b2125f56d9
Nathan Rijksen <n.rijksen@gmail.com>
2019-03-06 11:01:36 -0500

    tcsh and fish shell should still use bash when running scripts

--- internal/subshell/tcsh/tcsh.go
+++ internal/subshell/tcsh/tcsh.go
@@ -104,12 +104,12 @@ func (v *SubShell) Deactivate() error {
 
 // Run - see subshell.SubShell
 func (v *SubShell) Run(script string, args ...string) (int, error) {
-	tmpfile, err := ioutil.TempFile("", "tcsh-script")
+	tmpfile, err := ioutil.TempFile("", "bash-script")
 	if err != nil {
 		return 1, err
 	}
 
-	tmpfile.WriteString("#!/usr/bin/env tcsh\n")
+	tmpfile.WriteString("#!/usr/bin/env bash\n")
 	tmpfile.WriteString(script)
 	tmpfile.Close()
 	os.Chmod(tmpfile.Name(), 0755)
