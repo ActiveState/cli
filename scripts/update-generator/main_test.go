224c36dd2efd4180d146dad034d605af17a47bd3
Nathan Rijksen <n.rijksen@gmail.com>
2019-03-11 13:23:26 -0400

    We can target a specific branch for our updates (allowing it to transition branches)

--- scripts/update-generator/main_test.go
+++ scripts/update-generator/main_test.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/ActiveState/cli/internal/constants"
 
 	"github.com/ActiveState/cli/internal/environment"
@@ -15,15 +17,13 @@ import (
 )
 
 func TestCreateUpdate(t *testing.T) {
+	executable := os.Args[0]
+
 	dir, err := ioutil.TempDir(os.TempDir(), "update-generator-test")
 	if err != nil {
 		log.Fatalf("Cannot create temp dir: %s", err.Error())
 	}
 	defer os.RemoveAll(dir)
-
-	appPath = os.Args[0]
-	version = "1.0"
-	genDir = dir
 
 	exitCode := -1
 	exit = func(code int) {
@@ -31,11 +31,24 @@ func TestCreateUpdate(t *testing.T) {
 	}
 
 	os.Chdir(environment.GetRootPathUnsafe())
+	os.Args = []string{"", "-o", dir, executable, "1.0"}
 	run()
 
-	assert.Equal(t, -1, exitCode, "exit was not called")
+	require.Equal(t, -1, exitCode, "exit was not called")
 
 	assert.FileExists(t, filepath.Join(dir, constants.BranchName, defaultPlatform+".json"), "Should create update bits")
 	assert.FileExists(t, filepath.Join(dir, constants.BranchName, "1.0", defaultPlatform+".json"), "Should create update bits")
 	assert.FileExists(t, filepath.Join(dir, constants.BranchName, "1.0", defaultPlatform+".gz"), "Should create update bits")
+
+	// Test with branch override
+	os.Chdir(environment.GetRootPathUnsafe())
+	branchName := "foo"
+	os.Args = []string{"", "--b", branchName, "-o", dir, executable, "1.0"}
+	run()
+
+	require.Equal(t, -1, exitCode, "exit was not called")
+
+	assert.FileExists(t, filepath.Join(dir, branchName, defaultPlatform+".json"), "Should create update bits")
+	assert.FileExists(t, filepath.Join(dir, branchName, "1.0", defaultPlatform+".json"), "Should create update bits")
+	assert.FileExists(t, filepath.Join(dir, branchName, "1.0", defaultPlatform+".gz"), "Should create update bits")
 }
