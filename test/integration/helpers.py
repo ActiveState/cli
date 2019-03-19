f1af46421832ea4954d3494f28f0d6598553606b
Nathan Rijksen <n.rijksen@gmail.com>
2019-03-19 14:37:55 -0400

    Remove unused args

--- test/integration/helpers.py
+++ test/integration/helpers.py
@@ -130,7 +130,7 @@ class wait_for_timeout:
     def __init__(self, seconds=1, error_message='Timeout'):
         self.seconds = seconds
         self.error_message = error_message
-    def handle_timeout(self, signum, frame):
+    def handle_timeout(self):
         raise TimeoutError(self.error_message)
     def __enter__(self):
         signal.signal(signal.SIGALRM, self.handle_timeout)
