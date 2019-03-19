bb5deb3b3bdbd9a4ebd62bc3605736477b03b505
Nathan Rijksen <n.rijksen@gmail.com>
2019-03-19 12:17:36 -0400

    Implemented basic integration testing framework

---
+++ test/integration/auth_test.py
@@ -0,0 +1,58 @@
+import os
+from os import path
+import signal
+import pexpect
+import time
+from pexpect.popen_spawn import PopenSpawn
+import helpers
+
+class TestAuth(helpers.IntegrationTest):
+
+    def __init__(self, *args, **kwargs):
+        super(TestAuth, self).__init__(*args, **kwargs)
+        self.username = "user-%d" % time.time()
+        self.password = self.username
+        self.email = "%s@test.tld" % self.username
+
+    def auth_signup(self):
+        self.spawn("auth signup")
+        self.expect("username:")
+        self.send(self.username)
+        self.send("\r\n")
+        self.expect("password:")
+        self.send(self.password)
+        self.expect("again:")
+        self.send(self.password)
+        self.expect("name:")
+        self.send(self.username)
+        self.expect("email:")
+        self.send(self.email)
+        self.expect("account has been registered")
+        self.wait()
+
+    def auth_logout(self):
+        self.spawn("auth logout")
+        self.expect("You have been logged out")
+        self.wait()
+
+    def auth_login(self):
+        self.spawn("auth")
+        self.expect("username:")
+        self.send(self.username)
+        self.expect("password:")
+        self.send(self.password)
+        self.expect("succesfully authenticated")
+        self.wait()
+
+        # still logged in?
+        self.spawn("auth")
+        self.expect("You are logged in")
+        self.wait()
+
+    def test_auth(self):
+        self.auth_signup()
+        self.auth_logout()
+        self.auth_login()
+
+if __name__ == '__main__':
+    helpers.Run()
---
+++ test/integration/env_test.py
@@ -0,0 +1,18 @@
+import os
+from os import path
+import signal
+import pexpect
+import time
+import helpers
+import sys
+
+class TestEnv(helpers.IntegrationTest):
+
+    def test_project_env(self):
+        print('%s -c "exec(\\"import os\\nprint(os.environ[\'ACTIVESTATE_PROJECT\'])\\")"' % sys.executable)
+        self.spawn_command('%s -c "exec(\'import os\\nprint(os.environ[\\\'ACTIVESTATE_PROJECT\\\'])\')"' % sys.executable)
+        self.expect("KeyError: 'ACTIVESTATE_PROJECT'")
+        self.wait(code=1)
+
+if __name__ == '__main__':
+    helpers.Run()
