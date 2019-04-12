a8b1d7b27fc9637cb8d72554c9a1c4c7a4d74ed2
th3cop <cgchoffman@gmail.com>
2019-04-12 14:06:36 -0700

    Add Window implementation of isExecutable.

---
+++ internal/fileutils/filesutil_windows.go
@@ -0,0 +1,26 @@
+package fileutils
+
+import (
+	"os"
+	"strings"
+)
+
+// IsExecutable determines if the file at the given path has any execute permissions.
+// This function does not care whether the current user can has enough privilege to
+// execute the file.
+func IsExecutable(path string) bool {
+	pathSplit := strings.Split(path, ".")
+	exe := pathSplit[len(pathSplit)-1]
+	if exe == "exe" {
+		return true
+	}
+
+	pathExts := strings.Split(os.Getenv("PATHEXT"), ";")
+	for _, ext := range pathExts {
+		// pathext entries have `.` and are capitalize
+		if strings.ToLower(exe) == strings.ToLower(ext)[1:] {
+			return true
+		}
+	}
+	return false
+}
---
+++ internal/fileutils/fileutils_linux.go
@@ -0,0 +1,11 @@
+package fileutils
+
+import "os"
+
+// IsExecutable determines if the file at the given path has any execute permissions.
+// This function does not care whether the current user can has enough privilege to
+// execute the file.
+func IsExecutable(path string) bool {
+	stat, err := os.Stat(path)
+	return err == nil && (stat.Mode()&(0111) > 0)
+}
