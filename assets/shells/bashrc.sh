7f44741879474843da33ce5bc1f3fe902b73d54d
Carey Hoffman <cgchoffman@gmail.com>
2019-05-01 14:28:16 -0700

    Remove debug statement, use '\' instead of space

--- assets/shells/bashrc.sh
+++ assets/shells/bashrc.sh
@@ -1,7 +1,7 @@
 if [ -f ~/.bashrc ]; then source ~/.bashrc; fi
-echo CHANGED THE RC FILE
+
 if [ -z "$PROMPT_COMMAND" ]; then
-    export PROMPT_COMMAND="echo -e '\033[1mActive state: {{.Project.Owner}}/{{.Project.Name}} \033[0m'"
+    export PROMPT_COMMAND="echo -e '\033[1mActive state: {{.Project.Owner}}/{{.Project.Name}}\\033[0m'"
 fi
 
 {{range $K, $V := .Env}}
