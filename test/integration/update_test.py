ef1039a6c4e599989dd12595cedda5f32c07737d
cgchoffman <careyh@activestate.com>
2019-05-02 16:51:37 -0700

    Integration test for update bits

--- test/integration/update_test.py
+++ test/integration/update_test.py
@@ -7,35 +7,34 @@ import tempfile
 import auth_test
 import helpers
 
+testdir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+projectdir = os.path.abspath(os.path.dirname(os.getenv("ACTIVESTATE_PROJECT")))
 
 class TestUpdates(helpers.IntegrationTest):
-    S3_UPDATE_URL = "https://s3.ca-central-1.amazonaws.com/cli-update/update/"
-    CONSTANTS = {}
 
     def __init__(self, *args, **kwargs):
         super(TestUpdates, self).__init__(*args, **kwargs)
-        self.parseConstants()
+        self.constants = {}
+        self.parseConstantsFiles()
 
     def getPlatform(self):
         plat = ""
         arch = "amd64"
-        if sys.platform == "linux2":
-            plat = "linux"
         if sys.platform == "win32":
             plat = "windows"
         else:
-            plat = - "darwin"
+            plat = sys.platform # linux and darwin are output on those platforms
         return plat+"-"+arch
 
-    def parseConstants(self):
-        constPath = os.path.join("..", "internal", "constants", "generated.go")
+    def parseConstantsFiles(self):
+        constPath = os.path.join(projectdir, "internal", "constants", "generated.go")
         goConstVarRe = re.compile(
-            "const\s+(?P<name>\w+)\s+=\s(?P<value>\"*\w+\"*)")
+            "const\s+(?P<name>[\w\d]+)\s+=\s\"(?P<value>.*?)\"")
         with open(constPath, 'r') as f:
             for line in f:
                 match = goConstVarRe.search(line)
                 if match != None:
-                    self.CONSTANTS[match.group("name")] = match.group("value")
+                    self.constants[match.group("name")] = match.group("value")
 
     def getArchExt(self):
         if sys.platform == "win32":
@@ -52,15 +51,32 @@ class TestUpdates(helpers.IntegrationTest):
     def TestUpdatesWorks(self):
         unArchiveCmd = ""
         platform = self.getPlatform()
-        archivePath = os.path.join(
-            "..", "..", "public", "update", self.CONSTANTS["BranchName"], self.CONSTANTS["VERSION"], platform+self.getArchExt())
+        archivePath = os.path.join(projectdir,
+                                   "public",
+                                   "update",
+                                   self.constants["BranchName"],
+                                   self.constants["Version"],
+                                   platform+self.getArchExt())
+        
         if platform.startswith("windows"):
-            unArchiveCmd = "powershell.exe -nologo -noprofile -command \"Expand-Archive '{0}' (Get-Location)\"".format(
-                archivePath)
+            unArchiveCmd = ["powershell.exe",
+                            "-nologo",
+                            "-noprofile",
+                            "-command",
+                            "\"Expand-Archive '{0}' (Get-Location)\"".format(archivePath)]
         else:
-            unArchiveCmd = "tar -xf {0}".format(archivePath)
+            unArchiveCmd = ["tar",
+                            "-C",
+                            ".",
+                            "-xf",
+                            archivePath]
+            
         done = subprocess.run(unArchiveCmd)
-        if done.returncode != 0:
-            pass
-        self.spawn("{0} --version".format(platform+self.getBinExt()))
-        self.expect(self.CONSTANTS["Version"])
+        self.assertEqual(0, done.returncode, "Nothing should go wrong")
+        cmd = "{0} --version".format(os.path.join(testdir,platform+self.getBinExt()))
+        self.spawn_command(cmd)
+        self.expect(self.constants["BuildNumber"])
+
+
+if __name__ == '__main__':
+    helpers.Run()
