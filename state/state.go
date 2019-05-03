f06a305ea1f2afd00ad3e18760f96401bc56ad50
cgchoffman <careyh@activestate.com>
2019-05-03 15:05:54 -0700

    Don't let auto-update happen

--- state/state.go
+++ state/state.go
@@ -76,7 +76,7 @@ func main() {
 	logging.Debug("main")
 	// Don't auto-update if we're 'state update'ing
 	manualUpdate := funk.Contains(os.Args, "update")
-	if flag.Lookup("test.v") == nil && !manualUpdate && updater.TimedCheck() {
+	if (flag.Lookup("test.v") == nil && os.Getenv(constants.DisableUpdates) == "") && !manualUpdate && updater.TimedCheck() {
 		relaunch() // will not return
 	}
 
