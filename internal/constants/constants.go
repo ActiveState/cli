fd23e9b09f34c25e925ccd6863a1bc8a2d884098
Nathan Rijksen <n.rijksen@gmail.com>
2019-05-10 11:17:39 -0400

    Allow overriding the auto-update timeout

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -30,6 +30,9 @@ const DisableRuntime = "ACTIVESTATE_CLI_DISABLE_RUNTIME"
 // UpdateBranchEnvVarName is the env var that is used to override which branch to pull the update from
 const UpdateBranchEnvVarName = "ACTIVESTATE_CLI_UPDATE_BRANCH"
 
+// AutoUpdateTimeoutEnvVarName is the env var that is used to override the timeout for auto update checks
+const AutoUpdateTimeoutEnvVarName = "ACTIVESTATE_CLI_AUTO_UPDATE_TIMEOUT"
+
 // InternalConfigFileName is effectively the same as InternalConfigName, but includes our preferred extension
 const InternalConfigFileName = "config.yaml"
 
