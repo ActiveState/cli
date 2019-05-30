f042c445ffb0062a3b97e24baf717386a8f72073
Carey Hoffman <cgchoffman@gmail.com>
2019-05-30 07:39:21 -0700

    Send setting update event when updating registry

--- public/install.ps1
+++ public/install.ps1
@@ -32,6 +32,28 @@ $script:BRANCH = $b
 $ErrorActionPreference = "Stop"
 
 # Helpers
+
+function notify_settingchange(){
+    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath -Force
+
+    $HWND_BROADCAST = [IntPtr] 0xffff;
+    $WM_SETTINGCHANGE = 0x1a;
+    $result = [UIntPtr]::Zero
+
+    if (-not ("Win32.NativeMethods" -as [Type]))
+    {
+        # import sendmessagetimeout from win32
+        Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessageTimeout(
+        IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
+        uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
+"@
+    }
+    # notify all windows of environment block change
+    [Win32.Nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [UIntPtr]::Zero, "Environment", 2, 5000, [ref] $result);
+
+}
 function isInRegistry($path){
     $regpaths = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path.Split(';')
     $inReg = $False
@@ -311,6 +333,7 @@ function install()
             # This only sets it in the registry and it will NOT be accessible in the current session
             Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH -Value $newPath
             $Env:Path = $newPath
+            notify_settingchange
             Write-Host "You may now start using the '$script:STATE' program"
         }
     }
@@ -319,6 +342,7 @@ function install()
         $Env:Path = $newPath
         Write-Host "'$installDir' appended to PATH for current session`n" -ForegroundColor Yellow
     }
+    return $installDir
 }
 
 install
