35c4fd67e2081d05361e7b287f8faf8ac2647373
Daved <daved@codemodus.com>
2019-06-11 16:04:47 -0700

    Correct remaining yamlfilemod var names

--- pkg/projectfile/yamlfilemod_test.go
+++ pkg/projectfile/yamlfilemod_test.go
@@ -14,20 +14,20 @@ project: https://example.com/xowner/xproject?commitID=12345
 12345: xvalue
 `)
 
-func TestYAMLReader(t *testing.T) {
+func TestYAMLFileMod(t *testing.T) {
 	buf := bytes.NewBuffer(exampleYAML)
-	yr := yamlFileMod{buf}
+	yfm := yamlFileMod{buf}
 
-	_, fail := yr.replaceInValue("", "a", "b")
+	_, fail := yfm.replaceInValue("", "a", "b")
 	assert.Error(t, fail.ToError())
-	_, fail = yr.replaceInValue(ProjectKey, "", "b")
+	_, fail = yfm.replaceInValue(ProjectKey, "", "b")
 	assert.Error(t, fail.ToError())
-	_, fail = yr.replaceInValue(ProjectKey, "a", "")
+	_, fail = yfm.replaceInValue(ProjectKey, "a", "")
 	assert.Error(t, fail.ToError())
 
 	outputYAML := bytes.Replace(exampleYAML, []byte("12345"), []byte("987"), 1) // must be 1
 
-	r, fail := yr.replaceInValue(ProjectKey, "12345", "987")
+	r, fail := yfm.replaceInValue(ProjectKey, "12345", "987")
 	assert.NoError(t, fail.ToError())
 
 	out := &bytes.Buffer{}
