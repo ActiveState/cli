e40ac1f930f4daedd3377e7ab3cd1ee084f8a829
Carey Hoffman <cgchoffman@gmail.com>
2019-06-11 10:36:21 -0700

    Revert "I still think it's hanging on install or taking forever"
    This reverts commit afbc69d9d801a026d3fc47d4342828c12f7e373d.

--- internal/subshell/subshell.go
+++ internal/subshell/subshell.go
@@ -158,7 +158,6 @@ func getRcFile(v SubShell) (*os.File, error) {
 	}
 
 	tmpFile.WriteString(out.String())
-	logging.Debug("####INSTALLING####: %s", out.String())
 	tmpFile.Close()
 
 	return tmpFile, err
