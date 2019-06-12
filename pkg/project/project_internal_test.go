753e92e1519b5552eb365d5cf2b9df0e939b14fe
Carey Hoffman <cgchoffman@gmail.com>
2019-06-12 13:21:40 -0700

    Individual tests

--- pkg/project/project_internal_test.go
+++ pkg/project/project_internal_test.go
@@ -18,29 +18,27 @@ func (suite *ProjectInternalTestSuite) BeforeTest(suiteName, testName string) {
 	failures.ResetHandled()
 }
 
-func (suite *ProjectInternalTestSuite) TestParseURL() {
-	{
-		// url pass including commitID
-		meta, fail := parseURL("https://platform.activestate.com/Project/TestParseURL?commitID=00010001-0001-0001-0001-000100010001")
-		suite.NoError(fail.ToError(), "Should load project without issue.")
-		suite.Equal("Project", meta.owner, "They should match")
-		suite.Equal("TestParseURL", meta.name, "They should match")
-		suite.Equal("00010001-0001-0001-0001-000100010001", meta.commitID, "They should match")
-	}
-	{
-		// url pass without commitID
-		meta, fail := parseURL("https://platform.activestate.com/Project/TestParseURL")
-		suite.NoError(fail.ToError(), "Should load project without issue.")
-		suite.Equal("Project", meta.owner, "They should match")
-		suite.Equal("TestParseURL", meta.name, "They should match")
-		suite.Equal("", meta.commitID, "They should match")
-	}
-	{
-		// url fail
-		_, fail := parseURL("Thisisnotavalidaprojecturl")
-		suite.Error(fail.ToError(), "This should fail")
-	}
+func (suite *ProjectInternalTestSuite) TestPassParseURL() {
 
+	// url pass including commitID
+	meta, fail := parseURL("https://platform.activestate.com/Project/TestParseURL?commitID=00010001-0001-0001-0001-000100010001")
+	suite.NoError(fail.ToError(), "Should load project without issue.")
+	suite.Equal("Project", meta.owner, "They should match")
+	suite.Equal("TestParseURL", meta.name, "They should match")
+	suite.Equal("00010001-0001-0001-0001-000100010001", meta.commitID, "They should match")
+}
+func (suite *ProjectInternalTestSuite) TestPassParseURLNoCommit() {
+	// url pass without commitID
+	meta, fail := parseURL("https://platform.activestate.com/Project/TestParseURL")
+	suite.NoError(fail.ToError(), "Should load project without issue.")
+	suite.Equal("Project", meta.owner, "They should match")
+	suite.Equal("TestParseURL", meta.name, "They should match")
+	suite.Equal("", meta.commitID, "They should match")
+}
+func (suite *ProjectInternalTestSuite) TestFailParseURL() {
+	// url fail
+	_, fail := parseURL("Thisisnotavalidaprojecturl")
+	suite.Error(fail.ToError(), "This should fail")
 }
 
 func Test_ProjectInternalTestSuite(t *testing.T) {
