db680d889489e57cf8c914e68c7e8fc4fcd91762
Daved <daved@codemodus.com>
2019-06-28 19:52:09 -0700

    Add hail tests

---
+++ internal/hail/hail_test.go
@@ -0,0 +1,73 @@
+package hail
+
+import (
+	"io/ioutil"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSend(t *testing.T) {
+	fail := Send("../hail", []byte{})
+	assert.Error(t, fail.ToError())
+
+	file := "hail-garbage"
+	f, err := os.Create(file)
+	require.NoError(t, err)
+	defer func() {
+		assert.NoError(t, f.Close())
+		assert.NoError(t, os.Remove(file))
+	}()
+
+	want := []byte("some data")
+	fail = Send(file, want)
+	require.NoError(t, fail.ToError())
+
+	got, err := ioutil.ReadAll(f)
+	require.NoError(t, err)
+	assert.Equal(t, got, want)
+}
+
+func TestOpen(t *testing.T) {
+	start := time.Now()
+	done := make(chan struct{})
+	defer close(done)
+
+	_, fail := Open(done, "../hail")
+	assert.Error(t, fail.ToError())
+
+	file := "hail-garbage"
+	rcvs, fail := Open(done, file)
+	require.NoError(t, fail.ToError())
+	defer func() {
+		assert.NoError(t, os.Remove(file))
+	}()
+	postOpen := time.Now()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	data := []byte("some data")
+
+	go func() {
+		defer wg.Done()
+
+		r := <-rcvs
+		assert.True(t, r.Open.After(start) && postOpen.After(r.Open))
+		assert.True(t, r.Time.After(postOpen))
+	}()
+
+	f, err := os.OpenFile(file, os.O_TRUNC|os.O_WRONLY, 0660)
+	require.NoError(t, err)
+	_, err = f.Write(data)
+	require.NoError(t, err)
+	defer func() {
+		assert.NoError(t, f.Close())
+	}()
+
+	wg.Wait()
+}
