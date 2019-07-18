b3de7059278e0044822bcbf347a0fd0609b33b93
Daved <daved@codemodus.com>
2019-07-17 21:35:36 -0700

    Add export recipe cmd logic as comments

--- state/export/recipe.go
+++ state/export/recipe.go
@@ -8,5 +8,17 @@ import (
 // ExecuteRecipe processes the `export recipe` command.
 func ExecuteRecipe(cmd *cobra.Command, args []string) {
 	logging.Debug("Execute")
-	cmd.Usage()
+
+	// get project
+
+	// pkg/platform/model
+	//func FetchProjectByName(orgName string, projectName string) (*mono_models.Project, *failures.Failure) {
+
+	// pkg/platform/model
+	//func FetchEffectiveRecipeForProject(pj *mono_models.Project) (*Recipe, *failures.Failure) {
+
+	// Recipe type
+	//func (m *RecipeResponseRecipesItems0) MarshalBinary() ([]byte, error) {
+
+	//print []byte as string
 }
