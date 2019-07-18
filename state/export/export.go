7019d72ee20783392e72e89361a9eee2b26e2510
Daved <daved@codemodus.com>
2019-07-18 15:59:50 -0700

    Move export recipe subcommand struct to recipe file

--- state/export/export.go
+++ state/export/export.go
@@ -29,30 +29,3 @@ func Execute(cmd *cobra.Command, args []string) {
 	logging.Debug("Execute")
 	cmd.Usage()
 }
-
-func init() {
-	Command.Append(RecipeCommand)
-}
-
-// RecipeCommand is a sub-command of export.
-var RecipeCommand = &commands.Command{
-	Name:        "recipe",
-	Description: "export_recipe_cmd_description",
-	Run:         ExecuteRecipe,
-	Arguments: []*commands.Argument{
-		&commands.Argument{
-			Name:        "export_recipe_cmd_commitid_arg",
-			Description: "export_recipe_cmd_commitid_arg_description",
-			Variable:    &Args.CommitID,
-		},
-	},
-	Flags: []*commands.Flag{
-		&commands.Flag{
-			Name:        "pretty",
-			Shorthand:   "p",
-			Description: "export_recipe_flag_pretty",
-			Type:        commands.TypeBool,
-			BoolVar:     &Flags.Pretty,
-		},
-	},
-}
--- state/export/recipe.go
+++ state/export/recipe.go
@@ -8,12 +8,40 @@ import (
 	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/logging"
 	"github.com/ActiveState/cli/internal/print"
+	"github.com/ActiveState/cli/pkg/cmdlets/commands"
 	"github.com/ActiveState/cli/pkg/platform/api/mono/mono_models"
 	"github.com/ActiveState/cli/pkg/platform/model"
 	"github.com/ActiveState/cli/pkg/project"
 	"github.com/go-openapi/strfmt"
 	"github.com/spf13/cobra"
 )
+
+func init() {
+	Command.Append(RecipeCommand)
+}
+
+// RecipeCommand is a sub-command of export.
+var RecipeCommand = &commands.Command{
+	Name:        "recipe",
+	Description: "export_recipe_cmd_description",
+	Run:         ExecuteRecipe,
+	Arguments: []*commands.Argument{
+		&commands.Argument{
+			Name:        "export_recipe_cmd_commitid_arg",
+			Description: "export_recipe_cmd_commitid_arg_description",
+			Variable:    &Args.CommitID,
+		},
+	},
+	Flags: []*commands.Flag{
+		&commands.Flag{
+			Name:        "pretty",
+			Shorthand:   "p",
+			Description: "export_recipe_flag_pretty",
+			Type:        commands.TypeBool,
+			BoolVar:     &Flags.Pretty,
+		},
+	},
+}
 
 // ExecuteRecipe processes the `export recipe` command.
 func ExecuteRecipe(cmd *cobra.Command, _ []string) {
