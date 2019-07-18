79ff671c2ec9180379bfd2c82639f6de4426d5b8
Daved <daved@codemodus.com>
2019-07-18 09:49:02 -0700

    Add commit handling to export recipe cmd comments

--- state/export/recipe.go
+++ state/export/recipe.go
@@ -8,15 +8,19 @@ import (
 // ExecuteRecipe processes the `export recipe` command.
 func ExecuteRecipe(cmd *cobra.Command, args []string) {
 	logging.Debug("Execute")
-	break
 
 	// get project
 
 	// pkg/platform/model
 	//func FetchProjectByName(orgName string, projectName string) (*mono_models.Project, *failures.Failure) {
 
+	// if no commit id
 	// pkg/platform/model
 	//func FetchEffectiveRecipeForProject(pj *mono_models.Project) (*Recipe, *failures.Failure) {
+
+	// if commit id
+	// pkg/platform/model
+	//func FetchEffectiveRecipeForCommit(pj *mono_models.Project, commitID strfmt.UUID) (*Recipe, *failures.Failure) {
 
 	// Recipe type
 	//func (m *RecipeResponseRecipesItems0) MarshalBinary() ([]byte, error) {
