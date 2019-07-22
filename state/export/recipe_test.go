67971cc773bc22334db772597f4dda07502ac87c
Daved <daved@codemodus.com>
2019-07-22 16:13:55 -0700

    Add export recipe experimental integ test setup

--- state/export/recipe_test.go
+++ state/export/recipe_test.go
@@ -5,25 +5,47 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/ActiveState/cli/internal/environment"
-	"github.com/ActiveState/cli/internal/failures"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/ActiveState/cli/internal/environment"
+	"github.com/ActiveState/cli/internal/testhelpers/exiter"
+	invMock "github.com/ActiveState/cli/pkg/platform/api/inventory/mock"
+	authMock "github.com/ActiveState/cli/pkg/platform/authentication/mock"
 )
 
-func setupRecipe(t *testing.T, args []string) {
+func setupRecipeCommand(t *testing.T, args ...string) func() {
 	root, err := environment.GetRootPath()
 	require.NoError(t, err, "should detect root path")
 	os.Chdir(filepath.Join(root, "test"))
 
 	cc := Command.GetCobraCmd()
 	cc.SetArgs(append([]string{"recipe"}, args...))
+
+	im := invMock.Init()
+	am := authMock.Init()
+	cleanup := func() {
+		im.Close()
+		am.Close()
+	}
+
+	im.MockPlatforms()
+	im.MockOrderRecipes()
+
+	am.MockLoggedin()
+
+	return cleanup
 }
 
 func TestExportRecipe(t *testing.T) {
-	setupRecipe(t, []string{"test"})
+	cleanup := setupRecipeCommand(t, "test")
+	defer cleanup()
 
-	err := Command.Execute()
-	assert.NoError(t, err, "executed without error")
-	assert.NoError(t, failures.Handled(), "no failure occurred")
+	ex := exiter.New()
+	Command.Exiter = ex.Exit
+	exitCode := ex.WaitForExit(func() {
+		Command.Execute()
+	})
+
+	assert.Equal(t, 0, exitCode, "exited with code 0")
 }
