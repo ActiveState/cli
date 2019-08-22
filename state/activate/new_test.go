bd7fe3be539026c7a85cc3b1603b992bca616598
Daved <daved@codemodus.com>
2019-08-22 15:32:18 -0700

    Update activate new cmd test

--- state/activate/new_test.go
+++ state/activate/new_test.go
@@ -20,10 +20,14 @@ func (suite *ActivateTestSuite) TestActivateNew() {
 	httpmock.Register("POST", "/login")
 	httpmock.Register("GET", "/organizations")
 	httpmock.Register("POST", "organizations/test-owner/projects")
+	httpmock.Register("GET", "organizations/test-owner/projects/test-name")
+	httpmock.Register("POST", "vcs/commit")
+	httpmock.Register("PUT", "vcs/branch/00010001-0001-0001-0001-000100010001")
 
 	authentication.Get().AuthenticateWithToken("")
 
 	suite.promptMock.OnMethod("Input").Once().Return("test-name", nil)
+	suite.promptMock.OnMethod("Select").Once().Return("Python 3", nil)
 	suite.promptMock.OnMethod("Input").Once().Return("test-owner", nil)
 
 	err := Command.Execute()
