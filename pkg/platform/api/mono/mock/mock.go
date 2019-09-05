0b3f8c225a47a1796e03ca31d1597d6f021ff792
Martin Drohmann <mdrohmann@gmail.com>
2019-09-05 09:53:37 -0700

    add some mocks to invite users (also added doc strings)

--- pkg/platform/api/mono/mock/mock.go
+++ pkg/platform/api/mono/mock/mock.go
@@ -10,22 +10,26 @@ import (
 	mono_models "github.com/ActiveState/cli/pkg/platform/api/mono/mono_models"
 )
 
+// Mock registers some common http requests usually used by the model
 type Mock struct {
 	httpmock *httpmock.HTTPMock
 }
 
 var mock *httpmock.HTTPMock
 
+// Init initializes the mocking helper
 func Init() *Mock {
 	return &Mock{
 		httpmock.Activate(api.GetServiceURL(api.ServiceMono).String()),
 	}
 }
 
+// Close de-activates the mocking helper
 func (m *Mock) Close() {
 	httpmock.DeActivate()
 }
 
+// MockSignS3URI registers mocks for requests for receiving signed S3 URIs to packages
 func (m *Mock) MockSignS3URI() {
 	ext := ".tar.gz"
 	if runtime.GOOS == "windows" {
@@ -35,11 +39,13 @@ func (m *Mock) MockSignS3URI() {
 	m.httpmock.RegisterWithResponse("GET", "/s3/sign/http:%2F%2Ftest.tld%2Flegacy-python"+ext, 200, "s3/sign/legacy-python"+ext+".json")
 }
 
+// MockVcsGetCheckpoint registers mocks for version control commits
 func (m *Mock) MockVcsGetCheckpoint() {
 	m.httpmock.Register("GET", "/vcs/commits/00010001-0001-0001-0001-000100010001/checkpoint")
 	m.httpmock.Register("GET", "/vcs/commits/00020002-0002-0002-0002-000200020002/checkpoint")
 }
 
+// MockVcsGetCheckpointPython registers a mock returning a VCS checkpoint for python
 func (m *Mock) MockVcsGetCheckpointPython() {
 	m.MockVcsGetCheckpointCustomReq(&mono_models.Checkpoint{
 		Namespace:   "language",
@@ -47,6 +53,7 @@ func (m *Mock) MockVcsGetCheckpointPython() {
 	})
 }
 
+// MockVcsGetCheckpointCustomReq registers a mock returning the platform data for a specific requirement at a given VCS checkpoint
 func (m *Mock) MockVcsGetCheckpointCustomReq(requirement *mono_models.Checkpoint) {
 	jsonBytes, err := requirement.MarshalBinary()
 	if err != nil {
@@ -56,35 +63,63 @@ func (m *Mock) MockVcsGetCheckpointCustomReq(requirement *mono_models.Checkpoint
 	m.httpmock.RegisterWithResponseBody("GET", "/vcs/commits/00010001-0001-0001-0001-000100010001/checkpoint", 200, json)
 }
 
+// MockGetProject registers mocks for project "string" and a VCS checkpoint
 func (m *Mock) MockGetProject() {
 	m.httpmock.Register("GET", "/organizations/string/projects/string")
 	m.httpmock.Register("GET", "/vcs/commits/00010001-0001-0001-0001-000100010001/checkpoint")
 }
 
+// MockGetProjectNoLanguage returns a mock returning a project without a language set
 func (m *Mock) MockGetProjectNoLanguage() {
 	m.httpmock.RegisterWithResponse("GET", "/organizations/string/projects/string", 200, "organizations/string/projects/string-no-language")
 }
 
+// MockGetProjectDiffCommit registers a mock returning a project with a commit history
 func (m *Mock) MockGetProjectDiffCommit() {
 	m.httpmock.RegisterWithResponse("GET", "/organizations/string/projects/string", 200, "organizations/string/projects/string-diff-commit")
 }
 
+// MockGetProject404 registers a mock for a request for a non-existent project
 func (m *Mock) MockGetProject404() {
 	m.httpmock.RegisterWithCode("GET", "/organizations/string/projects/string", 404)
 }
 
+// MockGetOrganizations registers a mock returning organizations
 func (m *Mock) MockGetOrganizations() {
 	httpmock.RegisterWithCode("GET", "/organizations", 200)
 }
 
+// MockGetOrganization registers a mock returning the specific organization "string"
 func (m *Mock) MockGetOrganization() {
 	httpmock.RegisterWithCode("GET", "/organizations/string", 200)
 }
 
+// MockGetOrganizationLimits registers a mock returning the limits for an organization
+func (m *Mock) MockGetOrganizationLimits() {
+	httpmock.RegisterWithCode("GET", "/organizations/string/limits", 200)
+}
+
+// MockGetOrganizationLimits404 registers a mock for a limit request for a non-existent organization
+func (m *Mock) MockGetOrganizationLimits404() {
+	httpmock.RegisterWithCode("GET", "/organizations/string/limits", 404)
+}
+
+// MockGetOrganization404 registers a mock for a request for a non-existent organization
 func (m *Mock) MockGetOrganization404() {
 	httpmock.RegisterWithCode("GET", "/organizations/string", 404)
 }
 
+// MockInviteUserToOrg registers a mock invite request
+func (m *Mock) MockInviteUserToOrg() {
+	httpmock.RegisterWithCode("POST", "/organizations/string/invitations/string", 200)
+}
+
+// MockInviteUserToOrg404 registers a mock invite request for a non-existent organization
+func (m *Mock) MockInviteUserToOrg404() {
+	httpmock.RegisterWithCode("POST", "/organizations/string/invitations/string", 404)
+}
+
+// MockCommit registers a mock for a VCS commit
 func (m *Mock) MockCommit() {
 	m.httpmock.Register("POST", "/vcs/commit")
 }
