f525a2640a3af98494105148609c89f34ed0c348
Daved <daved@codemodus.com>
2019-09-12 22:03:25 -0700

    Add hangup (posix) to subshell stop

--- internal/subshell/sscommon/sscommon_darwin.go
+++ internal/subshell/sscommon/sscommon_darwin.go
@@ -8,11 +8,16 @@ import (
 )
 
 func stop(cmd *exec.Cmd) *failures.Failure {
-	sig := syscall.SIGTERM
-
 	// may panic if process no longer exists
 	defer failures.Recover()
+
 	// darwin randomly returns an error when using cmd.Process.Signal
+	sig := syscall.SIGHUP
+	if err := syscall.Kill(cmd.Process.Pid, sig); err != nil {
+		return FailSignalCmd.Wrap(err)
+	}
+
+	sig := syscall.SIGTERM
 	if err := syscall.Kill(cmd.Process.Pid, sig); err != nil {
 		return FailSignalCmd.Wrap(err)
 	}
