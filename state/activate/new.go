fb7479f043e3a5812d435f5a2c73bcd05711d4b9
Carey Hoffman <cgchoffman@gmail.com>
2019-09-17 20:11:57 -0700

    This isn't supposed to be in this PR

--- state/activate/new.go
+++ state/activate/new.go
@@ -87,16 +87,14 @@ func NewExecute(cmd *cobra.Command, args []string) {
 		exit(1)
 	}
 
-	var commitID string
-	commitID, fail = latestCommitID(owner, name)
-	if fail != nil || commitID == "" {
+	if fail != nil {
 		failures.Handle(fail, locale.T("error_state_activate_new_no_commit_aborted",
 			map[string]interface{}{"Owner": owner, "ProjectName": name}))
 
 		exit(1)
 	}
 
-	projectURL := fmt.Sprintf("https://%s/%s/%s?commitID=%s", constants.PlatformURL, owner, name, commitID)
+	projectURL := fmt.Sprintf("https://%s/%s/%s", constants.PlatformURL, owner, name)
 
 	// Create the project locally on disk.
 	if _, fail = projectfile.Create(projectURL, path); fail != nil {
@@ -111,19 +109,6 @@ func NewExecute(cmd *cobra.Command, args []string) {
 	}
 
 	print.Line(locale.T("state_activate_new_created", map[string]interface{}{"Dir": path}))
-}
-
-func latestCommitID(owner, project string) (string, *failures.Failure) {
-	cid, fail := model.LatestCommitID(owner, project)
-	if fail != nil {
-		return "", fail
-	}
-
-	if cid != nil {
-		return cid.String(), nil
-	}
-
-	return "", nil
 }
 
 func promptForLanguage() (language.Language, *failures.Failure) {
