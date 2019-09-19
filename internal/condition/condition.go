4d0e4713c253e4946d0cf1311d2c2e8266b1f19c
Daved <daved@codemodus.com>
2019-09-19 12:08:45 -0700

    Add all common in-test checks to condition pkg

--- internal/condition/condition.go
+++ internal/condition/condition.go
@@ -1,11 +1,14 @@
 package condition
 
 import (
+	"flag"
 	"os"
 	"strings"
 )
 
-var inTest = strings.HasSuffix(os.Args[0], ".test")
+var inTest = strings.HasSuffix(os.Args[0], ".test") ||
+	strings.Contains(os.Args[0], "/_test/") ||
+	flag.Lookup("test.v") != nil
 
 // InTest returns true when the app is being tested
 func InTest() bool { return inTest }
