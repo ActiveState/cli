1ec263cffb188053b9a35c7123d5a9cf0961ef13
mdrakos <mike.drakos@gmail.com>
2019-09-23 09:53:19 -0700

    Ensure only one test has secrets access warning

--- state/activate/activate_test.go
+++ state/activate/activate_test.go
@@ -255,8 +255,6 @@ func (suite *ActivateTestSuite) TestActivateSecretsAccessError() {
 	httpmock.Activate(api.GetServiceURL(api.ServiceMono).String())
 	defer httpmock.DeActivate()
 
-	httpmock.Register("POST", "/login")
-	httpmock.Register("GET", "organizations/ActiveState/projects/CodeIntel")
 	httpmock.RegisterWithCode("GET", "/organizations/ActiveState/members", 401)
 
 	dir := filepath.Join(environment.GetRootPathUnsafe(), "state", "activate", "testdata")
@@ -281,11 +279,10 @@ func (suite *ActivateTestSuite) TestActivateSecretsNoAccess() {
 	httpmock.Activate(api.GetServiceURL(api.ServiceMono).String())
 	defer httpmock.DeActivate()
 
-	httpmock.Register("POST", "/login")
-	httpmock.Register("GET", "organizations/ActiveState/projects/CodeIntel")
-	httpmock.Register("GET", "/organizations/ActiveState/members")
+	httpmock.Register("GET", "organizations/AccessOrg/projects/CodeIntel")
+	httpmock.Register("GET", "/organizations/AccessOrg/members")
 
-	dir := filepath.Join(environment.GetRootPathUnsafe(), "state", "activate", "testdata")
+	dir := filepath.Join(environment.GetRootPathUnsafe(), "state", "activate", "testdata", "access")
 	err := os.Chdir(dir)
 	suite.Require().NoError(err, "unable to chdir to testdata dir")
 	suite.Require().FileExists(filepath.Join(dir, constants.ConfigFileName))
