be142e6dbe6ee89ff6f85567eb70e890b3593240
Nathan Rijksen <n.rijksen@gmail.com>
2019-09-27 13:26:10 -0400

    Added test for rxutils

--- internal/rxutils/rxutils.go
+++ internal/rxutils/rxutils.go
@@ -6,19 +6,19 @@ import (
 
 // ReplaceAllStringSubmatchFunc allows you to replace regex with a callback that includes matched groups
 // based on https://medium.com/@elliotchance/go-replace-string-with-regular-expression-callback-f89948bad0bb
-func ReplaceAllStringSubmatchFunc(re *regexp.Regexp, str string, repl func([]string) string) string {
+func ReplaceAllStringSubmatchFunc(re *regexp.Regexp, input string, replacer func([]string) string) string {
 	result := ""
 	lastIndex := 0
-	for _, v := range re.FindAllSubmatchIndex([]byte(str), -1) {
+	for _, v := range re.FindAllSubmatchIndex([]byte(input), -1) {
 		groups := []string{}
 		for i := 0; i < len(v); i += 2 {
 			if v[i] == -1 {
 				continue // optional group
 			}
-			groups = append(groups, str[v[i]:v[i+1]])
+			groups = append(groups, input[v[i]:v[i+1]])
 		}
-		result += str[lastIndex:v[0]] + repl(groups)
+		result += input[lastIndex:v[0]] + replacer(groups)
 		lastIndex = v[1]
 	}
-	return result + str[lastIndex:]
+	return result + input[lastIndex:]
 }
