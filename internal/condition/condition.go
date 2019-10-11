abbccb8705c21be12f94848db33a4077d2ec918b
mdrakos <mike.drakos@gmail.com>
2019-10-11 10:23:08 -0700

    Move InTest check to init function

--- internal/condition/condition.go
+++ internal/condition/condition.go
@@ -10,20 +10,22 @@ var inTest = strings.HasSuffix(os.Args[0], ".test") ||
 	strings.Contains(os.Args[0], "/_test/") ||
 	flag.Lookup("test.v") != nil
 
-// InTest returns true when the app is being tested
-func InTest() bool {
+func init() {
 	if inTest {
-		return inTest
+		return
 	}
 
 	for _, flag := range os.Args {
 		if strings.Contains(flag, "test.timeout") {
-			return true
+			inTest = true
 		}
 		if strings.Contains(flag, "test.logfile") {
-			return true
+			inTest = true
 		}
 	}
+}
 
-	return false
+// InTest returns true when the app is being tested
+func InTest() bool {
+	return inTest
 }
