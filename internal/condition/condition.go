31ad83c3ee2c0c90aa86ba6e9cfdd4a0b1103673
mdrakos <mike.drakos@gmail.com>
2019-10-17 09:29:49 -0700

    Remove init from condition package

--- internal/condition/condition.go
+++ internal/condition/condition.go
@@ -1,29 +1,12 @@
 package condition
 
 import (
-	"flag"
 	"os"
 	"strings"
 )
 
-var inTest = strings.HasSuffix(os.Args[0], ".test") ||
-	strings.Contains(os.Args[0], "/_test/") ||
-	flag.Lookup("test.v") != nil
-
-func init() {
-	if inTest {
-		return
-	}
-
-	for _, flag := range os.Args {
-		if strings.Contains(flag, "test.timeout") {
-			inTest = true
-		}
-		if strings.Contains(flag, "test.logfile") {
-			inTest = true
-		}
-	}
-}
+var inTest = strings.HasSuffix(strings.TrimSuffix(os.Args[0], ".exe"), ".test") ||
+	strings.Contains(os.Args[0], "/_test/")
 
 // InTest returns true when the app is being tested
 func InTest() bool {
