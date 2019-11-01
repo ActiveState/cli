378f7565292760291ecb6e8def8cb17692ae1a2c
Nathan Rijksen <n.rijksen@gmail.com>
2019-11-01 16:57:15 -0400

    Remove the old activate command

--- state/activate/activate_lin_test.go
+++
@@ -1,9 +0,0 @@
-// +build linux
-
-package activate
-
-func (suite *ActivateTestSuite) TestExecuteWithNamespaceWithLang() {
-	suite.rMock.MockFullRuntime()
-	pjfile := suite.testExecuteWithNamespace(true)
-	suite.Require().Empty(pjfile.Languages)
-}
