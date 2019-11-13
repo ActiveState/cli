c2f2e32c1a3adaa77bdbfb0f4e4f0806815e3ba4
Martin C Drohmann <martind@activestate.com>
2019-11-12 23:27:10 -0800

    cleanup in integration test suite

--- internal/testhelpers/integration/integration.go
+++ internal/testhelpers/integration/integration.go
@@ -32,7 +32,6 @@ type Suite struct {
 	executable string
 	cmd        *exec.Cmd
 	env        []string
-	logFile    *os.File
 	wd         *string
 }
 
@@ -119,10 +118,6 @@ func (s *Suite) SpawnCustom(executable string, args ...string) {
 	fmt.Printf("Spawning '%s' from %s\n", osutils.CmdString(s.cmd), wd)
 
 	var err error
-	s.logFile, err = os.Create("pty.log")
-	if err != nil {
-		s.Failf("", "Could not open pty log file: %v", err)
-	}
 	s.console, err = expect.NewConsole(
 		expect.WithDefaultTimeout(10 * time.Second),
 	)
