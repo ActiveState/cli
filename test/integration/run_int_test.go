1216864aef9f54ea740a15e6555c402373374da3
Martin C Drohmann <martind@activestate.com>
2019-11-14 15:27:10 -0800

    finish up RunIntegrationTest

--- test/integration/run_int_test.go
+++ test/integration/run_int_test.go
@@ -1,96 +1,98 @@
 package integration
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/testhelpers/integration"
+	"github.com/ActiveState/cli/pkg/projectfile"
 	"github.com/stretchr/testify/suite"
+	"gopkg.in/yaml.v2"
 )
 
 type RunIntegrationTestSuite struct {
 	integration.Suite
+	tmpDirCleanup func()
 }
 
-func (suite *RunIntegrationTestSuite) prepareTempDirectory(prefix string) (tempDir string, cleanup func()) {
+func (suite *RunIntegrationTestSuite) createProjectFile(projectDir string) {
+	configFileContent := strings.TrimSpace(`
+project: https://platform.activestate.com/Owner/ProjectName
+scripts:
+  - name: test
+    description: A script that runs for 20 seconds doing nothing.  It should be interrupted.
+    standalone: true
+    value: |
+       bash -c "
+            function f() { echo received SIGINT; }
+            trap f SIGINT
+            trap -p SIGINT
+            echo 'Start of script'
+            sleep 10000          
+            echo 'After first sleep or interrupt'
+            trap 'exit 123' SIGINT
+            sleep 2
+            echo 'After second sleep, but not printed after second interrupt'
+          "
+`)
+	projectFile := &projectfile.Project{}
+	err := yaml.Unmarshal([]byte(configFileContent), projectFile)
+	suite.Require().NoError(err)
 
-	tempDir, err := ioutil.TempDir("", prefix)
-	suite.Require().NoError(err)
-	err = os.RemoveAll(tempDir)
-	suite.Require().NoError(err)
-	err = os.MkdirAll(tempDir, 0770)
-	suite.Require().NoError(err)
-	suite.Require().NoError(err)
-	suite.SetWd(tempDir)
+	projectFile.SetPath(filepath.Join(projectDir, constants.ConfigFileName))
+	fail := projectFile.Save()
+	suite.Require().NoError(fail.ToError())
 
-	f, err := os.Create(path.Join(tempDir, "activestate.yaml"))
-	suite.Require().NoError(err)
-	f.WriteString("project: https://platform.activestate.com/Owner/ProjectName\n" +
-		"scripts:\n" +
-		"   - name: test\n" +
-		"     description: A script that runs for 20 seconds doing nothing.  It should be interrupted.\n" +
-		"     value: |\n" +
-		"          echo start of script\n" +
-		"          timeout 4\n" +
-		"          echo ONLY PRINT IF NOT INTERRUPTED\n" +
-		"     constraints:\n" +
-		"       os: windows\n" +
-		"   - name: test\n" +
-		"     description: A script that runs for 20 seconds doing nothing.  It should be interrupted.\n" +
-		"     value: |\n" +
-		"          echo start of script\n" +
-		"          sleep 4\n" +
-		"          echo ONLY PRINT IF NOT INTERRUPTED\n" +
-		"     constraints:\n" +
-		"       os: linux,macos\n")
-	f.Close()
-
-	return tempDir, func() {
-		os.Chdir(os.TempDir())
-		os.RemoveAll(tempDir)
-	}
 }
 
-func (suite *RunIntegrationTestSuite) BeforeTest(suiteName, testName string) {
-	fmt.Printf("Run before test: %s %s", suiteName, testName)
+func (suite *RunIntegrationTestSuite) SetupTest() {
+	suite.Suite.SetupTest()
+	tmpDir, cleanup := suite.PrepareTemporaryWorkingDirectory(suite.T().Name())
+	suite.tmpDirCleanup = cleanup
+
+	suite.createProjectFile(tmpDir)
 }
 
-func (suite *RunIntegrationTestSuite) TestUninterruptedRun() {
+func (suite *RunIntegrationTestSuite) TeardownTest() {
+	suite.Suite.TeardownTest()
+	suite.tmpDirCleanup()
+}
 
-	_, cb := suite.prepareTempDirectory("activate_run_non_interrupt")
-	defer cb()
-	defer suite.TeardownTest()
+func (suite *RunIntegrationTestSuite) TestOneInterrupt() {
 
 	suite.Spawn("run", "test")
-	suite.Expect("start of script", 4*time.Second)
-	// wait for one second
-	time.Sleep(time.Second)
-	suite.Expect("ONLY PRINT IF NOT INTERRUPTED", 5*time.Second)
+	suite.Expect("Start of script")
+	time.Sleep(200 * time.Millisecond)
+	// interrupt the first (very long sleep)
+	suite.Send(string([]byte{3}))
+	suite.SendCtrlC()
+
+	suite.Expect("received SIGINT", 3*time.Second)
+	suite.Expect("After first sleep or interrupt", 2*time.Second)
+	suite.Expect("After second sleep")
 	res, err := suite.Wait(1 * time.Second)
 	suite.Require().NoError(err)
 	suite.Require().Equal(0, res.ExitCode())
 }
 
-func (suite *RunIntegrationTestSuite) TestInterruptedRun() {
-
-	_, cb := suite.prepareTempDirectory("activate_run_interrupt")
-	defer cb()
-	defer suite.TeardownTest()
-
+func (suite *RunIntegrationTestSuite) TestTwoInterrupts() {
 	suite.Spawn("run", "test")
-	suite.Expect("start of script", 2*time.Second)
-	// wait for one second
-	time.Sleep(time.Second)
-	suite.Send(string([]byte{0x03}))
-	suite.Expect("^C", time.Second)
-	res, err := suite.Wait(1 * time.Second)
-	suite.Require().NotContains(suite.TerminalSnapshot(), "ONLY PRINT IF NOT INTERRUPTED")
+	suite.Expect("Start of script")
+	time.Sleep(200 * time.Millisecond)
+	suite.SendCtrlC()
+	suite.Expect("received SIGINT", 3*time.Second)
+	suite.Expect("After first sleep or interrupt", 2*time.Second)
+	time.Sleep(200 * time.Millisecond)
+	suite.SendCtrlC()
+	res, err := suite.Wait(3 * time.Second)
 	suite.Require().NoError(err)
-	suite.Require().Equal(-1, res.ExitCode())
+	suite.Require().Equal(123, res.ExitCode())
+	suite.Require().NotContains(
+		suite.TerminalSnapshot(), "not printed after second interrupt",
+	)
 }
 
 func TestRunIntegrationTestSuite(t *testing.T) {
