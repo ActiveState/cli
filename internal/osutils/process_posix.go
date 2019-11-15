aca204243d61c2d02cf39e25a86de42c265b66d5
Martin C Drohmann <martind@activestate.com>
2019-11-14 21:49:53 -0800

    fix typo and add doc strings

--- internal/osutils/process_posix.go
+++ internal/osutils/process_posix.go
@@ -4,9 +4,9 @@ package osutils
 
 import "syscall"
 
-
+// SysProcAttrForNewProcessGroup returns a SysProcAttr structure configured to start a process with a new process group
 func SysProcAttrForNewProcessGroup() *syscall.SysProcAttr {
-	return &syscall.SysProcAttr {
-		SetSid: true,
+	return &syscall.SysProcAttr{
+		Setsid: true,
 	}
 }
--- internal/osutils/process_windows.go
+++ internal/osutils/process_windows.go
@@ -4,8 +4,9 @@ package osutils
 
 import "syscall"
 
+// SysProcAttrForNewProcessGroup returns a SysProcAttr structure configured to start a process with a new process group
 func SysProcAttrForNewProcessGroup() *syscall.SysProcAttr {
-	return &syscall.SysProcAttr {
+	return &syscall.SysProcAttr{
 		CreationFlags: 0x00000200, // CREATE_NEW_PROCESS_GROUP
 	}
 }
