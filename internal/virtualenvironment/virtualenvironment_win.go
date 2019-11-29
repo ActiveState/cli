5abc13ff6e9ea8820998f8b6be1f0c76a506b469
mdrakos <mike.drakos@gmail.com>
2019-11-29 13:30:09 -0800

    Only check special environment variables when necessary

--- internal/virtualenvironment/virtualenvironment_win.go
+++ internal/virtualenvironment/virtualenvironment_win.go
@@ -13,28 +13,30 @@ func inheritEnv(env map[string]string) map[string]string {
 		split := strings.Split(kv, "=")
 		key := split[0]
 		value := split[1]
-
-		// Windows allows environment variables that are not uppercase.
-		// This can lead to duplicate path entries. At this point we
-		// have already constructed a PATH so it's safe to discard the
-		// os Path.
-		if strings.ToLower(key) == "path" {
-			continue
-		}
+		dynamicEnvVarRe := regexp.MustCompile(`(^=.+)=(.+)`)
 
 		// cmd.exe on Windows uses some dynamic environment variables
 		// that begin with an '='. We want to make sure we include
 		// these in the virtual environment. For more information see:
 		// https://devblogs.microsoft.com/oldnewthing/20100506-00/?p=14133
-		dynamicEnvVarRe := regexp.MustCompile(`(^=.+)=(.+)`)
-		groups := dynamicEnvVarRe.FindStringSubmatch(kv)
-		if len(groups) == 0 {
-			continue
-		}
-		env[groups[1]] = groups[2]
+		if strings.HasPrefix(kv, "=") {
+			groups := dynamicEnvVarRe.FindStringSubmatch(kv)
+			if len(groups) == 0 {
+				continue
+			}
+			env[groups[1]] = groups[2]
+		} else {
+			// Windows allows environment variables that are not uppercase.
+			// This can lead to duplicate path entries. At this point we
+			// have already constructed the env vars that we need for
+			// our virtual environment so we discard any duplicate entries`.
+			if _, ok := env[strings.ToUpper(key)]; ok {
+				continue
+			}
 
-		if _, ok := env[key]; !ok {
-			env[key] = value
+			if _, ok := env[key]; !ok {
+				env[key] = value
+			}
 		}
 	}
 
