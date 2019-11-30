2bbc31acf556dfcd980a174a2c7a90c6a7bff7e7
mdrakos <mike.drakos@gmail.com>
2019-11-29 16:14:02 -0800

    Update activate for globals

--- cmd/state/internal/cmdtree/activate.go
+++ cmd/state/internal/cmdtree/activate.go
@@ -2,20 +2,27 @@ package cmdtree
 
 import (
 	"github.com/ActiveState/cli/internal/captain"
+	"github.com/ActiveState/cli/internal/failures"
 	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/prompt"
 	"github.com/ActiveState/cli/internal/runners/activate"
+	"github.com/ActiveState/cli/pkg/cmdlets/commands"
 	"github.com/ActiveState/cli/pkg/cmdlets/git"
 	"github.com/spf13/viper"
 )
 
-func newActivateCommand() *captain.Command {
+type ActivateArgs struct {
+	Path      string
+	Namespace string
+}
+
+func newActivateCommand(globals *globalOptions) *captain.Command {
 	prompter := prompt.New()
 	checkout := activate.NewCheckout(git.NewRepo())
 	namespaceSelect := activate.NewNamespaceSelect(viper.GetViper(), prompter)
 	activateRunner := activate.NewActivate(namespaceSelect, checkout)
 
-	var namespace, path string
+	var args = ActivateArgs{}
 	return captain.NewCommand(
 		"activate",
 		locale.T("activate_project"),
@@ -25,18 +32,39 @@ func newActivateCommand() *captain.Command {
 				Shorthand:   "",
 				Description: locale.T("flag_state_activate_path_description"),
 				Type:        captain.TypeString,
-				StringVar:   &path,
+				StringVar:   &args.Path,
 			},
 		},
 		[]*captain.Argument{
 			&captain.Argument{
 				Name:        locale.T("arg_state_activate_namespace"),
 				Description: locale.T("arg_state_activate_namespace_description"),
-				Variable:    &namespace,
+				Variable:    &args.Namespace,
 			},
 		},
-		func(ccmd *captain.Command, args []string) error {
-			return activateRunner.Run(namespace, path)
+		func(_ *captain.Command, _ []string) error {
+			params, err := newAcivateRunParams(args, globals)
+			if err != nil {
+				return err
+			}
+
+			return activateRunner.Run(params)
 		},
 	)
+}
+
+func newAcivateRunParams(args ActivateArgs, globals *globalOptions) (*activate.ActivateParams, error) {
+	output := commands.Output(globals.Output)
+	switch output {
+	case commands.JSON, commands.EditorV0:
+		// Input is correct
+	default:
+		return nil, failures.FailUserInput.New("TODO:")
+	}
+
+	return &activate.ActivateParams{
+		Namespace:     args.Namespace,
+		PreferredPath: args.Path,
+		Output:        output,
+	}, nil
 }
