69ee1107ccd9c488445a41f8c1eba0850e37bdd8
Daved <daved@codemodus.com>
2019-12-16 16:15:22 -0800

    Drop package int sub tests

--- test/integration/package_int_test.go
+++ test/integration/package_int_test.go
@@ -12,38 +12,55 @@ type PackageIntegrationTestSuite struct {
 	integration.Suite
 }
 
-func (suite *PackageIntegrationTestSuite) TestPackage_listing() {
-	tempDir, cleanup := suite.PrepareTemporaryWorkingDirectory("package_no_args")
+func (suite *PackageIntegrationTestSuite) TestPackage_listingSimple() {
+	tempDir, cleanup := suite.PrepareTemporaryWorkingDirectory(suite.T().Name())
 	defer cleanup()
 
-	suite.AppendEnv([]string{"ACTIVESTATE_CLI_DISABLE_RUNTIME=false"})
+	suite.PrepareActiveStateYAML(tempDir)
 
+	suite.Spawn("package")
+	suite.Expect("Name")
+	suite.Expect("python")
+	suite.Wait()
+}
+
+func (suite *PackageIntegrationTestSuite) TestPackage_listingWithCommitValid() {
+	tempDir, cleanup := suite.PrepareTemporaryWorkingDirectory(suite.T().Name())
+	defer cleanup()
+
+	suite.PrepareActiveStateYAML(tempDir)
+
+	suite.Spawn("package", "--commit", "c780f643-724b-49bb-aca9-194e3c072f64")
+	suite.Expect("Name")
+	suite.Expect("python")
+	suite.Wait()
+}
+
+func (suite *PackageIntegrationTestSuite) TestPackage_listingWithCommitInvalid() {
+	tempDir, cleanup := suite.PrepareTemporaryWorkingDirectory(suite.T().Name())
+	defer cleanup()
+
+	suite.PrepareActiveStateYAML(tempDir)
+
+	suite.Spawn("package", "--commit", "junk")
+	suite.Expect("Cannot obtain")
+	suite.Wait()
+}
+
+func (suite *PackageIntegrationTestSuite) TestPackage_listingWithCommitUnknown() {
+	tempDir, cleanup := suite.PrepareTemporaryWorkingDirectory(suite.T().Name())
+	defer cleanup()
+
+	suite.PrepareActiveStateYAML(tempDir)
+
+	suite.Spawn("package", "--commit", "00010001-0001-0001-0001-000100010001")
+	suite.Expect("No data")
+	suite.Wait()
+}
+
+func (suite *PackageIntegrationTestSuite) PrepareActiveStateYAML(dir string) {
 	asyData := `project: "https://platform.activestate.com/ActiveState-CLI/Python3"`
-	suite.Require().NoError(setupASY(tempDir, asyData))
-
-	suite.Run("simple", func() {
-		suite.Spawn("package")
-		suite.Expect("Name")
-		suite.Wait()
-	})
-
-	suite.Run("with commit", func() {
-		suite.Spawn("package", "--commit", "c780f643-724b-49bb-aca9-194e3c072f64")
-		suite.Expect("Name")
-		suite.Wait()
-	})
-
-	suite.Run("with commit junk val", func() {
-		suite.Spawn("package", "--commit", "junk")
-		suite.Expect("Cannot obtain")
-		suite.Wait()
-	})
-
-	suite.Run("with commit unknown id", func() {
-		suite.Spawn("package", "--commit", "00010001-0001-0001-0001-000100010001")
-		suite.Expect("No data")
-		suite.Wait()
-	})
+	suite.Suite.PrepareActiveStateYAML(dir, asyData)
 }
 
 func TestPackageIntegrationTestSuite(t *testing.T) {
