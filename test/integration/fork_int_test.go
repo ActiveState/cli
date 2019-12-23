4b4eef4e60ffc964c12f31e235933a75ba2df498
mdrakos <mike.drakos@gmail.com>
2019-12-23 13:14:09 -0800

    Add editor.v0 test for state run

--- test/integration/fork_int_test.go
+++ test/integration/fork_int_test.go
@@ -24,7 +24,7 @@ func (suite *ForkIntegrationTestSuite) TestFork_FailNameExists() {
 	suite.LoginAsPersistentUser()
 	suite.AppendEnv([]string{"ACTIVESTATE_CLI_DISABLE_RUNTIME=false"})
 
-	suite.Spawn("fork", "ActiveState-CLI/Python3")
+	suite.Spawn("fork", "ActiveState-CLI/Python3", "--org", integration.PersistentUsername)
 	suite.Expect("Could not create the forked project", 30*time.Second)
 	suite.Expect("The name 'Python3' is no longer available, it was used in a now deleted project.", 30*time.Second)
 	suite.NotContains(suite.Output(), "Successfully forked project")
