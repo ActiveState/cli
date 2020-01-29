1ebb70885fed2b4871f86312d614ca76b6b02f4d
Daved <daved@codemodus.com>
2020-01-29 15:14:12 -0800

    Minimize skeleton style methods

--- internal/runners/initialize/skeleton/style.go
+++ internal/runners/initialize/skeleton/style.go
@@ -57,36 +57,9 @@ func (s *Style) String() string {
 	return s.data().name
 }
 
-// Text returns the human-readable value.
-func (s *Style) Text() string {
-	if s == nil {
-		return ""
-	}
-	return s.data().text
-}
-
 // Recognized returns whether the style is a known useful value.
 func (s *Style) Recognized() bool {
 	return s != nil && *s != Unset && *s != Unknown
-}
-
-// UnmarshalYAML implements the go-yaml/yaml.Unmarshaler interface.
-func (s *Style) UnmarshalYAML(applyPayload func(interface{}) error) error {
-	if s == nil {
-		return fmt.Errorf("cannot unmarshal to nil style")
-	}
-
-	var payload string
-	if err := applyPayload(&payload); err != nil {
-		return err
-	}
-
-	return s.Set(payload)
-}
-
-// MarshalYAML implements the go-yaml/yaml.Marshaler interface.
-func (s Style) MarshalYAML() (interface{}, error) {
-	return s.String(), nil
 }
 
 // Set implements the captain marshaler interfaces.
@@ -111,17 +84,6 @@ func (s *Style) Set(v string) error {
 // Type implements the captain.FlagMarshaler interface.
 func (s *Style) Type() string {
 	return "skeleton-style"
-}
-
-// RecognizedStyles returns all skeleton styles that are supported.
-func RecognizedStyles() []Style {
-	var styles []Style
-	for i := range styleLookup {
-		if s := Style(i); s.Recognized() {
-			styles = append(styles, s)
-		}
-	}
-	return styles
 }
 
 // RecognizedStylesNames returns all skeleton style names that are supported.
