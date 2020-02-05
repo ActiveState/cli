a19cba450be622d6453ddf336e1d9ccc0d9d1669
mdrakos <mike.drakos@gmail.com>
2020-02-04 16:13:38 -0800

    Remove unnecessary checks from run

--- internal/runners/clean/run_lin_mac.go
+++ internal/runners/clean/run_lin_mac.go
@@ -3,30 +3,14 @@
 package clean
 
 import (
-	"errors"
 	"os"
 
-	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/logging"
 	"github.com/ActiveState/cli/internal/output"
 )
 
 func run(params *RunParams, confirm confirmAble, outputer output.Outputer) error {
-	if os.Getenv(constants.ActivatedStateEnvVarName) != "" {
-		return errors.New(locale.T("err_clean_activated"))
-	}
-
-	if !params.Force {
-		ok, fail := confirm.Confirm(locale.T("clean_confirm_remove"), false)
-		if fail != nil {
-			return fail.ToError()
-		}
-		if !ok {
-			return nil
-		}
-	}
-
 	logging.Debug("Removing cache path: %s", params.CachePath)
 	err := os.RemoveAll(params.CachePath)
 	if err != nil {
