8ce674434e7fdfb3d687061c33a086d8c86b0b9e
mdrakos <mike.drakos@gmail.com>
2020-03-23 15:07:24 -0700

    Add script to update version in .wxs file

---
+++ scripts/wix/wix.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+
+	"github.com/ActiveState/cli/internal/constants"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal(errors.New("Must provide only filepath to .wxs file"))
+	}
+
+	xmlRaw, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	versionRE := regexp.MustCompile(`REPLACE-VERSION`)
+	updated := versionRE.ReplaceAll(xmlRaw, []byte(constants.Version))
+
+	err = ioutil.WriteFile(os.Args[1], updated, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
