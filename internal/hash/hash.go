49e3e2d7710b5bb1de270a4a07ada8c158bec34d
Martin C Drohmann <martind@activestate.com>
2020-03-25 14:24:47 -0700

    commit internal/hash

---
+++ internal/hash/hash.go
@@ -0,0 +1,21 @@
+package hash
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// ShortHash will return the first 4 bytes in base16 of the sha1 sum of the provided data.
+//
+// For example:
+//   ShortHash("ActiveState-TestProject-python3")
+// 	 => e784c7e0
+//
+// This is useful for creating a shortened namespace for language installations.
+func ShortHash(data ...string) string {
+	h := sha1.New()
+	io.WriteString(h, strings.Join(data, ""))
+	return fmt.Sprintf("%x", h.Sum(nil)[:4])
+}
