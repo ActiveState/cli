ce8880f9cd6422c19efc7f319b1220b7ca59bd0b
Nathan Rijksen <n.rijksen@gmail.com>
2020-04-01 16:00:41 -0700

    Windows implementation

--- internal/subshell/cmd/cmd.go
+++ internal/subshell/cmd/cmd.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
-	"fmt"
+	"errors"
+	"github.com/ActiveState/cli/internal/locale"
+	"github.com/spf13/viper"
+	"golang.org/x/sys/windows/registry"
 	"os"
 	"os/exec"
 
@@ -42,19 +45,122 @@ func (v *SubShell) SetBinary(binary string) {
 
 // WriteUserEnv - see subshell.SubShell
 func (v *SubShell) WriteUserEnv(env map[string]string) error {
-	for k, v := range env {
-		var cmd *exec.Cmd
-		if k == "PATH" {
-			cmd = exec.Command("setx", "PATH", fmt.Sprintf("%s;%s", v, "%PATH%"))
-		} else {
-			cmd = exec.Command("setx", k, v)
+	// Clean up old entries
+	oldEnv := viper.GetStringMap("user_env")
+	for k, v  := range oldEnv {
+		if err := unsetUserEnv(k, v.(string)); err != nil {
+			return err
 		}
-		err := cmd.Run()
+	}
+
+	// Store new entries
+	viper.Set("user_env", env)
+
+	for k, v := range env {
+		value := v
+		if k == "PATH" {
+			path, err := getUserEnv("PATH")
+			if err != nil {
+				return err
+			}
+			if path != "" {
+				path = ";" + path
+			}
+
+			value = v+path
+		}
+
+		// Set key/value in the user environment
+		err := setUserEnv(k, value)
 		if err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func unsetUserEnv(name, ifValueEquals string) error {
+	key, err := registry.OpenKey(registry.CURRENT_USER, "Environment", registry.ALL_ACCESS)
+	if err != nil {
+		return failures.FailOS.Wrap(err, locale.T("err_windows_registry"))
+	}
+	defer key.Close()
+
+	v, _, err := key.GetStringValue(name)
+	if err != nil {
+		if errors.Is(err, registry.ErrNotExist) {
+			return nil
+		}
+		return failures.FailOS.Wrap(err, locale.T("err_windows_registry"))
+	}
+
+	if v != ifValueEquals {
+		return nil
+	}
+
+	// Check for backup value
+	backupValue, _, err := key.GetStringValue(envBackupName(name))
+	realError := err != nil && ! errors.Is(err, registry.ErrNotExist)
+	backupExists := err == nil
+
+	if realError {
+		return failures.FailOS.Wrap(err, locale.T("err_windows_registry"))
+	}
+	if backupExists {
+		if err := key.DeleteValue(envBackupName(name)); err != nil {
+			return failures.FailOS.Wrap(err, locale.T("err_windows_registry"))
+		}
+		return key.SetStringValue(name, backupValue)
+	}
+	return key.DeleteValue(name)
+}
+
+
+func setUserEnv(name, newValue string) error {
+	key, err := registry.OpenKey(registry.CURRENT_USER, "Environment", registry.ALL_ACCESS)
+	if err != nil {
+		return failures.FailOS.Wrap(err, locale.T("err_windows_registry"))
+	}
+	defer key.Close()
+
+	// Check if we're going to be overriding
+	oldValue, _, err := key.GetStringValue(name)
+	if err != nil && ! errors.Is(err, registry.ErrNotExist) {
+		return failures.FailOS.Wrap(err, locale.T("err_windows_registry"))
+	} else if err == nil {
+		// Save backup
+		if err2 := key.SetStringValue(envBackupName(name), oldValue); err2 != nil {
+			return failures.FailOS.Wrap(err2, locale.T("err_windows_registry"))
+		}
+	}
+
+	return key.SetStringValue(name, newValue)
+}
+
+func getUserEnv(name string) (string, error) {
+	key, err := registry.OpenKey(registry.CURRENT_USER, "Environment", registry.ALL_ACCESS)
+	if err != nil {
+		return "", failures.FailOS.Wrap(err, locale.T("err_windows_registry"))
+	}
+	defer key.Close()
+
+	// Check if we're going to be overriding
+	originalValue, _, err := key.GetStringValue(envBackupName(name))
+	if err != nil && ! errors.Is(err, registry.ErrNotExist) {
+		return "", failures.FailOS.Wrap(err, locale.T("err_windows_registry"))
+	} else if err == nil {
+		return originalValue, nil
+	}
+
+	v, _, err := key.GetStringValue(name)
+	if err != nil && ! errors.Is(err, registry.ErrNotExist) {
+		return v, failures.FailOS.Wrap(err, locale.T("err_windows_registry"))
+	}
+	return v, nil
+}
+
+func envBackupName(name string) string {
+	return name+"_ORIGINAL"
 }
 
 // SetEnv - see subshell.SetEnv
