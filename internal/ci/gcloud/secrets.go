f85e8586934109890558b32b4a68bf61d2ed10d3
Nathan Rijksen <n.rijksen@gmail.com>
2020-04-09 11:34:00 -0700

    Source projectID from gcloud metadata

--- internal/ci/gcloud/secrets.go
+++ internal/ci/gcloud/secrets.go
@@ -3,12 +3,11 @@ package gcloud
 import (
 	"context"
 	"fmt"
-	"os"
 
+	"cloud.google.com/go/compute/metadata"
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 
-	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/logging"
 )
 
@@ -19,25 +18,25 @@ func (e ErrNotAvailable) Error() string { return "" }
 
 // GetSecret accesses the payload for the given secret
 func GetSecret(name string) (string, error) {
-	// Check if we have the project ID set via env var
-	var project string
-	var found bool
-	if project, found = os.LookupEnv(constants.GcloudProjectEnvVarName); ! found {
-		return "", fmt.Errorf("%s is required %w", constants.GcloudProjectEnvVarName, ErrNotAvailable{})
-	}
-
-	// Formulate the secret path
-	path := fmt.Sprintf("projects/%s/secrets/%s/versions/latest", project, name)
-	logging.Debug("Accessing gcloud secret at: %s", path)
-
 	// Create the client.
 	ctx := context.Background()
 	client, err := secretmanager.NewClient(ctx)
 	if err != nil {
 		// gcloud does not expose the error type for "no credentials", so we're going to assume any error is a not available error
-		logging.Debug("%v", err)
-		return "", fmt.Errorf("failed to create secretmanager client: %v %w", err, ErrNotAvailable{})
+		logging.Debug("Gcloud Secretmanager failed to initialize (ignore if you're not trying to use gcloud): %v", err)
+		return "", fmt.Errorf("failed to create gcloud secretmanager client: %v %w", err, ErrNotAvailable{})
 	}
+
+	projectID, err := metadata.ProjectID()
+	if err != nil {
+		// we might not be on gcloud at all, for the time being I've yet to find a cheap way to identify this
+		logging.Debug("Gcloud could not get project ID (ignore if you're not trying to use gcloud): %v", err)
+		return "", fmt.Errorf("failed to create gcloud metadata client: %v %w", err, ErrNotAvailable{})
+	}
+
+	// Formulate the secret path
+	path := fmt.Sprintf("projects/%s/secrets/%s/versions/latest", projectID, name)
+	logging.Debug("Accessing gcloud secret at: %s", path)
 
 	// Build the request.
 	req := &secretmanagerpb.AccessSecretVersionRequest{
