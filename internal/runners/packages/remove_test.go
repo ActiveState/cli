220a27a6b80ab44bf749d65be4c63a1a271a8259
Daved <daved@codemodus.com>
2020-04-14 16:05:59 -0700

    Update pkgs remove test

--- internal/runners/packages/remove_test.go
+++ internal/runners/packages/remove_test.go
@@ -1,19 +1,51 @@
 package packages
 
 import (
+	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/suite"
+	"github.com/kami-zh/go-capturer"
 )
 
-type RemoveTestSuite struct {
-	PkgTestSuite
-}
+func TestRemove(t *testing.T) {
+	deps := &dependencies{}
 
-func (suite *RemoveTestSuite) TestRemove() {
-	suite.runsCommand([]string{"remove", "artifact"}, -1, "Package removed: artifact")
-}
+	tests := map[string]struct {
+		namevers        string
+		wantOutContains string
+		wantErr         bool
+	}{
+		"no version": {"artifact", "Package removed: artifact", noErr},
+	}
 
-func TestRemoveTestSuite(t *testing.T) {
-	suite.Run(t, new(RemoveTestSuite))
+	for tn, tt := range tests {
+		t.Run(tn, func(t *testing.T) {
+			deps.setUp()
+			defer deps.cleanUp()
+
+			params := RemoveRunParams{Name: tt.namevers}
+			runner := NewRemove()
+
+			var err error
+			out := capturer.CaptureOutput(func() {
+				err = runner.Run(params)
+			})
+			if !tt.wantErr && err != nil {
+				t.Errorf("got %v, want nil", err)
+				return
+			}
+
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("got nil, want err")
+					return
+				}
+				out = err.Error()
+			}
+
+			if !strings.Contains(out, tt.wantOutContains) {
+				t.Errorf("got %s, want (contains) %s", out, tt.wantOutContains)
+			}
+		})
+	}
 }
