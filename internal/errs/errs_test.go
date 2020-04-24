85d04c8468e680ec0eb3ad6546d599e292758c85
Nathan Rijksen <n.rijksen@gmail.com>
2020-04-24 13:32:08 -0700

    Simplify fatal failures

--- internal/errs/errs_test.go
+++ internal/errs/errs_test.go
@@ -38,18 +38,15 @@ func TestErrs(t *testing.T) {
 			}
 			ee, ok := err.(errs.Error)
 			if ! ok {
-				t.Error("Error should be of type errs.Error")
-				t.FailNow()
+				t.Fatalf("Error should be of type errs.Error")
 			}
 			if ee.Stack() == nil {
-				t.Error("Stacktrace was not created")
-				t.FailNow()
+				t.Fatalf("Stacktrace was not created")
 			}
 			for i, frame := range ee.Stack().Frames {
 				curFile := rtutils.CurrentFile()
 				if strings.Contains(frame.Path, filepath.Dir(curFile)) && frame.Path != curFile {
-					t.Errorf("Stack should not contain reference to errs package.\nFound: %s at frame %d. Full stack:\n%s", frame.Path, i, ee.Stack().String())
-					t.FailNow()
+					t.Fatalf("Stack should not contain reference to errs package.\nFound: %s at frame %d. Full stack:\n%s", frame.Path, i, ee.Stack().String())
 				}
 			}
 			if joinmessage := errs.Join(tt.err, ","); joinmessage.Error() != tt.wantJoinMessage {
