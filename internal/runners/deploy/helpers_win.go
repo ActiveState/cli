433396f57e94a3669f479085e6a61b6734309f20
mdrakos <mike.drakos@gmail.com>
2020-04-28 10:18:20 -0700

    Use locale errors when creating shortcuts

--- internal/runners/deploy/helpers_win.go
+++ internal/runners/deploy/helpers_win.go
@@ -13,41 +13,51 @@ import (
 )
 
 func link(src, dst string) error {
-	logging.Debug("Creating shortcut, oldname: %s newname: %s", src, dst)
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 
 	if strings.HasSuffix(dst, ".exe") {
 		dst = strings.Replace(dst, ".exe", ".lnk", 1)
 	}
+	logging.Debug("Creating shortcut, oldname: %s newname: %s", src, dst)
 
 	ole.CoInitializeEx(0, ole.COINIT_APARTMENTTHREADED|ole.COINIT_SPEED_OVER_MEMORY)
 	oleShellObject, err := oleutil.CreateObject("WScript.Shell")
 	if err != nil {
-		return err
+		return locale.WrapInputError(
+			err, "err_create_shell",
+			"Could not create OLE shell object")
 	}
 	defer oleShellObject.Release()
 
 	wshell, err := oleShellObject.QueryInterface(ole.IID_IDispatch)
 	if err != nil {
-		return err
+		return locale.WrapInputError(
+			err, "err_create_wshell",
+			"Could not create WShell dispatch")
 	}
 	defer wshell.Release()
 
 	cs, err := oleutil.CallMethod(wshell, "CreateShortcut", dst)
 	if err != nil {
-		return err
+		return locale.WrapInputError(
+			err, "err_create_shortcut",
+			"Could not create shortcut at path: {{.V0}}", dst)
 	}
 	idispatch := cs.ToIDispatch()
 
 	_, err = oleutil.PutProperty(idispatch, "TargetPath", src)
 	if err != nil {
-		return err
+		return locale.WrapInputError(
+			err, "err_short_add_target",
+			"Could not add target property to shortcut")
 	}
 
 	_, err = oleutil.CallMethod(idispatch, "Save")
 	if err != nil {
-		return err
+		return locale.WrapInputError(
+			err, "err_save_shortcut",
+			"Could not save shortcut")
 	}
 
 	return nil
