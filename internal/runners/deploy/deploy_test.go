0627270acc08b0e20706d5ebdc15da51f177fb12
mdrakos <mike.drakos@gmail.com>
2020-04-27 17:00:48 -0700

    Fix deploy unit test

--- internal/runners/deploy/deploy_test.go
+++ internal/runners/deploy/deploy_test.go
@@ -181,12 +181,6 @@ func Test_runStepsWithFuncs(t *testing.T) {
 }
 
 func Test_report(t *testing.T) {
-	baseEnv := map[string]string{
-		"KEY1": "VAL1",
-		"KEY2": "VAL2",
-	}
-	prepareDeployEnv(baseEnv)
-
 	type args struct {
 		envGetter runtime.EnvGetter
 	}
@@ -202,13 +196,19 @@ func Test_report(t *testing.T) {
 			args{
 				&EnvGetMock{
 					func(inherit bool, projectDir string) (map[string]string, *failures.Failure) {
-						baseEnv["PATH"] = "PATH1" + string(os.PathListSeparator) + "PATH2"
-						return baseEnv, nil
+						return map[string]string{
+							"KEY1": "VAL1",
+							"KEY2": "VAL2",
+							"PATH": "PATH1" + string(os.PathListSeparator) + "PATH2",
+						}, nil
 					},
 				},
 			},
 			[]string{"PATH1", "PATH2"},
-			baseEnv,
+			map[string]string{
+				"KEY1": "VAL1",
+				"KEY2": "VAL2",
+			},
 			nil,
 		},
 	}
