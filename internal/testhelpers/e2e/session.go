7d78fb34fb6ba9c1800af4d720241ba8fa13a18b
Nathan Rijksen <n.rijksen@gmail.com>
2020-04-30 09:29:23 -0700

    Fix bad merge

--- internal/testhelpers/e2e/session.go
+++ internal/testhelpers/e2e/session.go
@@ -21,8 +21,6 @@ import (
 	"github.com/ActiveState/cli/internal/environment"
 	"github.com/ActiveState/cli/internal/fileutils"
 	"github.com/ActiveState/cli/pkg/projectfile"
-
-	"github.com/ActiveState/cli/internal/osutils/stacktrace"
 )
 
 // Session represents an end-to-end testing session during which several console process can be spawned and tested
@@ -154,7 +152,6 @@ func (s *Session) PrepareActiveStateYAML(contents string) {
 	projectFile.SetPath(filepath.Join(s.Dirs.Work, "activestate.yaml"))
 	fail := projectFile.Save()
 	require.NoError(s.t, fail.ToError(), msg)
-	projectFile.Persist()
 }
 
 // PrepareFile writes a file to path with contents, expecting no error
@@ -234,26 +231,8 @@ func observeSendFn(s *Session) func(string, int, error) {
 	}
 }
 
-func observeExpectFn(s *Session) func([]expect.Matcher, string, string, error) {
-	return func(matchers []expect.Matcher, raw, pty string, err error) {
-		if err == nil {
-			return
-		}
-
-		var value string
-		var sep string
-		for _, matcher := range matchers {
-			value += fmt.Sprintf("%s%v", sep, matcher.Criteria())
-			sep = ", "
-		}
-
-		pty = strings.TrimRight(pty, " \n") + "\n"
-
-		s.t.Fatalf(
-			"Could not meet expectation: Expectation: '%s'\nError: %v at\n%s\n---\nTerminal snapshot:\n%s\n---\nParsed output:\n%s\n",
-			value, err, stacktrace.Get().String(), pty, raw,
-		)
-	}
+func observeExpectFn(s *Session) expect.ExpectObserver {
+	return termtest.TestExpectObserveFn(s.t)
 }
 
 // Close removes the temporary directory unless RetainDirs is specified
