06ae76c2c96bcd19a5a7c242a24ca08959731264
Nathan Rijksen <n.rijksen@gmail.com>
2020-05-08 15:17:40 -0700

    Fix powershell script using an invalid path

--- internal/runners/deploy/link_win.go
+++ internal/runners/deploy/link_win.go
@@ -5,13 +5,16 @@ package deploy
 import (
 	"bytes"
 	"os/exec"
-	"path/filepath"
 	"strconv"
 	"strings"
 
-	"github.com/ActiveState/cli/internal/environment"
+	"github.com/gobuffalo/packr"
+
+	"github.com/ActiveState/cli/internal/errs"
+	"github.com/ActiveState/cli/internal/language"
 	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/logging"
+	"github.com/ActiveState/cli/internal/scriptfile"
 )
 
 func link(src, dst string) error {
@@ -20,24 +23,21 @@ func link(src, dst string) error {
 	}
 	logging.Debug("Creating shortcut, source: %s target: %s", src, dst)
 
-	root, err := environment.GetRootPath()
-	if err != nil {
-		return locale.WrapError(
-			err, "err_link_get_root",
-			"Could not get root path of shortcut script",
-		)
+	box := packr.NewBox("../../../assets/scripts/")
+	sfile, fail := scriptfile.New(language.PowerShell, "createShortcut", box.String("createShortcut.ps1"))
+	if fail != nil {
+		return errs.Wrap(fail.ToError(), "Could not create createShortcut.ps1 scriptfile")
 	}
-	scriptPath := filepath.Join(root, "assets", "scripts", "createShortcut.ps1")
 
 	// Some paths may contain spaces so we must quote
 	src = strconv.Quote(src)
 	dst = strconv.Quote(dst)
 
-	cmd := exec.Command("powershell.exe", "-ExecutionPolicy", "Bypass", "-Command", scriptPath, src, dst)
+	cmd := exec.Command("powershell.exe", "-ExecutionPolicy", "Bypass", "-Command", sfile.Filename(), src, dst)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &out
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		return locale.WrapError(err, "err_powersell_symlink", "Invoking powershell to create a shortcut failed with error code: {{.V0}}, error: {{.V1}}", err.Error(), out.String())
 	}
