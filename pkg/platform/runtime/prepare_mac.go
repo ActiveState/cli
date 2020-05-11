a78807eaa73dfc8723855f8b697431a387b8220c
Martin C Drohmann <martind@activestate.com>
2020-05-11 09:35:30 -0700

    relocate script shebangs in binary directory
    The only part of a Python MacOS build that needs to be relocated are the
    script files in the binary directory.
    
    This commit adds functionality to do targeted relocations, and
    configures a targeted relocation for the specific use case.
    
    The activation integration tests for Python2 and Python3 are updated to
    test whether installed scripts (for `pip2` and `pip3`) are functional.

--- pkg/platform/runtime/prepare_mac.go
+++ pkg/platform/runtime/prepare_mac.go
@@ -18,9 +18,10 @@ import (
 // These runtimes do not include metadata files as they should
 // be runnable from where they are unarchived
 func (m *MetaData) Prepare() *failures.Failure {
+	frameWorkDir := "Library/Frameworks/Python.framework/Versions/"
 	m.BinaryLocations = []MetaDataBinary{
 		MetaDataBinary{
-			Path:     "Library/Frameworks/Python.framework/Versions/Current/bin/",
+			Path:     filepath.Join(frameWorkDir, "Current", "bin"),
 			Relative: true,
 		},
 	}
@@ -32,8 +33,8 @@ func (m *MetaData) Prepare() *failures.Failure {
 
 	m.setPythonEnv()
 
-	libDir := filepath.Join(m.Path, "Library/Frameworks/Python.framework/Versions/Current/lib")
-	dirRe := regexp.MustCompile(`python\d.\d`)
+	libDir := filepath.Join(m.Path, frameWorkDir, "Current", "lib")
+	dirRe := regexp.MustCompile(`python\d+.\d+`)
 
 	files, err := ioutil.ReadDir(libDir)
 	if err != nil {
@@ -53,6 +54,33 @@ func (m *MetaData) Prepare() *failures.Failure {
 
 	if fileutils.DirExists(sitePackages) {
 		m.Env["PYTHONPATH"] = m.Env["PYTHONPATH"] + string(os.PathListSeparator) + sitePackages
+	}
+
+	// the binaries are actually in a versioned directory
+	// this version is likely the same as the found above, but it doesn't hurt to get explicitly
+	dirRe = regexp.MustCompile(`\d+(?:\.\d+)+`)
+	files, err = ioutil.ReadDir(filepath.Join(m.Path, frameWorkDir))
+	if err != nil {
+		return failures.FailOS.Wrap(err)
+	}
+
+	var relVersionedFrameWorkDir string
+	for _, f := range files {
+		if !f.IsDir() {
+			continue
+		}
+		if dirRe.MatchString(f.Name()) {
+			relVersionedFrameWorkDir = filepath.Join(frameWorkDir, f.Name())
+			break
+		}
+	}
+
+	if relVersionedFrameWorkDir != "" {
+		m.TargetedRelocations = []TargetedRelocation{TargetedRelocation{
+			InDir:        filepath.Join(m.Path, frameWorkDir, "Current", "bin"),
+			SearchString: filepath.Join("/", relVersionedFrameWorkDir),
+			Replacement:  filepath.Join(m.Path, relVersionedFrameWorkDir),
+		}}
 	}
 
 	return nil
