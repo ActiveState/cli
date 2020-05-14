72d0b4889b82936e9c960c1c1c425c14bcf4e2e1
Martin C Drohmann <martind@activestate.com>
2020-05-14 12:04:39 -0700

    disable runtime env for win test

--- internal/virtualenvironment/virtualenvironment_win_test.go
+++ internal/virtualenvironment/virtualenvironment_win_test.go
@@ -3,7 +3,10 @@
 package virtualenvironment
 
 import (
+	"os"
 	"testing"
+
+	"github.com/ActiveState/cli/internal/constants"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -12,6 +15,9 @@ import (
 func TestGetEnvSlice_NoPath(t *testing.T) {
 	setup(t)
 	defer teardown()
+
+	os.Setenv(constants.DisableRuntime, "true")
+	defer os.Unsetenv(constants.DisableRuntime)
 
 	venv := Init()
 	env, err := venv.GetEnvSlice(true)
