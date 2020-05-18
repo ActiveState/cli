b283243b878b3a91f864dd8d901036e267f2b39a
mdrakos <mike.drakos@gmail.com>
2020-05-18 14:17:17 -0700

    Enable change dialog and run custom actions on change

--- public/wix/Deploy/StateDeploy/CustomAction.cs
+++ public/wix/Deploy/StateDeploy/CustomAction.cs
@@ -12,7 +12,7 @@ namespace StateDeploy
 
             string installDir = session["INSTALLDIR"];
             string projectName = session["PROJECT_NAME"];
-            string deployCmd = string.Format("state deploy --path {0} {1}", installDir, projectName);
+            string deployCmd = string.Format("state deploy --force --path {0} {1}", installDir, projectName);
 
             session.Log(string.Format("Executing deploy command: {0}", deployCmd));
             try
@@ -23,6 +23,7 @@ namespace StateDeploy
                 // The following commands are needed to redirect the standard output.
                 // This means that it will be redirected to the Process.StandardOutput StreamReader.
                 procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.RedirectStandardError = true;
                 procStartInfo.UseShellExecute = false;
                 // Do not create the black window.
                 procStartInfo.CreateNoWindow = true;
@@ -30,7 +31,8 @@ namespace StateDeploy
                 System.Diagnostics.Process proc = new System.Diagnostics.Process();
                 proc.StartInfo = procStartInfo;
                 proc.Start();
-                session.Log(proc.StandardOutput.ReadToEnd());
+                session.Log(string.Format("Standard output: {0}", proc.StandardOutput.ReadToEnd()));
+                session.Log(string.Format("Standard error: {0}", proc.StandardError.ReadToEnd()));
             }
             catch (Exception objException)
             {
