d23d3395f9c20e430b210967a3ea2ddc5b2b9535
Daved <daved@codemodus.com>
2020-05-18 09:54:57 -0700

    Add NumberIsProduction test to version pkg

--- internal/constants/version/version_test.go
+++ internal/constants/version/version_test.go
@@ -303,3 +303,25 @@ func TestService_IncrementVersionPreRelease(t *testing.T) {
 		})
 	}
 }
+
+func TestNumberIsProduction(t *testing.T) {
+	tests := []struct {
+		num  string
+		want bool
+	}{
+		{"0.0.0", false},
+		{"0.0.1", true},
+		{"0.1.0", true},
+		{"1.0.0", true},
+		{"1.1.0", true},
+		{"0.1.1", true},
+		{"junk", false},
+	}
+
+	for _, tt := range tests {
+		got := NumberIsProduction(tt.num)
+		if got != tt.want {
+			t.Errorf("%q: got %v, want %v", tt.num, got, tt.want)
+		}
+	}
+}
