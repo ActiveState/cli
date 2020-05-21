e64c1f66188079f521a9e4c0f8f5119e6360905c
Martin C Drohmann <martind@activestate.com>
2020-05-21 08:51:33 -0700

    update warning message

--- public/install.ps1
+++ public/install.ps1
@@ -171,7 +171,7 @@ function activateIfRequested() {
 
 function warningIfadmin() {
     if (IsAdmin) {
-        Write-Warning "It's recommended that you close this command prompt and start a new one without admin privileges.`n"
+        Write-Warning "It is recommended to use the state tool in a new terminal session without admin privileges.`n"
     }
 }
 
