d92f5f2a3f1c2709d0ce2f7cb4358ec8903c2f07
mdrakos <mike.drakos@gmail.com>
2020-05-29 09:50:31 -0700

    Don't escape env on Windows

--- internal/subshell/cmd/cmd.go
+++ internal/subshell/cmd/cmd.go
@@ -93,9 +93,8 @@ func (v *SubShell) Quote(value string) string {
 
 // Activate - see subshell.SubShell
 func (v *SubShell) Activate() *failures.Failure {
-	env := sscommon.EscapeEnv(v.env)
 	var fail *failures.Failure
-	if v.rcFile, fail = sscommon.SetupProjectRcFile("config.bat", ".bat", env); fail != nil {
+	if v.rcFile, fail = sscommon.SetupProjectRcFile("config.bat", ".bat", v.env); fail != nil {
 		return fail
 	}
 
