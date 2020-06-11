3d54dd098778624c2efbc5827cfb87948d104871
Martin C Drohmann <martind@activestate.com>
2020-06-11 13:35:27 -0700

    re-add grabbing of stdout / stderr during deploy

--- public/wix/Deploy/StateDeploy/CustomAction.cs
+++ public/wix/Deploy/StateDeploy/CustomAction.cs
@@ -7,6 +7,10 @@ namespace StateDeploy
 {
     public class CustomActions
     {
+
+        private static StringBuilder output = new StringBuilder();
+        private static StringBuilder error = new StringBuilder();
+
         [CustomAction]
         public static ActionResult StateDeploy(Session session)
         {
@@ -32,7 +36,7 @@ namespace StateDeploy
                 // and strerr output. Once we have a non-interactive mode in the State Tool these lines
                 // can be enabled
                 procStartInfo.RedirectStandardOutput = true;
-                //procStartInfo.RedirectStandardError = true;
+                procStartInfo.RedirectStandardError = true;
 
                 procStartInfo.UseShellExecute = false;
                 // Do not create the black window.
@@ -40,15 +44,42 @@ namespace StateDeploy
 
                 Process proc = new Process();
                 proc.StartInfo = procStartInfo;
+                proc.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+                {
+                    // Prepend line numbers to each line of the output.
+                    if (!String.IsNullOrEmpty(e.Data))
+                    {
+                        session.Log("out: " + e.Data);
+                        output.Append("\n" + e.Data);
+                    }
+                });
+                proc.ErrorDataReceived += new DataReceivedEventHandler((sender, e) =>
+                {
+                    // Prepend line numbers to each line of the output.
+                    if (!String.IsNullOrEmpty(e.Data))
+                    {
+                        session.Log("err: " + e.Data);
+                        // error.Append("\n" + e.Data);
+                    }
+                });
+
+
                 proc.Start();
 
+                
+                // Asynchronously read the standard output and standard error of the spawned process.
+                // This raises OutputDataReceived/ErrorDataReceived events for each line of output/errors.
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
+                session.Log("waiting for exit");
                 while (!proc.HasExited)
                 {
                     try
                     {
                         Status.ProgressBar.Increment(session, 0);
                         System.Threading.Thread.Sleep(200);
-                    } catch (InstallCanceledException)
+                    }
+                    catch (InstallCanceledException)
                     {
                         session.Log("Caught install canceled exception");
 
@@ -70,17 +101,25 @@ namespace StateDeploy
                         return ActionResult.UserExit;
                     }
                 }
-                
+
+
+                proc.WaitForExit();
+                session.Log("done");
+
                 // NOTE: See comment above re: progress bar. Can enable these lines once State Tool
                 // is updated
-                session.Log(string.Format("Standard output: {0}", proc.StandardOutput.ReadToEnd()));
-                //session.Log(string.Format("Standard error: {0}", proc.StandardError.ReadToEnd()));
+                session.Log(string.Format("Standard output: {0}", output.ToString()));
+                session.Log(string.Format("Standard error: {0}", error.ToString()));
+
+                proc.WaitForExit();
 
                 if (proc.ExitCode != 0)
                 {
                     session.Log(string.Format("Process exited with code: {0}", proc.ExitCode));
                     return ActionResult.Failure;
                 }
+
+                proc.Close();
             }
             catch (Exception objException)
             {
