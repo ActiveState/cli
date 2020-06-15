a25882f8e76ee38cbf749868f0637e3aa90228c6
mdrakos <mike.drakos@gmail.com>
2020-06-15 15:19:33 -0700

    Update licence disclaimer

--- assets/scripts/setenv.bat
+++ assets/scripts/setenv.bat
@@ -1,4 +1,4 @@
-:: Copyright 2016 Alan Shreve github@inconshreveable.
+:: Copyright 2019 Chocolatey github@chocolatey.
 
 :: Licensed under the Apache License, Version 2.0 (the "License");
 :: you may not use this file except in compliance with the License.
