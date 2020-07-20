8cadc759fb690fd286b556eb941985dcfa5550b9
Nathan Rijksen <n.rijksen@gmail.com>
2020-07-20 15:21:35 -0700

    Add X-Requestor header

--- pkg/platform/api/api.go
+++ pkg/platform/api/api.go
@@ -41,6 +41,7 @@ type RoundTripper struct{}
 // RoundTrip executes a single HTTP transaction, returning a Response for the provided Request.
 func (r *RoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Set("User-Agent", r.UserAgent())
+	req.Header.Set("X-Requestor", logging.UniqID())
 	return http.DefaultTransport.RoundTrip(req)
 }
 
