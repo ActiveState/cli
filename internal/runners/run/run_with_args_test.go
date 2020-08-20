edebab9bf81dde7aa2cee13d3b92fd7c7223e83f
mdrakos <mike.drakos@gmail.com>
2020-08-20 12:38:53 -0700

    Fix args test

--- internal/runners/run/run_with_args_test.go
+++ internal/runners/run/run_with_args_test.go
@@ -48,7 +48,7 @@ scripts:
 project: "https://platform.activestate.com/ActiveState/project?commitID=00010001-0001-0001-0001-000100010001"
 scripts:
   - name: %s
-    languages: [bash]
+    languages: [batch]
     standalone: true
     value: |
       echo "ARGS|%%1|%%2|%%3|%%4|"`, cmdName)
