c27b21731a905be7d850e27182c50503a0de4591
Mike Drakos <miked@activestate.com>
2020-08-25 09:55:12 -0700

    Improve deprecation warning
    Co-authored-by: Nathan Rijksen <nathanr@activestate.com>

--- internal/runners/run/run.go
+++ internal/runners/run/run.go
@@ -141,7 +141,7 @@ func run(out output.Outputer, subs subshell.SubShell, name string, args []string
 	if lang == language.Unset {
 		warning := locale.Tl(
 			"run_warn_deprecated_script_without_language",
-			"[YELLOW]DEPRECATION WARNING: Scripts without defined language currently fall back to using the default shell for your platform. This fallback mechanic will soon stop working and a language will need to be explicitly defined for each script. Please configure the 'language' field with a valid option (one of {{.V0}})[/RESET]",
+			"[YELLOW]DEPRECATION WARNING: Scripts without a defined language currently fall back to using the default shell for your platform. This fallback mechanic will soon stop working and a language will need to be explicitly defined for each script. Please configure the 'language' field with a valid option (one of {{.V0}})[/RESET]",
 			strings.Join(language.RecognizedNames(), ", "),
 		)
 		out.Notice(warning)
