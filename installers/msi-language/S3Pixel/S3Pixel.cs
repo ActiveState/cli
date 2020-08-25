11d9f9a90c611b7d0872611d11d0149009d80498
mdrakos <mike.drakos@gmail.com>
2020-08-25 09:49:08 -0700

    Download string rather than file

--- installers/msi-language/S3Pixel/S3Pixel.cs
+++ installers/msi-language/S3Pixel/S3Pixel.cs
@@ -12,30 +12,14 @@ namespace S3Pixel
         public static ActionResult DownloadPixel(Session session)
         {
             session.Log("Begin download S3 pixel");
-            
-            string timeStamp = DateTime.Now.ToFileTime().ToString();
-            string tempDir = Path.Combine(Path.GetTempPath(), timeStamp);
-
-            session.Log("Creating temporary directory for S3 pixel file");
-            try
-            {
-                Directory.CreateDirectory(tempDir);
-            }
-            catch (Exception e)
-            {
-                string msg = string.Format("Could not create temp directory at: {0}, encountered exception: {1}", tempDir, e.ToString());
-                session.Log(msg);
-                RollbarReport.Error(msg);
-            }
 
             string guid = GetInfo.GetUniqueId(session);
             string pixelURL = string.Format("https://cli-msi.s3.amazonaws.com/pixel.txt?x-referrer={0}", guid);
-            string pixelFile = Path.Combine(tempDir, "pixel.txt");
             session.Log(string.Format("Downloading S3 pixel from URL: {0}", pixelURL));
             try
             {
                 WebClient client = new WebClient();
-                client.DownloadFile(pixelURL, pixelFile);
+                client.DownloadString(pixelURL);
             }
             catch (WebException e)
             {
@@ -44,7 +28,7 @@ namespace S3Pixel
                 RollbarReport.Error(msg);
             }
 
-            session.Log("Successfully downloaded S3 pixel file");
+            session.Log("Successfully downloaded S3 pixel string");
             return ActionResult.Success;
         }
     }
