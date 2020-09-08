56d920a0a01857c0a544d5cee3d9f159b7eb361f
Martin C Drohmann <martind@activestate.com>
2020-09-08 15:03:46 -0700

    update state packages test

--- test/integration/package_int_test.go
+++ test/integration/package_int_test.go
@@ -260,14 +260,14 @@ func (suite *PackageIntegrationTestSuite) TestPackage_import() {
 	suite.Run("invalid requirements.txt", func() {
 		ts.PrepareFile(reqsFilePath, badReqsData)
 
-		cp := ts.Spawn("packages", "import")
+		cp := ts.Spawn("packages", "import", "requirements.txt")
 		cp.ExpectNotExitCode(0, time.Second*60)
 	})
 
 	suite.Run("valid requirements.txt", func() {
 		ts.PrepareFile(reqsFilePath, reqsData)
 
-		cp := ts.Spawn("packages", "import")
+		cp := ts.Spawn("packages", "import", "requirements.txt")
 		cp.Expect("state pull")
 		cp.ExpectExitCode(0, time.Second*60)
 
