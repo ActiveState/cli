f6a47b19c5d67851cb12bdc9e46027c35dbbf9d9
Martin C Drohmann <martind@activestate.com>
2020-09-11 23:28:36 -0700

    test relocateFile

--- pkg/platform/runtime/envdef/file_transform_test.go
+++ pkg/platform/runtime/envdef/file_transform_test.go
@@ -1,7 +1,53 @@
 package envdef
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/autarch/testify/assert"
+)
 
 func TestRelocateFile(t *testing.T) {
+	nullCharacter := "\u0000"
+	invalidPad := "too long"
+	cases := []struct {
+		Name        string
+		Replacement string
+		PadWith     *string
+		HasError    bool
+		Expected    string
+	}{
+		{
+			"null-padded", "/ghi", &nullCharacter, false,
+			"/ghi/something/else\u0000\u0000\u0000\u0000",
+		},
+		{
+			"text-only", "/ghi", nil, false,
+			"/ghi/something/else\u0000",
+		},
+		{
+			"invalid-padding", "/ghi", &invalidPad, true, "",
+		},
+		{
+			"replacement-too-long", "/too-long", &nullCharacter, true, "",
+		},
+	}
 
+	for _, c := range cases {
+
+		t.Run(c.Name, func(tt *testing.T) {
+			ft := &FileTransform{
+				Pattern: "/abcdef",
+				With:    c.Replacement,
+				PadWith: c.PadWith,
+			}
+
+			res, err := ft.relocateFile([]byte("/abcdef/something/else\u0000"), c.Replacement)
+			if c.HasError != (err != nil) {
+				tt.Fatalf("relocateFile returned with err: %v", err)
+			}
+			if err == nil {
+				assert.Equal(tt, res, []byte(c.Expected))
+			}
+		})
+	}
 }
