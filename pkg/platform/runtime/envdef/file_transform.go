a0085935e93f2d32e7617058f7a2d3b763bcceee
Martin C Drohmann <martind@activestate.com>
2020-09-11 23:02:13 -0700

    remove debug code

--- pkg/platform/runtime/envdef/file_transform.go
+++ pkg/platform/runtime/envdef/file_transform.go
@@ -34,7 +34,6 @@ type ConstTransform struct {
 func (ft *FileTransform) applyConstTransforms(constants Constants) (Constants, error) {
 	cs := constants
 	for _, ct := range ft.ConstTransforms {
-		fmt.Printf("pattern=%q\n", ct.Pattern)
 		r, err := regexp.Compile(regexp.QuoteMeta(ct.Pattern))
 		if err != nil {
 			return cs, errs.Wrap(err, "Failed to compile regexp pattern in const_transform.")
@@ -101,7 +100,6 @@ func (ft *FileTransform) ApplyTransform(baseDir string, constants Constants) err
 	// compute transformed constants
 	tcs, err := ft.applyConstTransforms(constants)
 	if err != nil {
-		fmt.Printf("%v\n", err)
 		return errs.Wrap(err, "Failed to apply the constant transformation to replacement text.")
 	}
 	replacement := expandConstants(ft.With, tcs)
