a2688484a8519dc88f556e8d42bdbb56fa893f4d
Daved <daved@codemodus.com>
2020-09-15 17:56:22 -0700

    Revert to failures in download manager logic

--- internal/download/manager.go
+++ internal/download/manager.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"path/filepath"
 
+	"github.com/ActiveState/cli/internal/failures"
 	"github.com/ActiveState/cli/internal/fileutils"
 	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/logging"
@@ -13,7 +14,7 @@ import (
 // Manager is our main download manager, it takes care of processing the downloads and communicating progress
 type Manager struct {
 	WorkerCount int
-	err         error
+	failure     *failures.Failure
 	entries     []*Entry
 	progress    *progress.Progress
 }
@@ -26,7 +27,7 @@ type Entry struct {
 }
 
 // Download will start the download progress and blocks until the progress completes
-func (m *Manager) Download() error {
+func (m *Manager) Download() *failures.Failure {
 	jobs := make(chan *Entry, len(m.entries))
 	done := make(chan bool, m.WorkerCount)
 
@@ -53,32 +54,31 @@ func (m *Manager) Download() error {
 		<-done
 	}
 
-	return m.err
+	return m.failure
 }
 
 // Job runs an individual download job
 func (m *Manager) Job(entry *Entry) {
-	if m.err != nil {
+	if m.failure != nil {
 		return
 	}
 
 	bytes, err := GetWithProgress(entry.Download, m.progress)
 	if err != nil {
-		m.err = err
+		m.failure = failures.FailIO.Wrap(err)
 		logging.Debug("error occured: %v", err)
 		return
 	}
 
 	dirname := filepath.Dir(entry.Path)
-	fail := fileutils.MkdirUnlessExists(dirname)
-	m.err = fail.ToError()
-	if m.err != nil {
+	m.failure = fileutils.MkdirUnlessExists(dirname)
+	if m.failure != nil {
 		return
 	}
 
 	err = ioutil.WriteFile(entry.Path, bytes, 0666)
 	if err != nil {
-		m.err = err
+		m.failure = failures.FailIO.Wrap(err)
 	}
 }
 
