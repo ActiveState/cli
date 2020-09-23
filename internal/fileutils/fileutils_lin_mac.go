b75d177db4be815d928b231162081ad6e5766089
mdrakos <mike.drakos@gmail.com>
2020-09-23 15:13:23 -0700

    Fix syntax error

--- internal/fileutils/fileutils_lin_mac.go
+++ internal/fileutils/fileutils_lin_mac.go
@@ -25,6 +25,6 @@ func IsWritable(path string) bool {
 
 // GetLongPath name returns the Windows long path (ie. ~1 notation is expanded)
 // This function does not alter the path in any way on Linux or MacOS
-func GetLongPathName(path string) (string error) {
+func GetLongPathName(path string) (string, error) {
 	return path, nil
 }
