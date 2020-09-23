fa935474e09e8fe0ecbb30911a9b7ddd9fd8e572
mdrakos <mike.drakos@gmail.com>
2020-09-23 16:29:54 -0700

    Fix syntax error

--- internal/testhelpers/osutil/path_helpers_lin_mac.go
+++ internal/testhelpers/osutil/path_helpers_lin_mac.go
@@ -4,6 +4,6 @@ package osutil
 
 // GetLongPath name returns the Windows long path (ie. ~1 notation is expanded)
 // This function does not alter the path in any way on Linux or MacOS
-func GetLongPathName(path string) (string error) {
+func GetLongPathName(path string) (string, error) {
 	return path, nil
 }
