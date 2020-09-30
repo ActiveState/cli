91a0cd5702bf6a134c9fe226d718fe231b1ef243
Martin C Drohmann <martind@activestate.com>
2020-09-30 15:08:26 -0700

    compute cid based on MAC address and Windows Product Key

--- installers/msi-language/GAPixel/GAPixelCA.cs
+++ installers/msi-language/GAPixel/GAPixelCA.cs
@@ -21,12 +21,74 @@ namespace GAPixel
             }
         }
 
+        public static string DecodeProductKeyWin8AndUp(byte[] digitalProductId)
+        {
+            var key = String.Empty;
+            const int keyOffset = 52;
+            var isWin8 = (byte)((digitalProductId[66] / 6) & 1);
+            digitalProductId[66] = (byte)((digitalProductId[66] & 0xf7) | (isWin8 & 2) * 4);
+
+            // Possible alpha-numeric characters in product key.
+            const string digits = "BCDFGHJKMPQRTVWXY2346789";
+            int last = 0;
+            for (var i = 24; i >= 0; i--)
+            {
+                var current = 0;
+                for (var j = 14; j >= 0; j--)
+                {
+                    current = current * 256;
+                    current = digitalProductId[j + keyOffset] + current;
+                    digitalProductId[j + keyOffset] = (byte)(current / 24);
+                    current = current % 24;
+                    last = current;
+                }
+                key = digits[current] + key;
+            }
+            var keypart1 = key.Substring(1, last);
+            const string insert = "N";
+            key = key.Substring(1).Replace(keypart1, keypart1 + insert);
+            if (last == 0)
+                key = insert + key;
+            for (var i = 5; i < key.Length; i += 6)
+            {
+                key = key.Insert(i, "-");
+            }
+            return key;
+        }
+
+        public static string GetWindowsProductKey(Session session=null)
+        {
+            try
+            {
+                const string keyPath = @"Software\Microsoft\Windows NT\CurrentVersion";
+                var digitalProductId = (byte[])Registry.LocalMachine.OpenSubKey(keyPath).GetValue("DigitalProductId");
+
+                var isWin8OrUp =
+                    (Environment.OSVersion.Version.Major == 6 && System.Environment.OSVersion.Version.Minor >= 2)
+                    ||
+                    (Environment.OSVersion.Version.Major > 6);
+
+                var productKey = isWin8OrUp ? DecodeProductKeyWin8AndUp(digitalProductId) : "windows < 8";
+                session.Log("Successfully decoded windows product key {0}", productKey);
+                return productKey;
+            }
+            catch (Exception err)
+            {
+                if (session != null)
+                {
+                    session.Log("Error getting windows product key: {0}", err);
+                }
+                return "error";
+            }
+        }
+
         public static string GetUniqueId(Session session=null)
         {
             try
             {
                 var oMClass = new ManagementClass("Win32_NetworkAdapterConfiguration");
                 var colMObj = oMClass.GetInstances();
+                var productKey = GetWindowsProductKey(session);
                 foreach (var objMO in colMObj)
                 {
                     try
@@ -37,7 +99,7 @@ namespace GAPixel
                             continue;
                         }
                         // return on first found MAC address
-                        return StringToGuid(macAddress.ToString()).ToString();
+                        return StringToGuid(macAddress.ToString() + productKey).ToString();
                     }
                     catch (NullReferenceException)
                     {
