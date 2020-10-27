be2490d375557243aadd8c328e36cce93f6c13ee
Daved <daved@codemodus.com>
2020-10-27 15:44:11 -0700

    Add sketch of before/after event targeting/execution

--- internal/runners/run/event.go
+++ internal/runners/run/event.go
@@ -17,6 +17,9 @@ func NewEvent(events []*project.Event) *Event {
 }
 
 func (es *Event) Run(args []string, t project.EventType) error {
+	// filter events by type (without mutating DefinedEvents)
+	// filter events by args
+	// run scripts that remain
 	fmt.Println(t)
 	return nil
 }
