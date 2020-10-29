023845c57495a7109edc1119ad8548f425f6934d
mdrakos <mike.drakos@gmail.com>
2020-10-29 14:00:21 -0700

    Update integration tests

--- test/integration/platforms_int_test.go
+++ test/integration/platforms_int_test.go
@@ -1,6 +1,8 @@
 package integration
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/ActiveState/cli/internal/testhelpers/e2e"
@@ -56,7 +58,7 @@ func (suite *PlatformsIntegrationTestSuite) TestPlatforms_listSimple() {
 	}
 }
 
-func (suite *PlatformsIntegrationTestSuite) TestPlatforms_addRemoveSimple() {
+func (suite *PlatformsIntegrationTestSuite) TestPlatforms_addRemove() {
 	suite.OnlyRunForTags(tagsuite.Platforms)
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
@@ -72,10 +74,68 @@ func (suite *PlatformsIntegrationTestSuite) TestPlatforms_addRemoveSimple() {
 	platform := "Windows"
 	version := "10.0.17134.1"
 
-	cp := ts.Spawn("platforms", "add", platform, version)
+	cp := ts.Spawn("platforms", "add", fmt.Sprintf("%s@%s", platform, version))
 	cp.ExpectExitCode(0)
+
+	cp = ts.Spawn("platforms")
+	expectations := []string{
+		platform,
+		version,
+		"64",
+	}
+	for _, expectation := range expectations {
+		cp.Expect(expectation)
+	}
+
 	cp = ts.Spawn("platforms", "remove", platform, version)
 	cp.ExpectExitCode(0)
+
+	cp = ts.Spawn("platforms")
+	cp.ExpectExitCode(0)
+	output := cp.TrimmedSnapshot()
+	if strings.Contains(output, "Windows") {
+		suite.T().Fatal("Windows platform should not be present after removal")
+	}
+}
+
+func (suite *PlatformsIntegrationTestSuite) TestPlatforms_addRemoveLatest() {
+	suite.OnlyRunForTags(tagsuite.Platforms)
+	ts := e2e.New(suite.T(), false)
+	defer ts.Close()
+
+	suite.PrepareActiveStateYAML(ts)
+
+	ts.LoginAsPersistentUser()
+	defer func() {
+		cp := ts.Spawn("auth", "logout")
+		cp.ExpectExitCode(0)
+	}()
+
+	platform := "Windows"
+	version := "10.0.17134.1"
+
+	cp := ts.Spawn("platforms", "add", "windows")
+	cp.ExpectExitCode(0)
+
+	cp = ts.Spawn("platforms")
+	expectations := []string{
+		platform,
+		version,
+		"64",
+	}
+	for _, expectation := range expectations {
+		cp.Expect(expectation)
+	}
+
+	cp = ts.Spawn("platforms", "remove", platform, version)
+	cp.ExpectExitCode(0)
+
+	cp = ts.Spawn("platforms")
+	cp.ExpectExitCode(0)
+	output := cp.TrimmedSnapshot()
+	if strings.Contains(output, "Windows") {
+		suite.T().Fatal("Windows platform should not be present after removal")
+	}
 }
 
 func (suite *PlatformsIntegrationTestSuite) PrepareActiveStateYAML(ts *e2e.Session) {
