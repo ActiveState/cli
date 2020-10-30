3d1c965e031fbdfa81cd65f9e4cbce440396e1e9
Nathan Rijksen <n.rijksen@gmail.com>
2020-10-30 14:56:25 -0700

    projectfile.Create is doing too much, try to clean it up as much as reasonable

--- internal/runners/initialize/init.go
+++ internal/runners/initialize/init.go
@@ -46,24 +46,22 @@ func New(prime primeable) *Initialize {
 	return &Initialize{prime.Output(), prime.Project()}
 }
 
-func sanitize(params *RunParams, isheadless bool) error {
-	if !isheadless {
-		if params.Language == "" {
-			// Manually check for language requirement, because we need to fallback on the --language flag to support editor.V0
-			return locale.NewInputError("err_init_no_language", "You need to supply the [NOTICE]language[/RESET] argument, run `[ACTIONABLE]state init --help[/RESET]` for more information.")
-		}
-		langParts := strings.Split(params.Language, "@")
-		if len(langParts) > 1 {
-			params.version = langParts[1]
-		}
+func sanitize(params *RunParams) error {
+	if params.Language == "" {
+		// Manually check for language requirement, because we need to fallback on the --language flag to support editor.V0
+		return locale.NewInputError("err_init_no_language", "You need to supply the [NOTICE]language[/RESET] argument, run `[ACTIONABLE]state init --help[/RESET]` for more information.")
+	}
+	langParts := strings.Split(params.Language, "@")
+	if len(langParts) > 1 {
+		params.version = langParts[1]
+	}
 
-		params.language = language.Supported{language.MakeByName(langParts[0])}
-		if !params.language.Recognized() {
-			return language.NewUnrecognizedLanguageError(
-				params.language.String(),
-				language.RecognizedSupportedsNames(),
-			)
-		}
+	params.language = language.Supported{language.MakeByName(langParts[0])}
+	if !params.language.Recognized() {
+		return language.NewUnrecognizedLanguageError(
+			params.language.String(),
+			language.RecognizedSupportedsNames(),
+		)
 	}
 
 	// Fail if target dir already has an activestate.yaml
@@ -77,20 +75,6 @@ func sanitize(params *RunParams, isheadless bool) error {
 
 	if !styleRecognized(params.Style) {
 		params.Style = SkeletonBase
-	}
-
-	if fail := params.Namespace.Validate(); fail != nil {
-		return locale.WrapInputError(fail.ToError(), "init_invalid_namespace_err", "The provided namespace argument is invalid.")
-	}
-
-	if params.Path == "" {
-		var wd string
-		wd, err := osutils.Getwd()
-		if err != nil {
-			return err
-		}
-
-		params.Path = wd
 	}
 
 	return nil
@@ -117,6 +101,10 @@ func (r *Initialize) Run(params *RunParams) error {
 }
 
 func run(params *RunParams, out output.Outputer, proj *project.Project) (string, error) {
+	if fail := params.Namespace.Validate(); fail != nil {
+		return "", locale.WrapInputError(fail.ToError(), "init_invalid_namespace_err", "The provided namespace argument is invalid.")
+	}
+
 	if err := sanitizePath(params); err != nil {
 		return "", locale.WrapInputError(err, "err_init_sanitize_path", "Could not prepare path: {{.V0}}", err.Error())
 	}
@@ -131,11 +119,6 @@ func run(params *RunParams, out output.Outputer, proj *project.Project) (string,
 
 	isHeadless := proj != nil && proj.IsHeadless()
 
-	// Sanitize rest of params
-	if err := sanitize(params, isHeadless); err != nil {
-		return "", err
-	}
-
 	_, err := fileutils.PrepareDir(params.Path)
 	if err != nil {
 		return "", locale.WrapError(err, "err_init_preparedir", "Could not create directory at [NOTICE]{{.V0}}[/RESET]. Error: {{.V1}}", params.Path, err.Error())
@@ -143,28 +126,32 @@ func run(params *RunParams, out output.Outputer, proj *project.Project) (string,
 
 	logging.Debug("Init: %s/%s %v", params.Namespace.Owner, params.Namespace.Project, params.Private)
 
-	createParams := &projectfile.CreateParams{
-		Owner:           params.Namespace.Owner,
-		Project:         params.Namespace.Project,
-		Language:        params.language.String(),
-		LanguageVersion: params.version,
-		Directory:       params.Path,
-		Private:         params.Private,
-	}
+	if isHeadless {
+		proj.Source().SetNamespace(params.Namespace.Owner, params.Namespace.Project)
+	} else {
+		// Sanitize rest of params
+		if err := sanitize(params); err != nil {
+			return "", err
+		}
 
-	if proj.CommitUUID() != "" {
-		cid := proj.CommitUUID()
-		createParams.CommitID = &cid
-	}
+		createParams := &projectfile.CreateParams{
+			Owner:           params.Namespace.Owner,
+			Project:         params.Namespace.Project,
+			Language:        params.language.String(),
+			LanguageVersion: params.version,
+			Directory:       params.Path,
+			Private:         params.Private,
+		}
 
-	if params.Style == SkeletonEditor {
-		box := packr.NewBox("../../../assets/")
-		createParams.Content = box.String("activestate.yaml.editor.tpl")
-	}
+		if params.Style == SkeletonEditor {
+			box := packr.NewBox("../../../assets/")
+			createParams.Content = box.String("activestate.yaml.editor.tpl")
+		}
 
-	fail = projectfile.Create(createParams)
-	if fail != nil {
-		return "", fail
+		fail = projectfile.Create(createParams)
+		if fail != nil {
+			return "", fail
+		}
 	}
 
 	out.Notice(locale.Tr(
