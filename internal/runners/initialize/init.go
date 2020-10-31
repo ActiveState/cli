ca20b5b36f70263a6c87213fc9c5d17b09ade6da
Martin C Drohmann <martind@activestate.com>
2020-10-30 21:12:45 -0700

    set commit id when converting headless commit with `state init`

--- internal/runners/initialize/init.go
+++ internal/runners/initialize/init.go
@@ -125,7 +125,9 @@ func run(params *RunParams, out output.Outputer) (string, error) {
 	logging.Debug("Init: %s/%s %v", params.Namespace.Owner, params.Namespace.Project, params.Private)
 
 	if isHeadless {
+		commitID := proj.CommitID()
 		proj.Source().SetNamespace(params.Namespace.Owner, params.Namespace.Project)
+		proj.Source().SetCommit(commitID, false)
 	} else {
 		// Sanitize rest of params
 		if err := sanitize(params); err != nil {
