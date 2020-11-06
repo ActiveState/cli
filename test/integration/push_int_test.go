c57836580d0e31c809d9b121e1102123f175e318
Nathan Rijksen <n.rijksen@gmail.com>
2020-11-05 17:07:15 -0800

    Accept any exit code

--- test/integration/push_int_test.go
+++ test/integration/push_int_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"path/filepath"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/suite"
 
@@ -80,9 +79,7 @@ func (suite *PushIntegrationTestSuite) TestCarlisle() {
 	cp.Expect("You're about to add packages as an anonymous user")
 	cp.Expect("(Y/n)")
 	cp.SendLine("y")
-	time.Sleep(500 * time.Millisecond)
-	cp.SendCtrlC()
-	cp.ExpectNotExitCode(0)
+	cp.Wait()
 
 	prj, fail := project.FromPath(filepath.Join(wd, constants.ConfigFileName))
 	suite.Require().NoError(fail.ToError(), "Could not parse project file")
