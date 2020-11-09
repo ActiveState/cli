13a969c471d6cde66e7e02403bb7049817967baa
Nathan Rijksen <n.rijksen@gmail.com>
2020-11-09 13:59:30 -0800

    Small styling changes

--- cmd/state/main.go
+++ cmd/state/main.go
@@ -182,7 +182,7 @@ func run(args []string, out output.Outputer) (int, error) {
 		if childCmd != nil {
 			cmdName = childCmd.Use() + " "
 		}
-		err = errs.AddTips(err, locale.Tl("err_tip_run_help", "Run → [ACTIONABLE]state {{.V0}}--help[/RESET]", cmdName))
+		err = errs.AddTips(err, locale.Tl("err_tip_run_help", "Run → [ACTIONABLE]`state {{.V0}}--help`[/RESET] for general help", cmdName))
 	}
 
 	return unwrapError(err)
