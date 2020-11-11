d418f31cb1261a99e40c15a715a45c02445a94a0
Martin C Drohmann <martind@activestate.com>
2020-11-11 15:18:24 -0800

    ignore ExitCode errors during state activate session

--- internal/errs/unwrap.go
+++ internal/errs/unwrap.go
@@ -6,13 +6,29 @@ import (
 	"github.com/ActiveState/cli/internal/failures"
 )
 
+func unwrapExitError(err error) (bool, int) {
+	var eerr interface{ ExitCode() int }
+	isExitError := errors.As(err, &eerr)
+	if isExitError {
+		// If exit error happened in activated shell, do not forward exit code, but return 0
+		var activatedErr interface{ IsFromActivatedShell() }
+		isFromActivatedShell := errors.As(err, &activatedErr)
+		if isFromActivatedShell {
+			return true, 0
+		}
+
+		return true, eerr.ExitCode()
+	}
+
+	return false, 0
+}
+
 // UnwrapExitCode checks if the given error is a failure of type FailExecCmdExit and
 // returns the ExitCode of the process that failed with this error
 func UnwrapExitCode(errFail error) int {
-	var eerr interface{ ExitCode() int }
-	isExitError := errors.As(errFail, &eerr)
+	isExitError, code := unwrapExitError(errFail)
 	if isExitError {
-		return eerr.ExitCode()
+		return code
 	}
 
 	// failure might be in the error stack
@@ -26,13 +42,9 @@ func UnwrapExitCode(errFail error) int {
 	}
 
 	err := fail.ToError()
-	isExitError = errors.As(err, &eerr)
+	isExitError, code = unwrapExitError(err)
 	if isExitError {
-		return eerr.ExitCode()
-	}
-
-	if fail.Type.Matches(failures.FailExecCmdExit) {
-		return 1
+		return code
 	}
 
 	return 1
--- internal/runners/activate/activation.go
+++ internal/runners/activate/activation.go
@@ -1,6 +1,7 @@
 package activate
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/signal"
@@ -18,6 +19,20 @@ import (
 	"github.com/ActiveState/cli/pkg/project"
 	"github.com/ActiveState/cli/pkg/projectfile"
 )
+
+type activationError struct {
+	error
+}
+
+func (ae activationError) IsFromActivatedShell() {}
+
+// Is Silent returns true if the wrapped error is an exit code error
+func (ae activationError) IsSilent() bool {
+	var eerr interface{ ExitCode() int }
+	return errors.As(ae, &eerr)
+}
+
+func (ae activationError) Unwrap() error { return ae.error }
 
 func (r *Activate) activateAndWait(proj *project.Project, venv *virtualenvironment.VirtualEnvironment) error {
 	logging.Debug("Activating and waiting")
@@ -69,7 +84,7 @@ func (r *Activate) activateAndWait(proj *project.Project, venv *virtualenvironme
 
 	err = <-r.subshell.Errors()
 	if err != nil {
-		return locale.WrapError(err, "error_in_active_subshell", "Failure encountered in active subshell")
+		return activationError{locale.WrapError(err, "error_in_active_subshell", "Failure encountered in active subshell")}
 	}
 
 	return nil
