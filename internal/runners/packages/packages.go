9a9e181ea33695bffebece1356ea5c4d258f3754
mdrakos <mike.drakos@gmail.com>
2020-11-16 13:14:20 -0800

    Match search string with feature names

--- internal/runners/packages/packages.go
+++ internal/runners/packages/packages.go
@@ -82,7 +82,7 @@ func executePackageOperation(pj *project.Project, out output.Outputer, authentic
 
 	// Check if this is an addition or an update
 	if operation == model.OperationAdded {
-		req, err := model.GetRequirement(pj.CommitUUID(), ingredient.Namespace, name)
+		req, err := model.GetRequirement(pj.CommitUUID(), ingredient.Namespace, *ingredient.Ingredient.Name)
 		if err != nil {
 			return errs.Wrap(err, "Could not get requirement")
 		}
@@ -92,7 +92,7 @@ func executePackageOperation(pj *project.Project, out output.Outputer, authentic
 	}
 
 	parentCommitID := pj.CommitUUID()
-	commitID, fail := model.CommitPackage(parentCommitID, operation, name, ingredient.Namespace, version)
+	commitID, fail := model.CommitPackage(parentCommitID, operation, *ingredient.Ingredient.Name, ingredient.Namespace, version)
 	if fail != nil {
 		return locale.WrapError(fail.ToError(), fmt.Sprintf("err_%s_%s", pt.String(), operation))
 	}
@@ -142,9 +142,9 @@ func executePackageOperation(pj *project.Project, out output.Outputer, authentic
 
 	// Print the result
 	if version != "" {
-		out.Print(locale.Tr(fmt.Sprintf("%s_version_%s", pt.String(), operation), name, version))
+		out.Print(locale.Tr(fmt.Sprintf("%s_version_%s", pt.String(), operation), *ingredient.Ingredient.Name, version))
 	} else {
-		out.Print(locale.Tr(fmt.Sprintf("%s_%s", pt.String(), operation), name))
+		out.Print(locale.Tr(fmt.Sprintf("%s_%s", pt.String(), operation), *ingredient.Ingredient.Name))
 	}
 
 	return nil
