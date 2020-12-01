5aac00092a913c585f467a1467d96e4c767f3e04
Daved <daved@codemodus.com>
2020-12-01 10:56:47 -0800

    Return error from languages runner if no commit id

--- internal/runners/languages/languages.go
+++ internal/runners/languages/languages.go
@@ -7,7 +7,6 @@ import (
 	"github.com/ActiveState/cli/internal/output"
 	"github.com/ActiveState/cli/pkg/platform/model"
 	"github.com/ActiveState/cli/pkg/project"
-	"github.com/go-openapi/strfmt"
 )
 
 // Languages manages the listing execution context.
@@ -43,43 +42,22 @@ func (l *Languages) Run() error {
 		return locale.NewInputError("err_no_project")
 	}
 
-	commitUUID, err := usableCommitUUID(l.project)
-	if err != nil {
-		return locale.WrapError(
-			err, "err_no_usable_commitid", "Cannot obtain a usable commit id",
+	commitUUID := l.project.CommitUUID()
+	if commitUUID == "" {
+		return locale.NewError(
+			"err_languages_no_commitid", "A CommitID is required to list languages",
 		)
 	}
 
 	langs, fail := model.FetchLanguagesForCommit(commitUUID)
 	if fail != nil {
-		return locale.WrapError(
-			fail, "err_fetching_languages",
-			"Cannot obtain languages for commit id {{.V0}}", commitUUID.String(),
-		)
+		return locale.WrapError(fail, "err_fetching_languages", "Cannot obtain languages")
 	}
 
 	formatLangs(langs)
 
 	l.out.Print(Listing{langs})
 	return nil
-}
-
-func usableCommitUUID(p *project.Project) (strfmt.UUID, error) {
-	commitUUID := p.CommitUUID()
-	if commitUUID == "" {
-		latestUUID, fail := model.LatestCommitID(p.Owner(), p.Name())
-		if fail != nil {
-			return "", fail.ToError()
-		}
-
-		if latestUUID == nil || *latestUUID == "" {
-			return "", locale.NewError("err_get_latest_commit_id")
-		}
-
-		commitUUID = *latestUUID
-	}
-
-	return commitUUID, nil
 }
 
 func formatLangs(langs []model.Language) {
