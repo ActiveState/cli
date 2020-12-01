14f9aa1c32bf7acee524aa6e71c56beabe92229d
Nathan Rijksen <n.rijksen@gmail.com>
2020-12-01 09:34:19 -0800

    Make test slightly more readable

--- pkg/platform/api/buildlogstream/streamer_test.go
+++ pkg/platform/api/buildlogstream/streamer_test.go
@@ -4,13 +4,34 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/ActiveState/cli/pkg/platform/api/inventory/inventory_models"
 	"github.com/autarch/testify/assert"
 	"github.com/go-openapi/strfmt"
+
+	"github.com/ActiveState/cli/pkg/platform/api/inventory/inventory_models"
 )
 
-func readableUUID(number int) strfmt.UUID {
+func intToUUID(number int) strfmt.UUID {
 	return strfmt.UUID(fmt.Sprintf("00000000-0000-0000-0000-%012d", number))
+}
+
+func intMapToUUIDMap(in map[int][]int) map[strfmt.UUID][]strfmt.UUID {
+	out := map[strfmt.UUID][]strfmt.UUID{}
+	for k := range in {
+		kk := intToUUID(k)
+		out[kk] = []strfmt.UUID{}
+		for _, vv := range in[k] {
+			out[kk] = append(out[kk], intToUUID(vv))
+		}
+	}
+	return out
+}
+
+func intsToArtifactMap(in []int) map[strfmt.UUID]ArtifactMapping {
+	out := map[strfmt.UUID]ArtifactMapping{}
+	for _, v := range in {
+		out[intToUUID(v)] = ArtifactMapping{}
+	}
+	return out
 }
 
 type depGraph map[int][]int
@@ -20,10 +41,10 @@ func depGraphsToResolvedIngredients(dgs depGraph) []*inventory_models.V1Solution
 
 	res := make([]*inventory_models.V1SolutionRecipeRecipeResolvedIngredientsItems, 0, len(dgs))
 	for d, dchildren := range dgs {
-		uuid := readableUUID(d)
+		uuid := intToUUID(d)
 		deps := make([]*inventory_models.V1SolutionRecipeRecipeResolvedIngredientsItemsDependenciesItems, 0, len(dchildren))
 		for _, dc := range dchildren {
-			duuid := readableUUID(dc)
+			duuid := intToUUID(dc)
 
 			deps = append(deps, &inventory_models.V1SolutionRecipeRecipeResolvedIngredientsItemsDependenciesItems{
 				IngredientVersionID: &duuid,
@@ -58,30 +79,21 @@ func TestFetchDepTree(t *testing.T) {
 		3: {31, 1, 39, 900},
 	}
 	ingredients := depGraphsToResolvedIngredients(dg)
-	ingredientMap := map[strfmt.UUID]ArtifactMapping{
-		readableUUID(1):   {},
-		readableUUID(2):   {},
-		readableUUID(3):   {},
-		readableUUID(11):  {},
-		readableUUID(12):  {},
-		readableUUID(900): {},
-		readableUUID(21):  {},
-		readableUUID(31):  {},
-	}
+	ingredientMap := intsToArtifactMap([]int{1, 2, 3, 11, 12, 900, 21, 31})
 
 	depTree, recursive := fetchDepTree(ingredients, ingredientMap)
 
-	expectedDirect := map[strfmt.UUID][]strfmt.UUID{
-		readableUUID(1): {readableUUID(11), readableUUID(12), readableUUID(2), readableUUID(900)},
-		readableUUID(2): {readableUUID(21), readableUUID(900)},
-		readableUUID(3): {readableUUID(31), readableUUID(1), readableUUID(900)},
-	}
+	expectedDirect := intMapToUUIDMap(map[int][]int{
+		1: {11, 12, 2, 900},
+		2: {21, 900},
+		3: {31, 1, 900},
+	})
 
-	expectedRecursive := map[strfmt.UUID][]strfmt.UUID{
-		readableUUID(1): {readableUUID(11), readableUUID(12), readableUUID(2), readableUUID(21), readableUUID(900)},
-		readableUUID(2): {readableUUID(21), readableUUID(900)},
-		readableUUID(3): {readableUUID(31), readableUUID(1), readableUUID(11), readableUUID(12), readableUUID(2), readableUUID(21), readableUUID(900)},
-	}
+	expectedRecursive := intMapToUUIDMap(map[int][]int{
+		1: {11, 12, 2, 21, 900},
+		2: {21, 900},
+		3: {31, 1, 11, 12, 2, 21, 900},
+	})
 
 	assert.Equal(t, expectedDirect, depTree)
 	assert.Equal(t, expectedRecursive, recursive)
@@ -93,22 +105,19 @@ func TestFetchRecursiveDepTree(t *testing.T) {
 		2: {1},
 	}
 	ingredients := depGraphsToResolvedIngredients(dg)
-	ingredientMap := map[strfmt.UUID]ArtifactMapping{
-		readableUUID(1): {},
-		readableUUID(2): {},
-	}
+	ingredientMap := intsToArtifactMap([]int{1, 2})
 
 	depTree, recursive := fetchDepTree(ingredients, ingredientMap)
 
-	expectedDirect := map[strfmt.UUID][]strfmt.UUID{
-		readableUUID(1): {readableUUID(2)},
-		readableUUID(2): {readableUUID(1)},
-	}
+	expectedDirect := intMapToUUIDMap(map[int][]int{
+		1: {2},
+		2: {1},
+	})
 
-	expectedRecursive := map[strfmt.UUID][]strfmt.UUID{
-		readableUUID(1): {readableUUID(2), readableUUID(1)},
-		readableUUID(2): {readableUUID(1), readableUUID(2)},
-	}
+	expectedRecursive := intMapToUUIDMap(map[int][]int{
+		1: {2, 1},
+		2: {1, 2},
+	})
 
 	assert.Equal(t, expectedDirect, depTree)
 	assert.Equal(t, expectedRecursive, recursive)
