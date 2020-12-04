083dc83a226477220f0615215cf55c788f8ac6cf
Martin C Drohmann <martind@activestate.com>
2020-12-04 12:08:58 -0800

    Add install.sh integration test

--- test/integration/install_scripts_int_test.go
+++ test/integration/install_scripts_int_test.go
@@ -1,11 +1,13 @@
 package integration
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/ActiveState/cli/internal/environment"
 	"github.com/ActiveState/cli/internal/fileutils"
@@ -68,6 +70,9 @@ func getEnvironmentPath(userScope bool) string {
 
 func scriptPath(t *testing.T) string {
 	name := "install.ps1"
+	if runtime.GOOS != "windows" {
+		name = "install.sh"
+	}
 	root := environment.GetRootPathUnsafe()
 	subdir := "installers"
 
@@ -81,6 +86,87 @@ func scriptPath(t *testing.T) string {
 
 type InstallScriptsIntegrationTestSuite struct {
 	tagsuite.Suite
+}
+
+func (suite *InstallScriptsIntegrationTestSuite) TestInstallSh() {
+	if runtime.GOOS == "windows" {
+		suite.T().SkipNow()
+	}
+	suite.OnlyRunForTags(tagsuite.Critical)
+
+	ts := e2e.New(suite.T(), false)
+	defer ts.Close()
+
+	script := scriptPath(suite.T())
+
+	cp := ts.SpawnCmdWithOpts("bash", e2e.WithArgs(script, "-t", ts.Dirs.Work))
+	cp.Expect("Installing to")
+	cp.Expect("Continue?")
+	cp.SendLine("y")
+	cp.Expect("Fetching the latest version")
+	cp.Expect("Allow $PATH to be appended in your")
+	cp.SendLine("n")
+	cp.Expect("State Tool Installed")
+	cp.ExpectExitCode(0)
+}
+
+func (suite *InstallScriptsIntegrationTestSuite) TestInstallPerl5_32() {
+	if runtime.GOOS != "linux" {
+		suite.T().SkipNow()
+	}
+	suite.OnlyRunForTags(tagsuite.Critical)
+
+	ts := e2e.New(suite.T(), false)
+	defer ts.Close()
+
+	script := scriptPath(suite.T())
+
+	cp := ts.SpawnCmdWithOpts(
+		"bash",
+		e2e.WithArgs("-c", fmt.Sprintf("cp $HOME/.bashrc %s/bashrc.bak", ts.Dirs.Work)),
+	)
+	cp.ExpectExitCode(0)
+
+	defer func() {
+		cp = ts.SpawnCmdWithOpts(
+			"bash",
+			e2e.WithArgs("-c", fmt.Sprintf("cp %s/.bashrc.bak $HOME/.bashrc", ts.Dirs.Work)),
+		)
+	}()
+
+	cp.ExpectExitCode(0)
+	cp = ts.SpawnCmdWithOpts(
+		"bash",
+		e2e.WithArgs(script, "-t", ts.Dirs.Work, "--activate-default", "ActiveState/Perl-5.32"),
+		e2e.AppendEnv("ACTIVESTATE_CLI_DISABLE_RUNTIME=false", "SHELL=bash"),
+	)
+	cp.Expect("Installing to")
+	cp.Expect("Continue?")
+	cp.SendLine("y")
+	cp.Expect("Fetching the latest version", 20*time.Second)
+	cp.Expect("Allow $PATH to be appended in your")
+	cp.SendLine("y")
+	cp.Expect("State Tool installation complete")
+	cp.Expect("Activating Virtual Environment")
+	cp.Expect("Choose Destination")
+	cp.Send("")
+	cp.Expect("Cloning Repository")
+	cp.Expect("Downloading missing artifacts")
+	cp.Expect("Updating missing artifacts", 20*time.Second)
+	cp.ExpectLongString("Successfully configured ActiveState/Perl-5.32 as the global default project")
+	cp.Expect("activated state")
+	cp.SendLine("exit")
+	cp.ExpectExitCode(0)
+
+	// we need to run an interactive bash session to ensure that the modified ~/.bashrc is being parsed
+	cp = ts.SpawnCmd("bash")
+	cp.SendLine("echo $PATH; exit")
+	// Expect Global Binary directory on PATH
+	globalBinDir := filepath.Join(ts.Dirs.Cache, "bin")
+	cp.ExpectLongString(globalBinDir, 1*time.Second)
+	// expect State Tool Installation directory
+	cp.ExpectLongString(ts.Dirs.Work, 1*time.Second)
+	cp.ExpectExitCode(0)
 }
 
 func (suite *InstallScriptsIntegrationTestSuite) TestInstallPs1() {
@@ -121,7 +207,7 @@ func (suite *InstallScriptsIntegrationTestSuite) TestInstallPs1() {
 	suite.Assert().Contains(paths, ts.Dirs.Work, "Could not find installation path in PATH")
 }
 
-func (suite *InstallScriptsIntegrationTestSuite) TestInstallPerl5_32() {
+func (suite *InstallScriptsIntegrationTestSuite) TestInstallPerl5_32_Windows() {
 	if runtime.GOOS != "windows" {
 		suite.T().SkipNow()
 	}
