dcb53800716f8babfc96fa4eba594702e04018b0
Nathan Rijksen <n.rijksen@gmail.com>
2020-12-15 11:06:35 -0800

    Allow sourcing environment on `state install`

--- test/integration/push_int_test.go
+++ test/integration/push_int_test.go
@@ -113,7 +113,10 @@ func (suite *PushIntegrationTestSuite) TestCarlisle() {
 
 	// anonymous commit
 	wd := filepath.Join(cp.WorkDirectory(), namespace)
-	cp = ts.SpawnWithOpts(e2e.WithArgs("install", suite.extraPackage), e2e.WithWorkDirectory(wd))
+	cp = ts.SpawnWithOpts(e2e.WithArgs(
+		"install", suite.extraPackage),
+		e2e.WithWorkDirectory(wd),
+		e2e.AppendEnv("ACTIVESTATE_CLI_DISABLE_RUNTIME=false"))
 	cp.Expect("You're about to add packages as an anonymous user")
 	cp.Expect("(Y/n)")
 	cp.Send("y")
