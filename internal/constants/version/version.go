f1496137bbd82bb0afa34a275375970dca62595e
Martin C Drohmann <martind@activestate.com>
2020-12-16 12:20:51 -0800

    use version.txt to set version

--- internal/constants/version/version.go
+++ internal/constants/version/version.go
@@ -1,31 +1,15 @@
 package version
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"regexp"
 
 	"github.com/ActiveState/cli/internal/environment"
 	"github.com/blang/semver"
 )
-
-// Semver increment type values.
-const (
-	Zeroed = "zeroed"
-	Patch  = "patch"
-	Minor  = "minor"
-	Major  = "major"
-)
-
-// IncrementTyper represents a client/service that returns strings related to
-// semver increment type values.
-type IncrementTyper interface {
-	IncrementType() (string, error)
-}
 
 // Env helps define and limit the available environments.
 type Env int
@@ -37,158 +21,44 @@ const (
 	RemoteEnv
 )
 
-// Incrementation provides methods for incrementing version numbers
-type Incrementation struct {
-	branch string
-	env    Env
-	typer  IncrementTyper
-}
+func ParseVersion(buildEnv Env) (*semver.Version, error) {
+	if buildEnv == LocalEnv {
+		return semver.New("0.0.0")
+	}
+	if buildEnv != RemoteEnv {
+		return nil, errors.New("encountered unknown build environment")
+	}
 
-// NewIncrementation returns a version service initialized with provider and environment information
-func NewIncrementation(typer IncrementTyper, branchName string, buildEnv Env) (*Incrementation, error) {
-	return &Incrementation{
-		branch: branchName,
-		env:    buildEnv,
-		typer:  typer,
-	}, nil
-}
-
-// Increment bumps the master version based on the current build environment
-// and the increment type provided.
-func (v *Incrementation) Increment() (*semver.Version, error) {
-	return v.incrementFromEnvironment()
-}
-
-// IncrementWithRevision bumps the master version based on the current build
-// environment, the increment type and revision string provided.
-func (v *Incrementation) IncrementWithRevision(revision string) (*semver.Version, error) {
-	version, err := v.incrementFromEnvironment()
+	root, err := environment.GetRootPath()
 	if err != nil {
 		return nil, err
 	}
+	versionFile := filepath.Join(root, "version.txt")
+	_, err = os.Stat(versionFile)
+	if err != nil {
+		return nil, fmt.Errorf("Could not access version.txt file at %s: %w", versionFile, err)
+	}
+	data, err := ioutil.ReadFile(versionFile)
+	if err != nil {
+		return nil, fmt.Errorf("Could not read from file %s: %w", versionFile, err)
+	}
+	v, err := semver.Parse(string(data))
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse version from file %s: %w", versionFile, err)
+	}
+	return &v, nil
+}
+
+func VersionWithRevision(version *semver.Version, revision string) (*semver.Version, error) {
+	v := version
 
 	prVersion, err := semver.NewPRVersion("SHA" + revision)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create pre-release version number: %v", err)
 	}
-	version.Pre = []semver.PRVersion{prVersion}
+	v.Pre = []semver.PRVersion{prVersion}
 
-	return version, nil
-}
-
-// needsIncrement whether we need to an increment for the environment
-func needsIncrement(env Env, branch string) bool {
-	return env != LocalEnv && (branch == "master" || branch == "unstable")
-}
-
-// Type returns the string representation of the version bump
-// ie. patch, minor, or major
-func (v *Incrementation) Type() (string, error) {
-	if needsIncrement(v.env, v.branch) {
-		return v.typer.IncrementType()
-	}
-
-	return Zeroed, nil
-}
-
-func latestVersionString(branch string) (string, error) {
-	type versionJSON struct {
-		Version string
-	}
-	var v versionJSON
-
-	rootPath, err := environment.GetRootPath()
-	if err != nil {
-		return "", err
-	}
-
-	versionFilePath := filepath.Join(rootPath, "build", "update", "version.json")
-	_, err = os.Stat(versionFilePath)
-	if err != nil {
-		return "", fmt.Errorf("Could not access version file at: %s", versionFilePath)
-	}
-
-	data, err := ioutil.ReadFile(versionFilePath)
-	if err != nil {
-		return "", err
-	}
-
-	err = json.Unmarshal(data, &v)
-	if err != nil {
-		return "", err
-	}
-
-	return v.Version, err
-}
-
-func masterVersion(branchName string) (*semver.Version, error) {
-	versionString, err := latestVersionString(branchName)
-	if err != nil {
-		return nil, err
-	}
-
-	regex := regexp.MustCompile(`\d+\.\d+\.\d+-(SHA)?[a-f0-9]+`)
-	match := regex.FindString(versionString)
-	if match == "" {
-		return nil, errors.New("could not determine master version")
-	}
-
-	masterVersion, err := semver.New(match)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create semver from version string: %s", err)
-	}
-	masterVersion.Pre = nil
-
-	return masterVersion, nil
-}
-
-func (v *Incrementation) incrementFromEnvironment() (*semver.Version, error) {
-	switch v.env {
-	case LocalEnv:
-		return semver.New("0.0.0")
-	case RemoteEnv:
-		return v.increment()
-	default:
-		return nil, errors.New("encountered unknown build environment")
-	}
-}
-
-func incrementFrom(baseVersion *semver.Version, increment string) (*semver.Version, error) {
-	copy := *baseVersion
-	switch increment {
-	case Patch:
-		copy.Patch++
-	case Minor:
-		copy.Minor++
-		copy.Patch = 0
-	case Major:
-		copy.Major++
-		copy.Minor = 0
-		copy.Patch = 0
-	default:
-		return nil, fmt.Errorf("encountered unexpected increment value: %s", increment)
-	}
-
-	return &copy, nil
-}
-
-func (v *Incrementation) increment() (*semver.Version, error) {
-	inc, err := v.Type()
-	if err != nil {
-		return nil, err
-	}
-
-	if inc == Zeroed {
-		return semver.New("0.0.0")
-	}
-
-	baseVersion, err := masterVersion(v.branch)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Printf("Base version for increment is: %s\n", baseVersion)
-
-	return incrementFrom(baseVersion, inc)
+	return v, nil
 }
 
 // NumberIsProduction returns whether or not the provided version number
