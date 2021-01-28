dafcaa9ac2c868bfc8e41feab82592670b548fb3
mdrakos <mike.drakos@gmail.com>
2021-01-28 09:47:01 -0800

    Move error check

--- internal/errs/unwrap.go
+++ internal/errs/unwrap.go
@@ -7,14 +7,15 @@ import (
 // UnwrapExitCode checks if the given error is a failure of type FailExecCmdExit and
 // returns the ExitCode of the process that failed with this error
 func UnwrapExitCode(err error) int {
+	if err == nil {
+		return 0
+	}
+
 	var eerr interface{ ExitCode() int }
 	isExitError := errors.As(err, &eerr)
 	if isExitError {
 		return eerr.ExitCode()
 	}
 
-	if err == nil {
-		return 0
-	}
 	return 1
 }
