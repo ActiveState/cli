6cf78827bd4ac5c2fd579d00e63310a37f8ea664
Daved <daved@codemodus.com>
2021-02-03 11:01:01 -0800

    Add doc comments to rtstat sendonce type

--- pkg/platform/runtime/internal/rtstat/once.go
+++ pkg/platform/runtime/internal/rtstat/once.go
@@ -4,17 +4,22 @@ import (
 	"sync"
 )
 
+// SendOnce provides a simple mechanism to ensure that only one "kind" of
+// analytics event is reported per instance lifetime.
 type SendOnce struct {
 	mu  sync.Mutex
 	set map[string]struct{}
 }
 
+// NewSendOnce sets up and returns a pointer to an instance of SendOnce.
 func NewSendOnce() *SendOnce {
 	return &SendOnce{
 		set: make(map[string]struct{}),
 	}
 }
 
+// Send calls send for each "kind" of analytics event only if it has not yet
+// been sent.
 func (so *SendOnce) Send(stat RtStat) {
 	key := stat.String()
 
