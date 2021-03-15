67c609d871d7aa1d674362b88284e98a471db6d0
mdrakos <mike.drakos@gmail.com>
2021-03-15 14:09:59 -0700

    Rename state cve

--- cmd/state/internal/cmdtree/cve.go
+++ cmd/state/internal/cmdtree/cve.go
@@ -12,9 +12,9 @@ func newCveCommand(prime *primer.Values) *captain.Command {
 	runner := cve.NewCve(prime)
 
 	cmd := captain.NewCommand(
-		"cve",
-		locale.Tl("cve_title", "Vulnerability Summary"),
-		locale.Tl("cve_description", "Show a summary of project vulnerabilities"),
+		"security",
+		locale.Tl("security_title", "Vulnerability Summary"),
+		locale.Tl("security_description", "Show a summary of project vulnerabilities"),
 		prime.Output(),
 		prime.Config(),
 		[]*captain.Flag{},
@@ -24,6 +24,7 @@ func newCveCommand(prime *primer.Values) *captain.Command {
 		},
 	)
 	cmd.SetGroup(PlatformGroup)
+	cmd.SetAliases("cve")
 	return cmd
 }
 
@@ -35,15 +36,15 @@ func newReportCommand(prime *primer.Values) *captain.Command {
 
 	return captain.NewCommand(
 		"report",
-		locale.Tl("cve_report_title", "Vulnerability Report"),
-		locale.Tl("cve_report_cmd_description", "Show a detailed report of project vulnerabilities"),
+		locale.Tl("security_report_title", "Vulnerability Report"),
+		locale.Tl("security_report_cmd_description", "Show a detailed report of project vulnerabilities"),
 		prime.Output(),
 		prime.Config(),
 		[]*captain.Flag{},
 		[]*captain.Argument{
 			{
-				Name:        locale.Tl("cve_report_namespace_arg", "Organization/Project"),
-				Description: locale.Tl("cve_report_namespace_arg_description", "The project for which the report is created"),
+				Name:        locale.Tl("security_report_namespace_arg", "Organization/Project"),
+				Description: locale.Tl("security_report_namespace_arg_description", "The project for which the report is created"),
 				Value:       params.Namespace,
 			},
 		},
@@ -59,15 +60,15 @@ func newOpenCommand(prime *primer.Values) *captain.Command {
 
 	return captain.NewCommand(
 		"open",
-		locale.Tl("cve_open_title", "Opening Vulnerability Details Page"),
-		locale.Tl("cve_open_cmd_description", "Open the given vulnerability details in your browser"),
+		locale.Tl("security_open_title", "Opening Vulnerability Details Page"),
+		locale.Tl("security_open_cmd_description", "Open the given vulnerability details in your browser"),
 		prime.Output(),
 		prime.Config(),
 		[]*captain.Flag{},
 		[]*captain.Argument{
 			{
-				Name:        locale.Tl("cve_open_id_arg", "ID"),
-				Description: locale.Tl("cve_open_id_arg_description", "The vulnerablility to open in your browser"),
+				Name:        locale.Tl("security_open_id_arg", "ID"),
+				Description: locale.Tl("security_open_id_arg_description", "The vulnerablility to open in your browser"),
 				Value:       &params.ID,
 			},
 		},
