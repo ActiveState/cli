0374cd864b7f0142d9064145ff3d4077e85785c9
Martin C Drohmann <martind@activestate.com>
2021-03-17 16:43:37 -0700

    Revert "remove caching used for development"
    This reverts commit ed8043ffef2fa541f285c4a328e23d4a91051caa.

--- pkg/platform/runtime2/setup/setup.go
+++ pkg/platform/runtime2/setup/setup.go
@@ -2,6 +2,7 @@ package setup
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -313,7 +314,7 @@ func (s *Setup) setupArtifact(buildEngine model.BuildEngine, a artifact.Artifact
 	}
 
 	archivePath := filepath.Join(targetDir, a.String()+as.InstallerExtension())
-	if err := s.downloadArtifact(unsignedURI, archivePath); err != nil {
+	if err := s.downloadArtifact(a, unsignedURI, archivePath); err != nil {
 		return errs.Wrap(err, "Could not download artifact %s", unsignedURI)
 	}
 	s.msgHandler.ArtifactDownloadCompleted(a)
@@ -371,7 +372,20 @@ func (s *Setup) setupArtifact(buildEngine model.BuildEngine, a artifact.Artifact
 
 // downloadArtifact retrieves the tarball for an artifactID
 // Note: the tarball may also be retrieved from a local cache directory if that is available.
-func (s *Setup) downloadArtifact(unsignedURI string, targetFile string) error {
+func (s *Setup) downloadArtifact(a artifact.ArtifactID, unsignedURI string, targetFile string) error {
+	cachePath := filepath.Join("/tmp", "cache")
+	_ = os.MkdirAll(cachePath, 0775)
+
+	cacheFile := filepath.Join(cachePath, a.String())
+	if fileutils.FileExists(cacheFile) {
+		err := fileutils.CopyFile(cacheFile, targetFile)
+		if err != nil {
+			logging.Debug("Failed to copy cached file %s -> %s", cacheFile, targetFile)
+		} else {
+			return nil
+		}
+	}
+
 	artifactURL, err := url.Parse(unsignedURI)
 	if err != nil {
 		return errs.Wrap(err, "Could not parse artifact URL %s.", unsignedURI)
@@ -389,6 +403,10 @@ func (s *Setup) downloadArtifact(unsignedURI string, targetFile string) error {
 	}
 	if err := fileutils.WriteFile(targetFile, b); err != nil {
 		return errs.Wrap(err, "Writing download to target file %s failed", targetFile)
+	}
+	fmt.Printf("Writing to cache file: %s\n", cacheFile)
+	if err := fileutils.WriteFile(cacheFile, b); err != nil {
+		logging.Debug("Failed to write %s to cache: %v", cacheFile, err)
 	}
 	return nil
 }
