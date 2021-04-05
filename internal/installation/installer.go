6e51e4aee5d92d60c294281bd4f35e82a63dd19c
Nathan Rijksen <n.rijksen@gmail.com>
2021-04-05 10:58:50 -0700

    Add comment for clarity

--- internal/installation/installer.go
+++ internal/installation/installer.go
@@ -10,6 +10,7 @@ import (
 )
 
 func InstallPath() (string, error) {
+	// Facilitate use-case of running executables from the build dir while developing
 	if !rtutils.BuiltViaCI && strings.Contains(path.Clean(os.Args[0]), "/build/") {
 		return filepath.Dir(os.Args[0]), nil
 	}
