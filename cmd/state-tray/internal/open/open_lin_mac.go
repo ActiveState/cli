301d51790ff917534ef15b97c9ab32ab1def9e8a
mdrakos <mike.drakos@gmail.com>
2021-04-08 16:23:39 -0700

    Initial working open prompt on MacOS

--- cmd/state-tray/internal/open/open_lin_mac.go
+++ cmd/state-tray/internal/open/open_lin_mac.go
@@ -2,8 +2,20 @@
 
 package open
 
-import "errors"
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/ActiveState/cli/internal/locale"
+)
 
 func Prompt(command string) error {
-	return errors.New("Not implemented")
+	script := fmt.Sprintf(`tell application "Terminal" to do script "%s"`, command)
+	cmd := exec.Command(`osascript`, "-s", "h", "-e", script)
+	err := cmd.Run()
+	if err != nil {
+		return locale.WrapError(err, "err_open_prompt", "Could not open prompt")
+	}
+
+	return nil
 }
