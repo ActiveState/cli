2da85af63f1b2fca89319a32574ada119c591cbe
mdrakos <mike.drakos@gmail.com>
2021-04-09 14:42:51 -0700

    Use open rather than osascript to open terminal Also check for iTerm installation

--- cmd/state-tray/internal/open/open_lin_mac.go
+++ cmd/state-tray/internal/open/open_lin_mac.go
@@ -3,19 +3,32 @@
 package open
 
 import (
-	"fmt"
 	"os/exec"
 
+	"github.com/ActiveState/cli/internal/fileutils"
+	"github.com/ActiveState/cli/internal/language"
 	"github.com/ActiveState/cli/internal/locale"
+	"github.com/ActiveState/cli/internal/scriptfile"
 )
 
 func Prompt(command string) error {
-	script := fmt.Sprintf(`tell application "Terminal" to do script "%s"`, command)
-	cmd := exec.Command(`osascript`, "-s", "h", "-e", script)
-	err := cmd.Run()
+	sf, err := scriptfile.New(language.Bash, "open-terminal", command)
+	if err != nil {
+		return locale.WrapError(err, "err_open_create_scriptfile", "Could not create temporary script file")
+	}
+
+	cmd := exec.Command("open", "-a", getPrompt(), sf.Filename())
+	err = cmd.Run()
 	if err != nil {
 		return locale.WrapError(err, "err_open_prompt", "Could not open prompt")
 	}
 
 	return nil
+}
+
+func getPrompt() string {
+	if fileutils.DirExists("/Applications/iTerm.app") {
+		return "iTerm"
+	}
+	return "Terminal"
 }
