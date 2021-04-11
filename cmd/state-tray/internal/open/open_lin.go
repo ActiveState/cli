52b7e21a3112f6eae8642f2b422fe3d14b1f049a
mdrakos <mike.drakos@gmail.com>
2021-04-11 13:41:31 -0700

    Further separate open library by platform

---
+++ cmd/state-tray/internal/open/open_lin.go
@@ -0,0 +1,13 @@
+//+build linux
+
+package open
+
+import "errors"
+
+func Prompt(command string) error {
+	return errors.New("not implemented")
+}
+
+func getPrompt() string {
+	return errors.New("not implemented")
+}
