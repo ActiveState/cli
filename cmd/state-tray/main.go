9e8640d0da15547049fce899176d75ef98574557
Martin C Drohmann <martind@activestate.com>
2021-04-12 16:17:03 -0700

    fix missing import

--- cmd/state-tray/main.go
+++ cmd/state-tray/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ActiveState/cli/cmd/state-tray/pkg/autostart"
 	"github.com/ActiveState/cli/internal/appinfo"
 	"github.com/ActiveState/cli/internal/config"
+	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/errs"
 	"github.com/ActiveState/cli/internal/exeutils"
 	"github.com/ActiveState/cli/internal/fileutils"
