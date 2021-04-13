3659e2108b7439bb0daabd8dfc42cbddb55f72e8
Martin C Drohmann <martind@activestate.com>
2021-04-13 10:02:01 -0700

    fix missing import on darwin

--- cmd/state-installer/internal/installer/installer_darwin.go
+++ cmd/state-installer/internal/installer/installer_darwin.go
@@ -2,6 +2,10 @@
 
 package installer
 
+import (
+	"github.com/ActiveState/cli/internal/errs"
+)
+
 // InstallSystemFiles installs files in the /Application directory
 func InstallSystemFiles(fromDir string) error {
 	return errs.New("not implemented yet.")
