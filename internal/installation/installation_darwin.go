06900e83727ab42b4aa3c763da75837607da03fc
Martin C Drohmann <martind@activestate.com>
2021-04-16 10:34:18 -0700

    fix imports

--- internal/installation/installation_darwin.go
+++ internal/installation/installation_darwin.go
@@ -2,10 +2,6 @@
 
 package installation
 
-import (
-	"github.com/ActiveState/cli/internal/errs"
-)
-
 // InstallSystemFiles installs files in the /Application directory
 func InstallSystemFiles(installDir string) error {
 	return nil
