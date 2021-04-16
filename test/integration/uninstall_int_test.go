068f5231d10e23a3713b56b3728aa4d154ab5cba
mdrakos <mike.drakos@gmail.com>
2021-04-15 17:13:56 -0700

    Add integration test

---
+++ test/integration/uninstall_int_test.go
@@ -0,0 +1,50 @@
+package integration
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/ActiveState/cli/internal/fileutils"
+	"github.com/ActiveState/cli/internal/testhelpers/e2e"
+	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
+	"github.com/stretchr/testify/suite"
+)
+
+type UninstallIntegrationTestSuite struct {
+	tagsuite.Suite
+}
+
+func (suite *UninstallIntegrationTestSuite) TestUninstall() {
+	ts := e2e.New(suite.T(), true)
+	defer ts.Close()
+
+	ts.UseDistinctStateExe()
+
+	installEntry := fmt.Sprintf("install_path: %s", ts.Dirs.Bin)
+	err := ioutil.WriteFile(filepath.Join(ts.Dirs.Config, "config.yaml"), []byte(installEntry), 0655)
+	suite.Require().NoError(err, "Could not write install entry")
+
+	cp := ts.Spawn("clean", "uninstall")
+	cp.Expect("You are about to remove")
+	cp.SendLine("y")
+	cp.ExpectLongString("Successfully removed State Tool and related files")
+	cp.ExpectExitCode(0)
+
+	if fileutils.DirExists(ts.Dirs.Cache) {
+		suite.Fail("Config dir should not exist after uninstall")
+	}
+
+	if fileutils.DirExists(ts.Dirs.Config) {
+		suite.Fail("Config dir should not exist after uninstall")
+	}
+
+	if fileutils.DirExists(ts.Dirs.Bin) {
+		suite.Fail("Installation dir should not exist after uninstall")
+	}
+}
+
+func TestUninstallIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(UninstallIntegrationTestSuite))
+}
