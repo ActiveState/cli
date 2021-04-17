b03e33a9554a3196efaf5120cf1232a897dc1aa5
daved <daved@codemodus.com>
2021-04-16 19:03:48 -0700

    Setup prompt func in tray for linux

--- cmd/state-tray/internal/open/open_lin.go
+++ cmd/state-tray/internal/open/open_lin.go
@@ -2,12 +2,29 @@
 
 package open
 
-import "errors"
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/ActiveState/cli/internal/locale"
+)
 
 func Prompt(command string) error {
-	return errors.New("not implemented")
-}
+	shellData, err := exec.Command("bash", "-c",
+		"grep ^$(id -un): /etc/passwd | cut -d: -f7-",
+	).Output()
+	if err != nil {
+		return locale.WrapError(err,
+			"err_determine_default_shell", "Could not determine default shell",
+		)
+	}
+	shell := string(shellData[:len(shellData)-1]) // trim newline
 
-func getPrompt() string {
-	return "not implemented"
+	command = fmt.Sprintf("%s;%s", command, shell)
+	cmd := exec.Command("x-terminal-emulator", "-e", shell, "-c", command)
+	if err := cmd.Run(); err != nil {
+		return locale.WrapError(err, "err_open_prompt", "Could not open prompt")
+	}
+
+	return nil
 }
