1ca2d8a42833472699e8a8d544997012a372a51f
mdrakos <mike.drakos@gmail.com>
2021-04-21 10:05:53 -0700

    Remove app directory, if it exists, before installation

--- cmd/state-installer/internal/installer/installation_darwin.go
+++ cmd/state-installer/internal/installer/installation_darwin.go
@@ -12,12 +12,20 @@ import (
 	"github.com/gobuffalo/packr"
 )
 
-const appDir = "/Applications"
+const (
+	appDir  = "/Applications"
+	appName = "ActiveState Desktop.app"
+)
 
 // InstallSystemFiles installs files in the /Application directory
 func InstallSystemFiles(installDir string) error {
+	err := os.RemoveAll(filepath.Join(appDir, appName))
+	if err != nil {
+		return errs.Wrap(err, "Could not remove old app directory")
+	}
+
 	box := packr.NewBox("../../../../assets/macOS/app")
-	err := box.Walk(func(path string, _ packr.File) error {
+	err = box.Walk(func(path string, _ packr.File) error {
 		if fileutils.IsDir(path) {
 			err := fileutils.Mkdir(filepath.Join(appDir, path))
 			if err != nil {
@@ -36,7 +44,7 @@ func InstallSystemFiles(installDir string) error {
 	}
 
 	trayInfo := appinfo.TrayApp()
-	err = createNewSymlink(filepath.Join(installDir, filepath.Base(trayInfo.Exec())), filepath.Join(appDir, "ActiveState Desktop.app", "Contents", "MacOS", filepath.Base(trayInfo.Exec())))
+	err = createNewSymlink(filepath.Join(installDir, filepath.Base(trayInfo.Exec())), filepath.Join(appDir, appName, "Contents", "MacOS", filepath.Base(trayInfo.Exec())))
 	if err != nil {
 		return errs.Wrap(err, "Could not create state-tray symlink")
 	}
