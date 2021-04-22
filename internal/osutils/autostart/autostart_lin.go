049f1b569d450199e8f0eefc295c04103db68db3
daved <daved@codemodus.com>
2021-04-22 13:40:30 -0700

    Make tray desktop file a tmpl to set exec val

--- internal/osutils/autostart/autostart_lin.go
+++ internal/osutils/autostart/autostart_lin.go
@@ -3,10 +3,14 @@
 package autostart
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"text/template"
 
+	"github.com/ActiveState/cli/internal/appinfo"
 	"github.com/ActiveState/cli/internal/errs"
 	"github.com/ActiveState/cli/internal/fileutils"
 	"github.com/gobuffalo/packr"
@@ -14,10 +18,11 @@ import (
 )
 
 func (a *App) Enable() error {
-	appPath, err := ensuredAppPath()
+	appDir, err := dirPath(applicationDir)
 	if err != nil {
-		return errs.Wrap(err, "Could not ensure application file is present")
+		return errs.Wrap(err, "Could not get application file")
 	}
+	appPath := filepath.Join(appDir, launchFile)
 
 	enabled, err := a.IsEnabled()
 	if err != nil {
@@ -58,6 +63,18 @@ func (a *App) Disable() error {
 }
 
 func (a *App) IsEnabled() (bool, error) {
+	appDir, err := dirPath(applicationDir)
+	if err != nil {
+		return false, errs.Wrap(err, "Could not get application file")
+	}
+	appPath := filepath.Join(appDir, launchFile)
+
+	if !fileutils.FileExists(appPath) {
+		if err = setupAppFile(appPath); err != nil {
+			return false, errs.Wrap(err, "Could not setup app file")
+		}
+	}
+
 	dir, err := dirPath(autostartDir)
 	if err != nil {
 		return false, errs.Wrap(err, "Could not get autostart file")
@@ -84,45 +101,71 @@ func dirPath(dir string) (string, error) {
 	return filepath.Join(homeDir, dir), nil
 }
 
-func ensuredAppPath() (string, error) {
-	appDir, err := dirPath(applicationDir)
+func setupAppFile(appPath string) error {
+	t := template.New("")
+	t, err := t.Parse(desktopFileTmpl)
 	if err != nil {
-		return "", errs.Wrap(err, "Could not get application file")
-	}
-	appPath := filepath.Join(appDir, launchFile)
-
-	if !fileutils.FileExists(appPath) {
-		box := packr.NewBox("../../../assets")
-
-		icons, err := dirPath(iconsDir)
-		if err != nil {
-			return "", errs.Wrap(err, "Could not get icons directory")
-		}
-		iconPath := filepath.Join(icons, iconFileState)
-
-		if err := fileutils.WriteFile(iconPath, box.Bytes(iconFileBase)); err != nil {
-			return "", errs.Wrap(err, "Could not write icon file")
-		}
-
-		if err := fileutils.WriteFile(appPath, box.Bytes(launchFile)); err != nil {
-			return "", errs.Wrap(err, "Could not write application file")
-		}
-
-		file, err := os.Open(appPath)
-		if err != nil {
-			return "", errs.Wrap(err, "Could not open application file")
-		}
-		err = file.Chmod(0770)
-		file.Close()
-		if err != nil {
-			return "", errs.Wrap(err, "Could not make file executable")
-		}
-
-		cmd := exec.Command("gio", "set", appPath, "metadata::trusted", "true")
-		if err := cmd.Run(); err != nil {
-			return "", errs.Wrap(err, "Could not set application file as trusted")
-		}
+		return errs.Wrap(err, "Could not parse desktop file template")
 	}
 
-	return appPath, nil
+	buf := &bytes.Buffer{}
+	trayExec := appinfo.TrayApp().Exec()
+	fmt.Println(trayExec)
+	data := desktopFileData{Exec: appinfo.TrayApp().Exec()}
+	if err = t.Execute(buf, data); err != nil {
+		return errs.Wrap(err, "Could not execute template")
+	}
+
+	icons, err := dirPath(iconsDir)
+	if err != nil {
+		return errs.Wrap(err, "Could not get icons directory")
+	}
+	iconPath := filepath.Join(icons, iconFileState)
+
+	box := packr.NewBox("../../../assets")
+	if err := fileutils.WriteFile(iconPath, box.Bytes(iconFileBase)); err != nil {
+		return errs.Wrap(err, "Could not write icon file")
+	}
+
+	if err := fileutils.WriteFile(appPath, buf.Bytes()); err != nil {
+		return errs.Wrap(err, "Could not write application file")
+	}
+
+	file, err := os.Open(appPath)
+	if err != nil {
+		return errs.Wrap(err, "Could not open application file")
+	}
+	err = file.Chmod(0770)
+	file.Close()
+	if err != nil {
+		return errs.Wrap(err, "Could not make file executable")
+	}
+
+	cmd := exec.Command("gio", "set", appPath, "metadata::trusted", "true")
+	if err := cmd.Run(); err != nil {
+		return errs.Wrap(err, "Could not set application file as trusted")
+	}
+
+	return nil
 }
+
+type desktopFileData struct {
+	Exec string
+}
+
+var desktopFileTmpl = `
+[Desktop Entry]
+Name=ActiveState Desktop
+GenericName=Language Runtime Manager
+Type=Application
+Comment=Manage ActiveState Platform Projects
+Exec="{{ .Exec }}"
+Terminal=false
+Keywords=activestate;state;tray;language;runtime;python;perl;tcl;
+Categories=Utility;Development;
+Hidden=false
+NoDisplay=false
+StartupNotify=false
+Icon=state-tray
+Name[en_US]=ActiveState Desktop
+`
