dc7e7b2342762110991d04159640aab29658ce76
Martin C Drohmann <martind@activestate.com>
2021-05-03 15:08:01 -0700

    fix mock URL

--- internal/testhelpers/updatemocks/updatemocks.go
+++ internal/testhelpers/updatemocks/updatemocks.go
@@ -2,10 +2,8 @@ package updatemocks
 
 import (
 	"fmt"
-
-	"github.com/ActiveState/cli/internal/constants"
 )
 
 func CreateRequestPath(branch, append string) string {
-	return fmt.Sprintf("%s/%s/%s", constants.CommandName, branch, append)
+	return fmt.Sprintf("%s/%s", branch, append)
 }
