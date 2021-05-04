51c5b3ee5724f2c02d42edfe7c55279a89b4c07b
mdrakos <mike.drakos@gmail.com>
2021-05-04 10:13:00 -0700

    Fix syntax error

--- test/integration/install_scripts_int_test.go
+++ test/integration/install_scripts_int_test.go
@@ -213,7 +213,7 @@ func (suite *InstallScriptsIntegrationTestSuite) TestLegacyInstallShInstallMulti
 	cp := ts.SpawnCmdWithOpts(
 		"bash",
 		e2e.WithArgs(script, "-t", ts.Dirs.Work, "-b", constants.BranchName),
-		e2e.AppendEnv(fmt.Sprintf("_TEST_UPDATE_URL=http://localhost:%s/", testPort, fmt.Sprintf("_TEST_SYSTEM_PATH=%s", dir))))
+		e2e.AppendEnv(fmt.Sprintf("_TEST_UPDATE_URL=http://localhost:%s/", testPort), fmt.Sprintf("_TEST_SYSTEM_PATH=%s", dir)))
 
 	expectLegacyStateToolInstallation(cp, "n")
 	cp.Expect("State Tool Installed")
