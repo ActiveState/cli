36f561c54b8420542b9f0d24662df62a308b7449
Martin C Drohmann <martind@activestate.com>
2021-05-12 11:16:04 -0700

    removeInstall script expects directory as argument

--- assets/scripts/removeInstall.bat
+++ assets/scripts/removeInstall.bat
@@ -4,5 +4,7 @@ REM Tool binary cannot be removed while it is running.
 REM The expected argument is the path to the State Tool binary
 
 timeout 2
-del /s /q %1
+del /s /q %1/state.exe
+del /s /q %1/state-svc.exe
+del /s /q %1/state-tray.exe
 exit 0
