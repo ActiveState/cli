d57a1a7c53e1799a61680f914f22a134e029800e
Martin C Drohmann <martind@activestate.com>
2021-05-13 14:48:26 -0700

    do not use _TEST_SYSTEM_PATH on darwin

--- test/integration/install_scripts_int_test.go
+++ test/integration/install_scripts_int_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/environment"
 	"github.com/ActiveState/cli/internal/fileutils"
+	"github.com/ActiveState/cli/internal/installation"
 	"github.com/ActiveState/cli/internal/osutils"
 	"github.com/ActiveState/cli/internal/testhelpers/e2e"
 	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
@@ -238,10 +239,16 @@ func (suite *InstallScriptsIntegrationTestSuite) TestInstallSh() {
 
 	for _, tt := range tests {
 		suite.Run(tt.Name, func() {
-			dir, err := ioutil.TempDir("", "temp_home*")
-			suite.NoError(err)
+			dir, err := installation.LauncherInstallPath()
+			suite.Require().NoError(err)
+			var extraEnv []string
+			if runtime.GOOS == "linux" {
+				dir, err = ioutil.TempDir("", "temp_home*")
+				suite.Require().NoError(err)
+				extraEnv = append(extraEnv, fmt.Sprintf("HOME=%s", dir), fmt.Sprintf("_TEST_SYSTEM_PATH=%s", dir))
+			}
 
-			ts := e2e.New(suite.T(), false, fmt.Sprintf("HOME=%s", dir), fmt.Sprintf("_TEST_SYSTEM_PATH=%s", dir))
+			ts := e2e.New(suite.T(), false, extraEnv...)
 			defer ts.Close()
 
 			script := scriptPath(suite.T(), ts.Dirs.Work, false, tt.TestInstall)
