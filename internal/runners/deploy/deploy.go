bfc0e27dc58e894be76a5d07324b7b4b19fbff39
Martin C Drohmann <martind@activestate.com>
2021-05-13 22:43:27 -0700

    ensure that failure / success event is sent for deploy install

--- internal/runners/deploy/deploy.go
+++ internal/runners/deploy/deploy.go
@@ -157,6 +157,10 @@ func (d *Deploy) install(rtTarget setup.Targeter) error {
 		return locale.WrapError(err, "deploy_install_failed", "Installation failed.")
 	}
 
+	// call rti.Environ as this completes the runtime activation cycle:
+	// It ensures that the analytics event for failure / success are sent
+	_, _ = rti.Env(false, false)
+
 	if rt.GOOS == "windows" {
 		box := packr.NewBox("../../../assets/scripts")
 		contents := box.Bytes("setenv.bat")
