c4f09c45fd5dceeaa7bf4a6fd67a51d90ade3496
Martin C Drohmann <martind@activestate.com>
2021-05-17 09:49:21 -0700

    aggregate un-installation errors

--- internal/runners/clean/run.go
+++ internal/runners/clean/run.go
@@ -3,41 +3,42 @@ package clean
 import (
 	"os"
 
-	"github.com/ActiveState/cli/internal/errs"
 	"github.com/ActiveState/cli/internal/fileutils"
 	"github.com/ActiveState/cli/internal/locale"
-	"github.com/ActiveState/cli/internal/logging"
 	"github.com/ActiveState/cli/internal/runners/prepare"
 )
 
 func (u *Uninstall) runUninstall() error {
+	// we aggregate installation errors, such that we can display all installation problems in the end
+	// TODO: This behavior should be replaced with a proper rollback mechanism https://www.pivotaltracker.com/story/show/178134918
+	var aggErr error
 	err := removeCache(u.cfg.CachePath())
 	if err != nil {
-		u.reportError(locale.Tl("uninstall_remove_cache_err", "Failed to remove cache directory."), err)
+		aggErr = locale.WrapError(aggErr, "uninstall_remove_cache_err", "Failed to remove cache directory {{.V0}}.", u.cfg.CachePath())
 	}
 
 	err = removeInstall(u.cfg, u.installDir)
 	if err != nil {
-		u.reportError(locale.Tl("uninstall_remove_executables_err", "Failed to remove all State Tool files in installation directory {{.V0}}", u.installDir), err)
+		aggErr = locale.WrapError(aggErr, "uninstall_remove_executables_err", "Failed to remove all State Tool files in installation directory {{.V0}}", u.installDir)
 	}
 
 	err = removeConfig(u.cfg)
 	if err != nil {
-		u.reportError(locale.Tl("uninstall_remove_config_err", "Failed to remove configuration directory {{.V0}}", u.cfg.ConfigPath()), err)
+		aggErr = locale.WrapError(aggErr, "uninstall_remove_config_err", "Failed to remove configuration directory {{.V0}}", u.cfg.ConfigPath())
+
 	}
 
 	err = undoPrepare()
 	if err != nil {
-		u.reportError(locale.Tl("uninstall_prepare_err", "Failed to undo some installation steps."), err)
+		aggErr = locale.WrapError(aggErr, "uninstall_prepare_err", "Failed to undo some installation steps.")
+	}
+
+	if aggErr != nil {
+		return aggErr
 	}
 
 	u.out.Print(locale.T("clean_success_message"))
 	return nil
-}
-
-func (u *Uninstall) reportError(msg string, err error) {
-	logging.Error("%s: %v", msg, errs.Join(err, ": "))
-	u.out.Notice(msg)
 }
 
 func removeCache(cachePath string) error {
@@ -56,7 +57,7 @@ func undoPrepare() error {
 		if fileutils.TargetExists(f) {
 			err := os.Remove(f)
 			if err != nil {
-				aggErr = errs.Wrap(aggErr, "Failed to remove %s: %v", f, err)
+				aggErr = locale.WrapError(aggErr, "err_undo_prepare_remove_file", "Failed to remove file %s", f)
 			}
 		}
 	}
