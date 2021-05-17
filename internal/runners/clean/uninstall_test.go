b7d2a2f59ca159a1e702ca165a48be3e844fd958
mdrakos <mike.drakos@gmail.com>
2021-05-17 13:12:14 -0700

    Remove unit test covered by integration test

--- internal/runners/clean/uninstall_test.go
+++ internal/runners/clean/uninstall_test.go
@@ -4,9 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"runtime"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/suite"
 
@@ -52,29 +50,6 @@ func (suite *CleanTestSuite) SetupTest() {
 	suite.cachePath, err = ioutil.TempDir("", "")
 	suite.Require().NoError(err)
 	suite.Require().DirExists(suite.cachePath)
-}
-
-func (suite *CleanTestSuite) TestUninstall() {
-	runner, err := newUninstall(&outputhelper.TestOutputer{}, &confirmMock{confirm: true}, newConfigMock(suite.T(), suite.cachePath, suite.configPath))
-	suite.Require().NoError(err)
-	runner.installDir = filepath.Dir(suite.installPath)
-	err = runner.Run(&UninstallParams{})
-	suite.Require().NoError(err)
-
-	// On windows the files are deleted in the background, so we have to wait for that process to finish
-	if runtime.GOOS == "windows" {
-		time.Sleep(3 * time.Second)
-	}
-
-	if fileutils.DirExists(suite.configPath) {
-		suite.Fail("config directory should not exist after uninstall")
-	}
-	if fileutils.DirExists(suite.cachePath) {
-		suite.Fail("cache directory should not exist after uninstall")
-	}
-	if fileutils.FileExists(suite.installPath) {
-		suite.Fail("installed file should not exist after uninstall")
-	}
 }
 
 func (suite *CleanTestSuite) TestUninstall_PromptNo() {
