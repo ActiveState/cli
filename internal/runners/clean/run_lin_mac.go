fbcb9fbf21bccb749bae8509c05f30669421b2e3
mdrakos <mike.drakos@gmail.com>
2021-05-17 12:59:14 -0700

    Fix build error on Mac

--- internal/runners/clean/run_lin_mac.go
+++ internal/runners/clean/run_lin_mac.go
@@ -14,26 +14,6 @@ import (
 	"github.com/ActiveState/cli/internal/output"
 )
 
-func (u *Uninstall) runUninstall() error {
-	err := removeCache(u.cfg.CachePath())
-	if err != nil {
-		return err
-	}
-
-	err = removeInstall(u.installPath)
-	if err != nil {
-		return locale.WrapError(err, "err_clean_install_dir", "Coul dnot remove installation directory")
-	}
-
-	err = removeConfig(u.cfg.ConfigPath(), u.out)
-	if err != nil {
-		return locale.WrapError(err, "err_clean_config_dir", "Could not remove config directory")
-	}
-
-	u.out.Print(locale.T("clean_success_message"))
-	return nil
-}
-
 func removeConfig(configPath string, out output.Outputer) error {
 	file, err := os.Open(logging.FilePath())
 	if err != nil {
