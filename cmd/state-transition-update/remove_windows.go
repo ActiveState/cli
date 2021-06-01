1f81c937ad01e3594c27ad2772c4f5ef6327f323
Martin C Drohmann <martind@activestate.com>
2021-06-01 10:28:54 -0700

    fix remove State Tool for windows-case

--- cmd/state-transition-update/remove_windows.go
+++ cmd/state-transition-update/remove_windows.go
@@ -1,5 +1,22 @@
 // +build windows
 
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/ActiveState/cli/internal/appinfo"
+	"github.com/ActiveState/cli/internal/exeutils"
+	"github.com/ActiveState/cli/internal/language"
+	"github.com/ActiveState/cli/internal/locale"
+	"github.com/ActiveState/cli/internal/logging"
+	"github.com/ActiveState/cli/internal/scriptfile"
+	"github.com/gobuffalo/packr"
+)
+
 func removeSelf() error {
 	scriptName := "removePaths"
 	box := packr.NewBox("../../../assets/scripts/")
@@ -11,6 +28,13 @@ func removeSelf() error {
 
 	exe := appinfo.StateApp().Exec()
 
+	var logFile string = "remove-old-state.log"
+	logDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		logging.Error("Failed to create temporary dir for old State Tool removal-log file: %v", err)
+	} else {
+		logFile = filepath.Join(logDir, "remove-old-state.log")
+	}
 	args := []string{"/C", sf.Filename(), logFile, fmt.Sprintf("%d", os.Getpid()), filepath.Base(exe), exe}
 	_, err = exeutils.ExecuteAndForget("cmd.exe", args)
 	if err != nil {
