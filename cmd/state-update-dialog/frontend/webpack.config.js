a08ce4701fdfa58d182dca452e71948ebd56cccc
Martin C Drohmann <martind@activestate.com>
2021-06-07 12:18:27 -0700

    compile with production generated main.js

--- cmd/state-update-dialog/frontend/webpack.config.js
+++ cmd/state-update-dialog/frontend/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: ['babel-polyfill', './main.js'],
     target: "es5",
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'generated')
