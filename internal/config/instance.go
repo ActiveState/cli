6b69096b32f9d82b242353f92677574f96b42c51
Martin C Drohmann <martind@activestate.com>
2021-06-09 19:29:39 -0700

    add lock for processes

--- internal/config/instance.go
+++ internal/config/instance.go
@@ -42,6 +42,9 @@ type Instance struct {
 	data          map[string]interface{}
 	// dataMutex is used to synchronize manipulations of the data map between threads, as most of these are not guarded by the file lock.
 	dataMutex *sync.RWMutex
+	// lockMutex ensures that file lock can be held only once per process.  Theoretically, this should be ensured by the `flock` package, but it isn't.  So, we need this hack.
+	// https://www.pivotaltracker.com/story/show/178478669
+	lockMutex *sync.Mutex
 }
 
 func new(localPath string) (*Instance, error) {
@@ -49,6 +52,7 @@ func new(localPath string) (*Instance, error) {
 		localPath: localPath,
 		data:      make(map[string]interface{}),
 		dataMutex: &sync.RWMutex{},
+		lockMutex: &sync.Mutex{},
 	}
 	err := instance.ensureConfigExists()
 	if err != nil {
@@ -64,15 +68,18 @@ func new(localPath string) (*Instance, error) {
 }
 
 func (i *Instance) GetLock() error {
+	i.lockMutex.Lock()
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	locked, err := i.lock.TryLockContext(ctx, lockRetryDelay)
 	if err != nil {
+		i.lockMutex.Unlock()
 		return errs.Wrap(err, "Timed out waiting for exclusive lock")
 	}
 
 	if !locked {
+		i.lockMutex.Unlock()
 		return errs.New("Timeout out waiting for exclusive lock")
 
 	}
@@ -96,6 +103,17 @@ func (i *Instance) GetRLock() error {
 }
 
 func (i *Instance) ReleaseLock() error {
+	defer i.lockMutex.Unlock()
+	f, err := os.OpenFile("/tmp/config_lock",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(fmt.Sprintf("[%s] Process %s (%d) releases lock\n", time.Now(), os.Args[0], os.Getpid())); err != nil {
+		log.Println(err)
+	}
+
 	if err := i.lock.Unlock(); err != nil {
 		return errs.Wrap(err, "Failed to release lock")
 	}
