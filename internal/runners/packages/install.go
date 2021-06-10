56e5db0bd7fa909cab17f777998ef9f3a342c978
mdrakos <mike.drakos@gmail.com>
2021-06-10 11:40:46 -0700

    More WIP

--- internal/runners/packages/install.go
+++ internal/runners/packages/install.go
@@ -45,7 +45,17 @@ func NewInstall(prime primeable) *Install {
 func (a *Install) Run(params InstallRunParams, nstype model.NamespaceType) error {
 	logging.Debug("ExecuteInstall")
 	if a.proj == nil {
-		return a.addNoProject(params.Package.Name(), params.Package.Version())
+		// TODO: This function could get the necessary information to eventually call executePackageOperation
+		// It will use the search endpoint and if it can't resolve the language then prompt the user for it.
+		// With that info it should have everything it needs to call the function
+		lang, err := a.getPackageLanguage(params.Package.Name(), params.Package.Version())
+		if err != nil {
+			return locale.WrapError(err, "err_install_get_langauge", "Could not get language for package: {{.V0}}", params.Package.Name())
+		}
+		fmt.Println("Lang:", lang)
+		return nil
+
+		// return a.addNoProject(params.Package.Name(), params.Package.Version())
 		// return locale.NewInputError("err_no_project")
 	}
 
@@ -57,6 +67,20 @@ func (a *Install) Run(params InstallRunParams, nstype model.NamespaceType) error
 	ns := model.NewNamespacePkgOrBundle(language, nstype)
 
 	return executePackageOperation(a.proj, a.cfg, a.out, a.auth, a.Prompter, params.Package.Name(), params.Package.Version(), model.OperationAdded, ns)
+}
+
+func (a *Install) getPackageLanguage(name, version string) (string, error) {
+	ns := model.NewBlankNamespace()
+	results, err := model.SearchIngredients(ns, name)
+	if err != nil {
+		return "", locale.WrapError(err, "package_ingredient_err_search", "Failed to resolve ingredient named: {{.V0}}", name)
+	}
+
+	for _, result := range results {
+		fmt.Println("Result name:", *result.Ingredient.Name)
+		fmt.Println("Result namespace:", result.Namespace)
+	}
+	return "", nil
 }
 
 func (a *Install) addNoProject(name, version string) error {
