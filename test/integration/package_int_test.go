02a7fbdbdbd958710da4e17f05f7010f653c024c
mdrakos <mike.drakos@gmail.com>
2021-07-20 15:08:47 -0700

    Add push between imports

--- test/integration/package_int_test.go
+++ test/integration/package_int_test.go
@@ -312,11 +312,13 @@ func (suite *PackageIntegrationTestSuite) TestPackage_import() {
 		cp := ts.Spawn("import", "requirements.txt")
 		cp.ExpectExitCode(0, time.Second*60)
 
-		suite.Run("already added", func() {
-			cp := ts.Spawn("import", "requirements.txt")
-			cp.ExpectLongString("The following requirement was added, but it already exists in the previous commit. Namespace: language/python, Requirement: Click")
-			cp.ExpectNotExitCode(1, time.Second*60)
-		})
+		cp = ts.Spawn("push")
+		cp.ExpectExitCode(0, time.Second*60)
+
+		cp = ts.Spawn("import", "requirements.txt")
+		cp.Expect("Are you sure you want to do this")
+		cp.Send("n")
+		cp.ExpectNotExitCode(0, time.Second*60)
 	})
 }
 
