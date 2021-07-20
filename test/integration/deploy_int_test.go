9605d5f7cf538fa10dac9ceab37744a08c66aad2
Nathan Rijksen <n.rijksen@gmail.com>
2021-07-20 16:32:50 -0700

    Fix pytest failure

--- test/integration/deploy_int_test.go
+++ test/integration/deploy_int_test.go
@@ -199,7 +199,7 @@ func (suite *DeployIntegrationTestSuite) TestDeployPython() {
 	}
 
 	cp.SendLine("python3 -m pytest --version")
-	cp.Expect("This is pytest version")
+	cp.ExpectRe(`pytest \d+\.\d+\.\d+`)
 
 	if runtime.GOOS != "windows" {
 		// AzureCI has multiple representations for the work directory that
