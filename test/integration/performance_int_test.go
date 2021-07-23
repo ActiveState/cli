96d529d3fb8b2960424db2d84e6d2fc0467ebc82
Nathan Rijksen <n.rijksen@gmail.com>
2021-07-23 10:14:04 -0700

    Add integration test to ensure we `state --version` remains fast

---
+++ test/integration/performance_int_test.go
@@ -0,0 +1,58 @@
+package integration
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/ActiveState/cli/internal/testhelpers/e2e"
+	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
+)
+
+var StateVersionMaxTime = 250 * time.Millisecond
+var StateVersionTotalSamples = 10
+
+type PerformanceIntegrationTestSuite struct {
+	tagsuite.Suite
+}
+
+func (suite *PerformanceIntegrationTestSuite) TestShow() {
+	suite.OnlyRunForTags(tagsuite.Performance)
+	ts := e2e.New(suite.T(), false)
+	defer ts.Close()
+
+	times := []time.Duration{}
+	var total int64
+	for x := 0; x < StateVersionTotalSamples+1; x++ {
+		start := time.Now()
+		cp := ts.Spawn("--version")
+		cp.ExpectExitCode(0)
+		end := time.Since(start)
+		if x == 0 {
+			// Skip the first one as this one will always be slower due to having to wait for state-svc
+			continue
+		}
+		times = append(times, end)
+		total += end.Nanoseconds()
+	}
+
+	var avg = time.Duration(total / int64(StateVersionTotalSamples))
+	if avg.Nanoseconds() > StateVersionMaxTime.Nanoseconds() {
+		suite.FailNow(
+			fmt.Sprintf(`'state --version' is performing poorly!
+Average duration: %s
+Minimum: %s
+Total: %s
+Totals: %v`,
+				avg.String(),
+				StateVersionMaxTime.String(),
+				time.Duration(total).String(),
+				times))
+	}
+}
+
+func TestPerformanceIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(PerformanceIntegrationTestSuite))
+}
