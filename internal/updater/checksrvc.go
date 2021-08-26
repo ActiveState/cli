665627cdfa1b39b905fa630f83475d1591f3cdfe
Martin C Drohmann <martind@activestate.com>
2021-08-26 15:23:06 -0700

    some cleanup

--- internal/updater/checksrvc.go
+++ internal/updater/checksrvc.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ActiveState/cli/internal/logging"
 )
 
+// CheckService checks for updates every 30 minutes in the background, and the most recent update response can be requested
 type CheckService struct {
 	requests chan (chan<- updateResponse)
 	ctx      context.Context
@@ -18,15 +19,7 @@ type updateResponse struct {
 	err error
 }
 
-func refreshUpdate(cfg Configurable) updateResponse {
-	up, err := NewDefaultChecker(cfg).Check()
-	if err != nil {
-		logging.Error("Failed to check for latest update in state-svc: %s", errs.JoinMessage(err))
-	}
-	logging.Debug("Available update result is %v", up)
-	return updateResponse{up, err}
-}
-
+// NewCheckService returns a new CheckService continuously polling for update information
 func NewCheckService(cfg Configurable, ctx context.Context) *CheckService {
 	requests := make(chan (chan<- updateResponse))
 	go func() {
@@ -46,6 +39,7 @@ func NewCheckService(cfg Configurable, ctx context.Context) *CheckService {
 	return &CheckService{requests, ctx}
 }
 
+// LatestUpdate returns the latest update response that has been queried in the background
 func (s *CheckService) LatestUpdate() (*AvailableUpdate, error) {
 	resp := make(chan updateResponse)
 	defer close(resp)
@@ -56,4 +50,13 @@ func (s *CheckService) LatestUpdate() (*AvailableUpdate, error) {
 	}
 	r := <-resp
 	return r.up, r.err
+}
+
+func refreshUpdate(cfg Configurable) updateResponse {
+	up, err := NewDefaultChecker(cfg).Check()
+	if err != nil {
+		logging.Error("Failed to check for latest update in state-svc: %s", errs.JoinMessage(err))
+	}
+	logging.Debug("Available update result is %v", up)
+	return updateResponse{up, err}
 }
