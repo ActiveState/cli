7a09b7f855c74a7cddb31b9bf35489efa148cdda
Nathan Rijksen <n.rijksen@gmail.com>
2021-09-20 10:13:56 -0700

    Fix failing unit test

--- internal/testhelpers/outputhelper/outputer.go
+++ internal/testhelpers/outputhelper/outputer.go
@@ -1,11 +1,16 @@
 package outputhelper
 
-import "github.com/ActiveState/cli/internal/output"
+import (
+	"io"
+
+	"github.com/ActiveState/cli/internal/output"
+)
 
 type TestOutputer struct{}
 
-func (o *TestOutputer) Type() output.Format      { return "" }
-func (o *TestOutputer) Print(value interface{})  {}
-func (o *TestOutputer) Error(value interface{})  {}
-func (o *TestOutputer) Notice(value interface{}) {}
-func (o *TestOutputer) Config() *output.Config   { return nil }
+func (o *TestOutputer) Type() output.Format                   { return "" }
+func (o *TestOutputer) Print(value interface{})               {}
+func (o *TestOutputer) Fprint(w io.Writer, value interface{}) {}
+func (o *TestOutputer) Error(value interface{})               {}
+func (o *TestOutputer) Notice(value interface{})              {}
+func (o *TestOutputer) Config() *output.Config                { return nil }
