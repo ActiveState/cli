238ff7156da9a52d9c7d408a8761a5d10e043bda
Martin C Drohmann <martind@activestate.com>
2021-09-28 09:42:23 -0700

    fix check for executables on windows

--- internal/exeutils/exeutils_windows.go
+++ internal/exeutils/exeutils_windows.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/ActiveState/cli/internal/fileutils"
+	"github.com/thoas/go-funk"
 )
 
 const Extension = ".exe"
@@ -13,7 +14,13 @@ const Extension = ".exe"
 // PathForExecutable returns the first path from the PATH env var for which the executable exists
 func PathForExecutable(executable string) string {
 	candidates := strings.Split(os.Getenv("PATH"), string(os.PathListSeparator))
-	exts := append([]string{""}, strings.Split(os.Getenv("PATHEXT"), string(os.PathListSeparator))...)
+	var exts []string // list of extensions to look for
+	pathexts := strings.Split(os.Getenv("PATHEXT"), string(os.PathListSeparator))
+	// if executable has valid extension for an executable file, we have to check for its existence without appending more extensions
+	if funk.ContainsString(pathexts, filepath.Ext(executable)) {
+		exts = append(exts, "")
+	}
+	exts = append(exts, pathexts...)
 	for _, p := range candidates {
 		for _, ext := range exts {
 			if fileutils.TargetExists(filepath.Join(p, executable+ext)) {
