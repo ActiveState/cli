4c2f82c18e55fd7902aa5922e6ad27d6e5c66b4e
Martin C Drohmann <martind@activestate.com>
2021-09-28 16:01:48 -0700

    fix test

--- internal/exeutils/exeutils_windows_test.go
+++ internal/exeutils/exeutils_windows_test.go
@@ -23,7 +23,7 @@ func Test_PathForExecutables(t *testing.T) {
 	assert.Equal(t, filepath.Join(tmpdir, "state.exe"), findExecutable("state", "/other_path;"+tmpdir, ".COM;.EXE;.BAT"))
 	assert.Equal(t, filepath.Join(tmpdir, "state.EXE"), findExecutable("state.EXE", "/other_path;"+tmpdir, ".COM;.EXE;.BAT"))
 	assert.Equal(t, filepath.Join(tmpdir, "state.exe"), findExecutable("state.exe", "/other_path;"+tmpdir, ".COM;.EXE;.BAT"))
-	assert.Equal(t, "", findExecutable("state", "/other_path;"+tmpdir, ".COM;.EXE;.BAT"))
+	assert.Equal(t, "", findExecutable("state", "/other_path;"+tmpdir, ""))
 	assert.Equal(t, "", findExecutable("non-existent", "/other_path;"+tmpdir, ".COM;.EXE;.BAT"))
 	assert.Equal(t, "", findExecutable("state", "/other_path", ".COM;.EXE;.BAT"))
 }
