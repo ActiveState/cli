317d01b45b0516c0c23b88724e3baed310879855
Nathan Rijksen <n.rijksen@gmail.com>
2021-10-05 15:28:02 -0700

    Update integration tests

--- test/integration/update_int_test.go
+++ test/integration/update_int_test.go
@@ -2,7 +2,6 @@ package integration
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -11,16 +10,13 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/suite"
-	"github.com/thoas/go-funk"
-
 	"github.com/ActiveState/cli/internal/config"
 	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/fileutils"
 	"github.com/ActiveState/cli/internal/rtutils/singlethread"
 	"github.com/ActiveState/cli/internal/testhelpers/e2e"
 	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
-	"github.com/ActiveState/cli/internal/updater"
+	"github.com/stretchr/testify/suite"
 )
 
 type UpdateIntegrationTestSuite struct {
@@ -135,7 +131,8 @@ func (suite *UpdateIntegrationTestSuite) TestUpdate() {
 
 	cp = ts.SpawnWithOpts(e2e.WithArgs("update"), e2e.AppendEnv(suite.env(false, true)...))
 	cp.Expect("Updating State Tool to latest version available")
-	cp.Expect("State Tool has been updated")
+	cp.Expect("Installing Update")
+	cp.Expect("Done")
 	cp.ExpectExitCode(0)
 }
 
@@ -163,12 +160,6 @@ func (suite *UpdateIntegrationTestSuite) TestUpdateChannel() {
 			cp := ts.SpawnCmdWithOpts(ts.SvcExe, e2e.WithArgs("start"), e2e.AppendEnv(suite.env(true, false)...))
 			cp.ExpectExitCode(0)
 
-			before := fileutils.ListDirSimple(ts.Dirs.Config, false)
-
-			info, err := os.Stat(ts.Exe)
-			suite.Require().NoError(err)
-			modTime := info.ModTime()
-
 			updateArgs := []string{"update", "--set-channel", tt.Channel}
 			if tt.Version != "" {
 				updateArgs = append(updateArgs, "--set-version", tt.Version)
@@ -177,32 +168,10 @@ func (suite *UpdateIntegrationTestSuite) TestUpdateChannel() {
 				e2e.WithArgs(updateArgs...),
 				e2e.AppendEnv(suite.env(false, false)...),
 			)
-			if tt.Version == "" {
-				cp.Expect("Updating State Tool to latest version available")
-			} else {
-				cp.Expect("Updating State Tool to version")
-			}
-			cp.Expect(fmt.Sprintf("Version update to %s@", tt.Channel))
+			cp.Expect("Updating State Tool to latest version available")
+			cp.Expect("Installing Update")
+			cp.Expect("Done")
 			cp.ExpectExitCode(0)
-
-			logs := suite.pollForUpdateInBackground(ts.Dirs.Config, before)
-			suite.Assert().Contains(logs, "was successful")
-
-			// Check for state tool executable to be updated
-			updated := false
-			// wait for up to two minutes for the State Tool to get modified
-			for x := 0; x < 600; x++ {
-				info, err := os.Stat(ts.Exe)
-				if errors.Is(err, os.ErrNotExist) {
-					continue
-				}
-				if !info.ModTime().Equal(modTime) {
-					updated = true
-					break
-				}
-				time.Sleep(200 * time.Millisecond)
-			}
-			suite.Require().True(updated, "Timeout: Expected the State Tool to get modified. Output: %s", cp.Snapshot())
 
 			suite.branchCompare(ts, tt.Channel, suite.Equal)
 
@@ -218,8 +187,6 @@ func (suite *UpdateIntegrationTestSuite) TestUpdateTags() {
 	// Disabled, waiting for - https://www.pivotaltracker.com/story/show/179646813
 	suite.T().Skip("Disabled for now")
 	suite.OnlyRunForTags(tagsuite.Update)
-
-	tagName := "experiment"
 
 	tests := []struct {
 		name          string
@@ -237,34 +204,7 @@ func (suite *UpdateIntegrationTestSuite) TestUpdateTags() {
 			// use unique exe
 			ts.UseDistinctStateExes()
 
-			fakeHome := filepath.Join(ts.Dirs.Work, "home")
-			err := fileutils.Mkdir(fakeHome)
-			suite.Require().NoError(err)
-
-			cfg, err := config.NewCustom(ts.Dirs.Config, singlethread.New(), true)
-			suite.Require().NoError(err)
-			defer cfg.Close()
-
-			if tt.tagged {
-				err := cfg.Set(updater.CfgUpdateTag, tagName)
-				suite.Require().NoError(err)
-				suite.Assert().Equal(tagName, cfg.GetString(updater.CfgUpdateTag))
-			}
-
-			before := fileutils.ListDirSimple(ts.Dirs.Config, false)
-			cp := ts.SpawnWithOpts(e2e.WithArgs("update"), e2e.AppendEnv(suite.env(true, true)...), e2e.AppendEnv(fmt.Sprintf("HOME=%s", fakeHome)))
-			cp.Expect("Updating State Tool to latest version available")
-			if tt.expectSuccess {
-				cp.Expect(fmt.Sprintf("Version update to %s@", constants.BranchName))
-				cp.ExpectExitCode(0)
-				logs := suite.pollForUpdateInBackground(ts.Dirs.Config, before)
-				suite.Assert().Contains(logs, "was successful")
-				suite.Assert().Equal("experiment", cfg.GetString(updater.CfgUpdateTag))
-				suite.versionCompare(ts, constants.Version, suite.NotEqual)
-			} else {
-				cp.ExpectLongString("404 Not Found")
-				cp.ExpectExitCode(1)
-			}
+			// ..
 		})
 	}
 }
@@ -291,8 +231,6 @@ func (suite *UpdateIntegrationTestSuite) TestAutoUpdate() {
 	cp := ts.SpawnCmdWithOpts(ts.SvcExe, e2e.WithArgs("start"), e2e.AppendEnv(suite.env(false, true)...))
 	cp.ExpectExitCode(0)
 
-	before := fileutils.ListDirSimple(ts.Dirs.Config, false)
-
 	fakeHome := filepath.Join(ts.Dirs.Work, "home")
 	suite.Require().NoError(fileutils.Mkdir(fakeHome))
 
@@ -305,46 +243,6 @@ func (suite *UpdateIntegrationTestSuite) TestAutoUpdate() {
 		e2e.AppendEnv("VERBOSE=true"))
 	cp.Expect("Auto Update")
 	cp.Expect("Updating State Tool")
+	cp.Expect("Update installed")
 	cp.ExpectExitCode(0)
-
-	suite.pollForUpdateInBackground(ts.Dirs.Config, before)
-}
-
-func (suite *UpdateIntegrationTestSuite) pollForUpdateInBackground(configDir string, beforeFiles []string) string {
-	for i := 0; i < 10; i++ {
-		after := fileutils.ListDirSimple(configDir, false)
-		onlyAfter, _ := funk.Difference(after, beforeFiles)
-		logFile, ok := funk.FindString(onlyAfter.([]string), func(s string) bool { return strings.HasPrefix(filepath.Base(s), "state-installer") })
-		if ok {
-			return suite.pollForUpdateFromLogfile(logFile)
-		}
-		time.Sleep(200 * time.Millisecond)
-	}
-	return ""
-}
-
-func (suite *UpdateIntegrationTestSuite) pollForUpdateFromLogfile(logFile string) string {
-	var logs []byte
-	// poll for successful auto-update
-	for i := 0; i < 60; i++ {
-		time.Sleep(time.Millisecond * 200)
-
-		var err error
-		logs, err = ioutil.ReadFile(logFile)
-		if errors.Is(err, os.ErrNotExist) {
-			continue
-		}
-		suite.NoError(err)
-		if strings.Contains(string(logs), "was successful") || strings.Contains(string(logs), "Installation failed") {
-			return string(logs)
-		}
-	}
-
-	if !fileutils.FileExists(logFile) {
-		suite.T().Errorf("logFile does not exist: %s", logFile)
-	} else {
-		suite.T().Errorf("could not verify logFile contents at %s, contents:\n%s", logFile, string(logs))
-	}
-
-	return ""
 }
