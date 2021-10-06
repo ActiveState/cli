f4e0e567a4d1129f78f21e36509e7fdbf6a8a563
Nathan Rijksen <n.rijksen@gmail.com>
2021-10-06 16:52:36 -0700

    Fix updating from custom state tool locations

--- internal/updater/updater.go
+++ internal/updater/updater.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ActiveState/cli/internal/errs"
 	"github.com/ActiveState/cli/internal/exeutils"
 	"github.com/ActiveState/cli/internal/fileutils"
+	"github.com/ActiveState/cli/internal/installation"
 	"github.com/ActiveState/cli/internal/installation/storage"
 	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/logging"
@@ -81,6 +82,13 @@ func (u *AvailableUpdate) prepareInstall(installTargetPath string, args []string
 	logging.Debug("Using installer: %s", installerPath)
 	if !fileutils.FileExists(installerPath) {
 		return "", nil, errs.Wrap(err, "Downloaded update does not have installer")
+	}
+
+	if installTargetPath == "" {
+		installTargetPath, err = installation.InstallPath()
+		if err != nil {
+			return "", nil, errs.Wrap(err, "Could not detect install path")
+		}
 	}
 
 	args = append(args, "--source-path", sourcePath)
