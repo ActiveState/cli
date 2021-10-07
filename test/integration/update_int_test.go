5b98af181b97ab2d2bf4a333b9859fe2dda303df
Nathan Rijksen <n.rijksen@gmail.com>
2021-10-07 13:15:02 -0700

    Don't test updating for release while on v0.29

--- test/integration/update_int_test.go
+++ test/integration/update_int_test.go
@@ -300,6 +300,10 @@ func (suite *UpdateIntegrationTestSuite) TestAutoUpdateToCurrent() {
 }
 
 func (suite *UpdateIntegrationTestSuite) TestUpdateToCurrent() {
+	if strings.HasSuffix(constants.Version, "0.30") {
+		// Feel free to drop this once the release channel is no longer on 0.29
+		suite.T().Skip("Updating from release 0.29 to 0.30 is not covered due to how 0.29 did updates (async)")
+	}
 	suite.OnlyRunForTags(tagsuite.Update, tagsuite.Critical)
 
 	ts := e2e.New(suite.T(), true)
