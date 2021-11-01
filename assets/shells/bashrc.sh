e1b5accb2b369b99260cb3eec9ea83103e1bbd75
daved <daved@codemodus.com>
2021-11-01 11:28:44 -0700

    Remove recent bashrc asset changes

--- assets/shells/bashrc.sh
+++ assets/shells/bashrc.sh
@@ -1,14 +1,12 @@
 if [ -f ~/.bashrc ]; then source ~/.bashrc; fi
 
-{{ if .Owner }}
+{{if ne .Owner ""}}
 if [ -z "$PROMPT_COMMAND" ]; then
   export PS1="[{{.Owner}}/{{.Name}}] $PS1"
 fi
 {{end}}
 
-{{ if .WD }}
 cd "{{.WD}}"
-{{ end }}
 
 {{- range $K, $V := .Env}}
 {{- if eq $K "PATH"}}
@@ -29,11 +27,6 @@ function {{$K}} {
 export -f {{$K}}
 {{end}}
 
-{{ if .ActivatedMessage }}
 echo "{{.ActivatedMessage}}"
-{{ end }}
 
-{{ if .UserScripts }}
 {{.UserScripts}}
-{{ end }}
-cat ${BASH_SOURCE}
