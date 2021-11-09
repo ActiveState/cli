b8ae78a05f5b18cf5ddf58ee0551bd6a327d817f
Nathan Rijksen <n.rijksen@gmail.com>
2021-11-09 14:57:34 -0800

    Fix compilation of state installer

--- cmd/state-installer/cmd.go
+++ cmd/state-installer/cmd.go
@@ -124,7 +124,7 @@ func main() {
 		"state-installer",
 		"",
 		"Installs or updates the State Tool",
-		primer.New(nil, out, nil, nil, nil, nil, cfg, nil, an),
+		primer.New(nil, out, nil, nil, nil, nil, cfg, nil, nil, an),
 		[]*captain.Flag{ // The naming of these flags is slightly inconsistent due to backwards compatibility requirements
 			{
 				Name:        "channel",
