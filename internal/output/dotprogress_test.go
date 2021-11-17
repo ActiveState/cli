71670f915412986c5627b459986daad550b0351e
Nathan Rijksen <n.rijksen@gmail.com>
2021-11-17 14:38:32 -0800

    Make test less sensitive to race conditions

--- internal/output/dotprogress_test.go
+++ internal/output/dotprogress_test.go
@@ -13,12 +13,12 @@ import (
 
 func Test_Dotprogress(t *testing.T) {
 	out := outputhelper.NewCatcher()
-	interval := 10 * time.Millisecond
-	noIntervals := 10
+	interval := 1 * time.Millisecond
+	noIntervals := 100
 	sleepTime := time.Duration(noIntervals+1) * interval
 	dp := output.NewDotProgress(out, "Progress", interval)
 	time.Sleep(sleepTime)
 	dp.Stop("Done")
-	dots := strings.Repeat(".", noIntervals)
+	dots := strings.Repeat(".", (noIntervals / 20)) // To avoid race conditions we're only counting half the supposed dots
 	require.Regexp(t, regexp.MustCompile("Progress..."+dots+"\\.* Done"), out.ErrorOutput())
 }
