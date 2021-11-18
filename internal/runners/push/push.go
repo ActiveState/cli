61b95f3e21b3deeb02b0d048e5cd168347cfa353
Mike Drakos <mike.drakos@gmail.com>
2021-11-18 10:13:03 -0800

    Add back PT comment

--- internal/runners/push/push.go
+++ internal/runners/push/push.go
@@ -164,6 +164,7 @@ func (r *Push) Run(params PushParams) error {
 	// Detect the target branch
 	var branch *mono_models.Branch
 	if projectCreated || r.project.BranchName() == "" {
+		// https://www.pivotaltracker.com/story/show/176806415
 		// If we have created an empty project the only existing branch will be the default one
 		branch, err = model.DefaultBranchForProject(targetPjm)
 		if err != nil {
