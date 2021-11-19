b41e09ebcc71c417e0ae5bf00743fb85672a95cd
Nathan Rijksen <n.rijksen@gmail.com>
2021-11-19 11:11:12 -0800

    Kill process instead, and timeout after 1 second

--- internal/rtutils/rtutils_test.go
+++ internal/rtutils/rtutils_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -32,4 +33,27 @@ func Test_Closer(t *testing.T) {
 	require.Error(t, err)
 	assert.True(t, strings.HasPrefix(err.Error(), closerErr.Error()))
 	assert.False(t, errors.Is(err, returnErr))
+}
+
+func TestTimeout(t *testing.T) {
+	err := Timeout(func() error {
+		time.Sleep(time.Millisecond * 20)
+		return nil
+	}, time.Millisecond*10)
+	require.True(t, errors.Is(err, ErrTimeout), "Should return timeout error, actual: %v", err)
+
+	v := false
+	err = Timeout(func() error {
+		v = true
+		return nil
+	}, time.Millisecond*10)
+	require.NoError(t, err)
+	require.True(t, v, "Value should've been modified")
+
+	expected := fmt.Errorf("I'm an error")
+	err = Timeout(func() error {
+		return expected
+	}, time.Millisecond*10)
+	require.Error(t, err)
+	require.Equal(t, expected, err)
 }
