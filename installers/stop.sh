6cf031d07fa602090463bc7060802cfc2b5cc45c
Nathan Rijksen <n.rijksen@gmail.com>
2022-02-02 16:32:02 -0800

    Ensure we are killing the right process

--- installers/stop.sh
+++ installers/stop.sh
@@ -13,6 +13,17 @@ if ! [ "$response" != "${response#[Yy]}" ] ;then
 fi
 
 echo "Stopping running State Tool processes"
-ps ax | grep -i "state-installer\|activestate.*state" | awk '{ print $1;}' | xargs kill -9
+# Because POSIX doesn't always give the executable path of a process we have to get a little creative with lsof here.
+# Which isn't as reliable as checking the executable path because it gives all files in use by the process.
+# Therefore we also check that the en-us.yaml is in use, which helps build confidence that we are killing the right process.
+for PID in `ps ax | grep -i "state-installer\|state" | grep -v "grep" | awk '{ print $1;}'`; do
+  LSOUT=`lsof -p $PID`
+  if [ "`echo $LSOUT | grep -i "activestate"`" != "" ]; then
+    if [ "`basename $(ps -p $PID -o comm=)`" == "state" ] && [ "`echo $LSOUT | grep -i "en-us.yaml"`" == "" ]; then
+      continue
+    fi
+    kill -9 $PID
+  fi
+done
 
 echo "Done"
