973130077f7d1c34574628440ddbc4f7ef21244b
mdrakos <mike.drakos@gmail.com>
2022-02-04 08:36:08 -0800

    Revert "Don't use init func"
    This reverts commit b2300a6fc854ac26c3f7a3ad39b916faa043c237.

--- internal/mediators/config/rules.go
+++ internal/mediators/config/rules.go
@@ -24,7 +24,7 @@ var defaultRule = Rule{String, EmptyEvent, EmptyEvent}
 
 type Rules map[string]Rule
 
-var rules = make(Rules)
+var rules Rules
 
 func GetRule(key string) Rule {
 	rule, ok := rules[key]
@@ -40,4 +40,8 @@ func NewRule(key string, t Type, get Event, set Event) {
 
 func SetRule(key string, rule Rule) {
 	rules[key] = rule
+}
+
+func init() {
+	rules = make(Rules)
 }
