b176723e9528d7b06834d08b30712380467da8ce
Mike Drakos <mike.drakos@gmail.com>
2022-03-14 14:15:55 -0700

    Add use command

---
+++ cmd/state/internal/cmdtree/use.go
@@ -0,0 +1,36 @@
+package cmdtree
+
+import (
+	"github.com/ActiveState/cli/internal/captain"
+	"github.com/ActiveState/cli/internal/locale"
+	"github.com/ActiveState/cli/internal/primer"
+	"github.com/ActiveState/cli/internal/runners/use"
+	"github.com/ActiveState/cli/pkg/project"
+)
+
+func newUseCommand(prime *primer.Values) *captain.Command {
+	runner := use.NewUse(prime)
+
+	params := &use.Params{
+		Namespace: &project.Namespaced{},
+	}
+
+	cmd := captain.NewCommand(
+		"use",
+		"",
+		"Switch to using the given project",
+		prime,
+		[]*captain.Flag{},
+		[]*captain.Argument{
+			{
+				Name:        locale.T("arg_state_activate_namespace"),
+				Description: locale.T("arg_state_activate_namespace_description"),
+				Value:       params.Namespace,
+			},
+		},
+		func(_ *captain.Command, _ []string) error {
+			return runner.Run(params)
+		},
+	).SetGroup(EnvironmentGroup)
+	return cmd
+}
---
+++ internal/runners/use/use.go
@@ -0,0 +1,175 @@
+package use
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/ActiveState/cli/internal/analytics"
+	"github.com/ActiveState/cli/internal/config"
+	"github.com/ActiveState/cli/internal/constants"
+	"github.com/ActiveState/cli/internal/errs"
+	"github.com/ActiveState/cli/internal/fileutils"
+	"github.com/ActiveState/cli/internal/globaldefault"
+	"github.com/ActiveState/cli/internal/installation/storage"
+	"github.com/ActiveState/cli/internal/locale"
+	"github.com/ActiveState/cli/internal/logging"
+	"github.com/ActiveState/cli/internal/osutils"
+	"github.com/ActiveState/cli/internal/output"
+	"github.com/ActiveState/cli/internal/primer"
+	"github.com/ActiveState/cli/internal/prompt"
+	"github.com/ActiveState/cli/internal/runbits"
+	"github.com/ActiveState/cli/internal/subshell"
+	"github.com/ActiveState/cli/internal/svcmanager"
+	"github.com/ActiveState/cli/pkg/cmdlets/checker"
+	"github.com/ActiveState/cli/pkg/platform/authentication"
+	"github.com/ActiveState/cli/pkg/platform/model"
+	"github.com/ActiveState/cli/pkg/platform/runtime"
+	"github.com/ActiveState/cli/pkg/platform/runtime/target"
+	"github.com/ActiveState/cli/pkg/project"
+	"github.com/ActiveState/cli/pkg/projectfile"
+)
+
+type Params struct {
+	// TODO: Make owner optional?
+	Namespace *project.Namespaced
+}
+
+type primeable interface {
+	primer.Auther
+	primer.Outputer
+	primer.Projecter
+	primer.Subsheller
+	primer.Prompter
+	primer.Configurer
+	primer.Svcer
+	primer.SvcModeler
+	primer.Analyticer
+}
+
+type Use struct {
+	auth      *authentication.Auth
+	out       output.Outputer
+	svcMgr    *svcmanager.Manager
+	svcModel  *model.SvcModel
+	config    *config.Instance
+	proj      *project.Project
+	subshell  subshell.SubShell
+	prompt    prompt.Prompter
+	analytics analytics.Dispatcher
+}
+
+func NewUse(prime primeable) *Use {
+	return &Use{
+		prime.Auth(),
+		prime.Output(),
+		prime.SvcManager(),
+		prime.SvcModel(),
+		prime.Config(),
+		prime.Project(),
+		prime.Subshell(),
+		prime.Prompt(),
+		prime.Analytics(),
+	}
+}
+
+func (u *Use) Run(params *Params) error {
+	logging.Debug("Use %v", params.Namespace)
+
+	checker.RunUpdateNotifier(u.svcMgr, u.config, u.out)
+
+	path, err := u.getProjectPath(params.Namespace)
+	if err != nil {
+		return locale.WrapError(err, "err_use_get_project_path", "Could not get path for project")
+	}
+
+	proj, err := initProject(params.Namespace, path)
+	if err != nil {
+		return locale.WrapError(err, "err_use_init_project", "Could not initialize project")
+	}
+
+	rt, err := runtime.New(target.NewProjectTarget(proj, storage.CachePath(), nil, target.TriggerActivate), u.analytics, u.svcModel)
+	if err != nil {
+		if !runtime.IsNeedsUpdateError(err) {
+			return locale.WrapError(err, "err_activate_runtime", "Could not initialize a runtime for this project.")
+		}
+
+		eh, err := runbits.ActivateRuntimeEventHandler(u.out)
+		if err != nil {
+			return locale.WrapError(err, "err_initialize_runtime_event_handler")
+		}
+
+		if err = rt.Update(u.auth, eh); err != nil {
+			if errs.Matches(err, &model.ErrOrderAuth{}) {
+				return locale.WrapInputError(err, "err_update_auth", "Could not update runtime, if this is a private project you may need to authenticate with `[ACTIONABLE]state auth[/RESET]`")
+			}
+			return locale.WrapError(err, "err_update_runtime", "Could not update runtime installation.")
+		}
+	}
+
+	if err := globaldefault.SetupDefaultActivation(u.subshell, u.config, rt, filepath.Dir(proj.Source().Path())); err != nil {
+		return locale.WrapError(err, "err_activate_default", "Could not configure your project as the default.")
+	}
+
+	u.out.Print(fmt.Sprintf(`[NOTICE]Switched to[/RESET] [ACTIONABLE]%s[/RESET]`, params.Namespace.Project))
+
+	u.out.Print("Note you may need to run '[ACTIONABLE]hash -r[/RESET]' to reset your environment.")
+
+	if err := u.config.Set("projects.active", proj.Namespace().String()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *Use) getProjectPath(namespace *project.Namespaced) (string, error) {
+	potentialPaths := projectfile.GetProjectPaths(u.config, namespace.String())
+	if len(potentialPaths) > 0 {
+		return potentialPaths[0], nil
+	}
+
+	dir, err := osutils.Getwd()
+	if err != nil {
+		return "", locale.WrapError(err, "err_use_get_wd", "Could not get current working directory")
+	}
+
+	return filepath.Join(dir, namespace.Project), nil
+}
+
+func initProject(ns *project.Namespaced, dir string) (*project.Project, error) {
+	if fileutils.TargetExists(filepath.Join(dir, constants.ConfigFileName)) {
+		return project.FromPath(dir)
+	}
+
+	pj, err := model.FetchProjectByName(ns.Owner, ns.Project)
+	if err != nil {
+		return nil, err
+	}
+
+	branch, err := model.DefaultBranchForProject(pj)
+	if err != nil {
+		return nil, errs.Wrap(err, "Could not grab branch for project")
+	}
+	branchName := branch.Label
+
+	commitID := ns.CommitID
+	if commitID == nil {
+		commitID = branch.CommitID
+	}
+
+	if commitID == nil {
+		return nil, errs.New("commitID is nil")
+	}
+
+	pf, err := projectfile.Create(&projectfile.CreateParams{
+		Owner:      ns.Owner,
+		Project:    ns.Project,
+		CommitID:   commitID,
+		BranchName: branchName,
+		Directory:  dir,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return project.New(pf, nil)
+}
