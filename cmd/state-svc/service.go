ee348ebaedcc5a0f496d79abd53fca933dce3e2a
daved <daved@codemodus.com>
2022-04-06 17:06:34 -0700

    Remove stray dot from port data in service setup

--- cmd/state-svc/service.go
+++ cmd/state-svc/service.go
@@ -49,7 +49,7 @@ func (s *service) Start() error {
 
 	spath := svcctl.NewIPCSockPathFromGlobals()
 	reqHandlers := []ipc.RequestHandler{ // caller-defined handlers to expand ipc capabilities
-		svcctl.HTTPAddrHandler(".:" + strconv.Itoa(s.server.Port())),
+		svcctl.HTTPAddrHandler(":" + strconv.Itoa(s.server.Port())),
 	}
 	s.ipcSrv = ipc.NewServer(spath, reqHandlers...)
 	err = s.ipcSrv.Start()
