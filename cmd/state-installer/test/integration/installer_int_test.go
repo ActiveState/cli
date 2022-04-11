46ccaec89536ebd66f47e00b757a45dfd9d15669
mitchell <mitchellb@activestate.com>
2022-04-11 10:48:20 -0400

    Attempt to fix installer integration tests.
    The --source-path option is hidden with a comment "Source path should ideally only be used through state tool updates (ie. it's internally routed)".

--- cmd/state-installer/test/integration/installer_int_test.go
+++ cmd/state-installer/test/integration/installer_int_test.go
@@ -35,7 +35,7 @@ func (suite *InstallerIntegrationTestSuite) TestInstallFromLocalSource() {
 		e2e.AppendEnv(constants.DisableUpdates+"=false"))
 
 	// Assert output
-	cp.Expect("Installing State Tool")
+	cp.Expect("Installing Update")
 	cp.Expect("Done")
 	cp.Expect("successfully installed")
 	suite.NotContains(cp.TrimmedSnapshot(), "Downloading State Tool")
