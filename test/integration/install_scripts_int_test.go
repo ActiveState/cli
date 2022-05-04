32fed115ffddbee47a123ac3b2449a85205d1a62
Mike Drakos <mike.drakos@gmail.com>
2022-05-04 13:29:36 -0700

    Use bin dir as base

--- test/integration/install_scripts_int_test.go
+++ test/integration/install_scripts_int_test.go
@@ -120,7 +120,7 @@ func (suite *InstallScriptsIntegrationTestSuite) TestInstall() {
 
 			cp.ExpectExitCode(0)
 
-			state, err := installation.NewAppInfoInDir(installDir, installation.StateApp)
+			state, err := installation.NewAppInfoInDir(filepath.Join(installDir, "bin"), installation.StateApp)
 			require.NoError(suite.T(), err)
 			suite.FileExists(state.Exec())
 
@@ -247,7 +247,7 @@ func (suite *InstallScriptsIntegrationTestSuite) assertCorrectVersion(ts *e2e.Se
 		Branch  string `json:"branch"`
 	}
 
-	state, err := installation.NewAppInfoInDir(installDir, installation.StateApp)
+	state, err := installation.NewAppInfoInDir(filepath.Join(installDir, "bin"), installation.StateApp)
 	require.NoError(suite.T(), err)
 	cp := ts.SpawnCmd(state.Exec(), "--version", "--output=json")
 	cp.ExpectExitCode(0)
