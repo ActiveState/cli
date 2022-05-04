01b09c617e78971c61ef4b0cc55674afba71248c
Mike Drakos <mike.drakos@gmail.com>
2022-05-04 14:25:54 -0700

    Initialize info with bin path

--- cmd/state-installer/cmd.go
+++ cmd/state-installer/cmd.go
@@ -354,16 +354,17 @@ func postInstallEvents(out output.Outputer, cfg *config.Instance, an analytics.D
 		return errs.Wrap(err, "Could not resolve installation path")
 	}
 
-	stateInfo, err := installation.NewAppInfoInDir(installPath, installation.StateApp)
+	binPath, err := installation.BinPathFromInstallPath(installPath)
+	if err != nil {
+		return errs.Wrap(err, "Could not detect installation bin path")
+	}
+
+	stateInfo, err := installation.NewAppInfoInDir(binPath, installation.StateApp)
 	if err != nil {
 		return locale.WrapError(err, "err_state_info")
 	}
 
 	stateExe := stateInfo.Exec()
-	binPath, err := installation.BinPathFromInstallPath(installPath)
-	if err != nil {
-		return errs.Wrap(err, "Could not detect installation bin path")
-	}
 
 	// Execute requested command, these are mutually exclusive
 	switch {
