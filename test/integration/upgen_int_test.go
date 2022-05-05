f634f1c901665e5b6d6490fa4351ebb4a30263b8
Mike Drakos <mike.drakos@gmail.com>
2022-05-05 12:02:18 -0700

    Dont' use appinfo in upgen test

--- test/integration/upgen_int_test.go
+++ test/integration/upgen_int_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/environment"
-	"github.com/ActiveState/cli/internal/installation"
+	"github.com/ActiveState/cli/internal/osutils"
 	"github.com/ActiveState/cli/internal/testhelpers/e2e"
 	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
 	"github.com/ActiveState/termtest"
@@ -52,9 +52,7 @@ func (suite *UpdateGenIntegrationTestSuite) TestUpdateBits() {
 
 	cp.ExpectExitCode(0)
 
-	state, err := installation.NewAppInfoInDir(filepath.Join(tempPath, constants.ToplevelInstallArchiveDir, "bin"), installation.StateApp)
-	suite.Require().NoError(err)
-	cp = ts.SpawnCmd(state.Exec(), "--version")
+	cp = ts.SpawnCmd(filepath.Join(tempPath, constants.ToplevelInstallArchiveDir, "bin", constants.StateCmd+osutils.ExeExt), "--version")
 	cp.Expect(constants.RevisionHashShort)
 	cp.ExpectExitCode(0)
 }
