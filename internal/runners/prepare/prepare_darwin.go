c80926f8b58291de2da179d6a7c9b12db1ea1149
mdrakos <mike.drakos@gmail.com>
2022-05-06 11:30:48 -0700

    Fix macOS build errors

--- internal/runners/prepare/prepare_darwin.go
+++ internal/runners/prepare/prepare_darwin.go
@@ -1,6 +1,7 @@
 package prepare
 
 import (
+	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/installation"
 	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/multilog"
@@ -14,11 +15,11 @@ func (r *Prepare) prepareOS() error {
 // InstalledPreparedFiles returns the files installed by the prepare command
 func InstalledPreparedFiles(cfg autostart.Configurable) ([]string, error) {
 	var files []string
-	trayInfo, err := installation.NewAppInfo(installation.TrayApp)
+	trayExec, err := installation.NewExec(installation.TrayApp)
 	if err != nil {
 		return nil, locale.WrapError(err, "err_tray_info")
 	}
-	name, exec := trayInfo.Name(), trayInfo.Exec()
+	name, exec := constants.TrayAppName, trayExec
 
 	sc, err := autostart.New(name, exec, cfg).Path()
 	if err != nil {
