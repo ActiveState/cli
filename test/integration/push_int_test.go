2223ec70bede1c51370e258508a9863c7e1f72e0
Nathan Rijksen <n.rijksen@gmail.com>
2022-05-09 13:01:04 -0700

    Use package that won't fail

--- test/integration/push_int_test.go
+++ test/integration/push_int_test.go
@@ -39,9 +39,9 @@ func (suite *PushIntegrationTestSuite) SetupSuite() {
 	suite.extraPackage2 = "DateTime"
 	if runtime.GOOS == "darwin" {
 		suite.language = "python3"
-		suite.languageFull = suite.language
+		suite.languageFull = "python3@3.10.1"
 		suite.baseProject = "ActiveState-CLI/small-python"
-		suite.extraPackage = "six@1.10.0"
+		suite.extraPackage = "bzip@1.0.8"
 	}
 }
 
