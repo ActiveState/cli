8cf8ac64ab68434bae86d1f5b23677c313151aee
Nathan Rijksen <n.rijksen@gmail.com>
2022-05-09 10:01:26 -0700

    Update assertion

--- test/integration/komodo_int_test.go
+++ test/integration/komodo_int_test.go
@@ -196,7 +196,7 @@ func (suite *ScriptsIntegrationTestSuite) TestScripts_EditorV0() {
 	suite.setupConfigFile(ts)
 
 	cp := ts.Spawn("scripts", "--output", "editor.v0")
-	cp.Expect(`[{"name":"first-script"},{"name":"second-script"},{"name":"super-script"}]`)
+	cp.Expect(`[{"name":"first-script"},{"name":"second-script"},{"name":"super-script"},{"name":"testenv"}]`)
 	cp.ExpectExitCode(0)
 }
 
