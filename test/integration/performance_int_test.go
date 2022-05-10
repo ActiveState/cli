cbd9a25e40b3ec882efdc5f9b4a191b04e93c123
Nathan Rijksen <n.rijksen@gmail.com>
2022-05-10 12:05:28 -0700

    Debug performance

--- test/integration/performance_int_test.go
+++ test/integration/performance_int_test.go
@@ -2,8 +2,6 @@ package integration
 
 import (
 	"fmt"
-	"regexp"
-	"strconv"
 	"testing"
 	"time"
 
@@ -17,14 +15,14 @@ import (
 
 // The max time is based on the average execution times across platforms at the time that this was configured
 // Increasing this should be a LAST RESORT
-var StateVersionMaxTime = 100 * time.Millisecond // DO NOT CHANGE WITHOUT DISCUSSION WITH THE TEAM
+var StateVersionMaxTime = 350 * time.Millisecond // DO NOT CHANGE WITHOUT DISCUSSION WITH THE TEAM
 var StateVersionTotalSamples = 10
 
 type PerformanceIntegrationTestSuite struct {
 	tagsuite.Suite
 }
 
-func (suite *PerformanceIntegrationTestSuite) TestVersionPerformance() {
+func (suite *PerformanceIntegrationTestSuite) TestShow() {
 	suite.OnlyRunForTags(tagsuite.Performance)
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
@@ -33,39 +31,27 @@ func (suite *PerformanceIntegrationTestSuite) TestVersionPerformance() {
 	stdout, stderr, err := exeutils.ExecSimple(ts.SvcExe, []string{"start"}, []string{})
 	suite.Require().NoError(err, fmt.Sprintf("Full error:\n%v\nstdout:\n%s\nstderr:\n%s", errs.JoinMessage(err), stdout, stderr))
 
-	rx := regexp.MustCompile(`Profiling: main took .*\((\d+)\)`)
-
-	var firstEntry, firstStateLog, firstSvcLog string
+	var firstEntry string
 	times := []time.Duration{}
-	var total time.Duration
+	var total int64
 	for x := 0; x < StateVersionTotalSamples+1; x++ {
-		cp := ts.SpawnWithOpts(
-			e2e.WithArgs("--version"),
-			e2e.AppendEnv("ACTIVESTATE_CLI_DISABLE_UPDATES=true", "ACTIVESTATE_PROFILE=true"))
-		cp.ExpectExitCode(0)
-		v := rx.FindStringSubmatch(cp.Snapshot())
-		if len(v) < 2 {
-			suite.T().Fatalf("Could not find '%s' in output: %s", rx.String(), cp.Snapshot())
-		}
-		durMS, err := strconv.Atoi(v[1])
-		suite.Require().NoError(err)
-		dur := time.Millisecond * time.Duration(durMS)
-
+		start := time.Now()
+		stdout, stderr, err := exeutils.ExecSimple(ts.Exe, []string{"--version"}, []string{"ACTIVESTATE_CLI_DISABLE_UPDATES=true", "ACTIVESTATE_PROFILE=true"})
+		suite.Require().NoError(err, fmt.Sprintf("Full error:\n%v\nstdout:\n%s\nstderr:\n%s", errs.JoinMessage(err), stdout, stderr))
+		end := time.Since(start)
 		if firstEntry == "" {
-			firstEntry = cp.Snapshot()
-			firstStateLog = ts.MostRecentStateLog()
-			firstSvcLog = ts.SvcLog()
+			firstEntry = stdout
 		}
 		if x == 0 {
 			// Skip the first one as this one will always be slower due to having to wait for state-svc
 			continue
 		}
-		times = append(times, dur)
-		total = total + dur
+		times = append(times, end)
+		total += end.Nanoseconds()
 	}
 
-	var avg = total / time.Duration(StateVersionTotalSamples)
-	if avg.Milliseconds() > StateVersionMaxTime.Milliseconds() {
+	var avg = time.Duration(total / int64(StateVersionTotalSamples))
+	if avg.Nanoseconds() > StateVersionMaxTime.Nanoseconds() {
 		suite.FailNow(
 			fmt.Sprintf(`'state --version' is performing poorly!
 Average duration: %s
@@ -74,20 +60,12 @@ Total: %s
 Totals: %v
 
 Output of first run:
-%s
-
-State Tool log:
-%s
-
-Svc log:
 %s`,
 				avg.String(),
 				StateVersionMaxTime.String(),
 				time.Duration(total).String(),
 				times,
-				firstEntry,
-				firstStateLog,
-				firstSvcLog))
+				firstEntry))
 	}
 }
 
