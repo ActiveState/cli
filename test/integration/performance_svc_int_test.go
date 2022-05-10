b003a3b730d1f1eacc035736268070a3d328b304
Nathan Rijksen <n.rijksen@gmail.com>
2022-05-10 13:39:45 -0700

    Performance test improvements

--- test/integration/performance_svc_int_test.go
+++ test/integration/performance_svc_int_test.go
@@ -6,8 +6,7 @@ import (
 	"time"
 
 	"github.com/ActiveState/cli/internal/errs"
-	"github.com/ActiveState/cli/internal/installation/storage"
-	"github.com/ActiveState/cli/internal/logging"
+	"github.com/ActiveState/cli/internal/installation"
 	"github.com/ActiveState/cli/internal/svcctl"
 	"github.com/ActiveState/cli/internal/testhelpers/e2e"
 	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
@@ -18,13 +17,13 @@ import (
 
 var SvcEnsureStartMaxTime = 1000 * time.Millisecond // https://activestatef.atlassian.net/browse/DX-935
 var SvcRequestMaxTime = 50 * time.Millisecond
-var SvcStopMaxTime = 50 * time.Millisecond
+var SvcStopMaxTime = 10 * time.Millisecond
 
 type PerformanceSvcIntegrationTestSuite struct {
 	tagsuite.Suite
 }
 
-func (suite *PerformanceIntegrationTestSuite) TearDownSuite() {
+func (suite *PerformanceIntegrationTestSuite) AfterTest(suiteName, testName string) {
 	ipcClient := svcctl.NewDefaultIPCClient()
 	err := svcctl.StopServer(ipcClient)
 	suite.Require().NoError(err)
@@ -34,20 +33,16 @@ func (suite *PerformanceIntegrationTestSuite) TestSvcPerformance() {
 	suite.OnlyRunForTags(tagsuite.Performance)
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
-
-	// This integration test is a bit special because it bypasses the spawning logic
-	// so in order to get the right log files when debugging we manually provide the config dir
-	var err error
-	ts.Dirs.Config, err = storage.AppDataPath()
-	suite.Require().NoError(err)
-
 	ipcClient := svcctl.NewDefaultIPCClient()
 	var svcPort string
-	
+
 	suite.Run("StartServer", func() {
+		svcExec, err := installation.ServiceExecFromDir(ts.Dirs.Bin)
+		suite.Require().NoError(err, errs.JoinMessage(err))
+
 		t := time.Now()
-		svcPort, err = svcctl.EnsureExecStartedAndLocateHTTP(ipcClient, ts.SvcExe)
-		suite.Require().NoError(err, ts.DebugMessage(fmt.Sprintf("Error: %s\nLog Tail:\n%s", errs.JoinMessage(err), logging.ReadTail())))
+		svcPort, err = svcctl.EnsureExecStartedAndLocateHTTP(ipcClient, svcExec)
+		suite.Require().NoError(err, errs.JoinMessage(err))
 		duration := time.Since(t)
 
 		if duration.Nanoseconds() > SvcEnsureStartMaxTime.Nanoseconds() {
@@ -55,13 +50,11 @@ func (suite *PerformanceIntegrationTestSuite) TestSvcPerformance() {
 		}
 	})
 
-	svcmodel := model.NewSvcModel(svcPort)
-	svcmodel.EnableDebugLog()
-
 	suite.Run("Query StateVersion", func() {
 		t := time.Now()
+		svcmodel := model.NewSvcModel(svcPort)
 		_, err := svcmodel.StateVersion(context.Background())
-		suite.Require().NoError(err, ts.DebugMessage(fmt.Sprintf("Error: %s\nLog Tail:\n%s", errs.JoinMessage(err), logging.ReadTail())))
+		suite.Require().NoError(err, errs.JoinMessage(err))
 		duration := time.Since(t)
 
 		if duration.Nanoseconds() > SvcRequestMaxTime.Nanoseconds() {
@@ -71,34 +64,24 @@ func (suite *PerformanceIntegrationTestSuite) TestSvcPerformance() {
 
 	suite.Run("Query Analytics", func() {
 		t := time.Now()
+		svcmodel := model.NewSvcModel(svcPort)
 		err := svcmodel.AnalyticsEvent(context.Background(), "performance-test", "performance-test", "performance-test", "{}")
-		suite.Require().NoError(err, ts.DebugMessage(fmt.Sprintf("Error: %s\nLog Tail:\n%s", errs.JoinMessage(err), logging.ReadTail())))
+		suite.Require().NoError(err, errs.JoinMessage(err))
 		duration := time.Since(t)
 
 		if duration.Nanoseconds() > SvcRequestMaxTime.Nanoseconds() {
-			suite.Fail(fmt.Sprintf("Service analytics request took too long: %s (should be under %s)", duration.String(), SvcEnsureStartMaxTime.String()))
-		}
-	})
-
-	suite.Run("Query Update", func() {
-		t := time.Now()
-		_, err := svcmodel.CheckUpdate(context.Background())
-		suite.Require().NoError(err, ts.DebugMessage(fmt.Sprintf("Error: %s\nLog Tail:\n%s", errs.JoinMessage(err), logging.ReadTail())))
-		duration := time.Since(t)
-
-		if duration.Nanoseconds() > SvcRequestMaxTime.Nanoseconds() {
-			suite.Fail(fmt.Sprintf("Service update request took too long: %s (should be under %s)", duration.String(), SvcEnsureStartMaxTime.String()))
+			suite.Fail(fmt.Sprintf("Service request took too long: %s (should be under %s)", duration.String(), SvcEnsureStartMaxTime.String()))
 		}
 	})
 
 	suite.Run("StopServer", func() {
 		t := time.Now()
 		err := svcctl.StopServer(ipcClient)
-		suite.Require().NoError(err, ts.DebugMessage(fmt.Sprintf("Error: %s\nLog Tail:\n%s", errs.JoinMessage(err), logging.ReadTail())))
+		suite.Require().NoError(err, errs.JoinMessage(err))
 		duration := time.Since(t)
 
 		if duration.Nanoseconds() > SvcStopMaxTime.Nanoseconds() {
-			suite.Fail(fmt.Sprintf("Service request took too long: %s (should be under %s)", duration.String(), SvcStopMaxTime.String()))
+			suite.Fail(fmt.Sprintf("Service request took too long: %s (should be under %s)", duration.String(), SvcEnsureStartMaxTime.String()))
 		}
 	})
 }
