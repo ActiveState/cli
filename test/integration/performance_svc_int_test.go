7c970c98cf3a66e84ac3ff53b605fdeb44ba4cd5
Nathan Rijksen <n.rijksen@gmail.com>
2022-05-11 12:53:14 -0700

    Fix logging of test failure

--- test/integration/performance_svc_int_test.go
+++ test/integration/performance_svc_int_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ActiveState/cli/internal/errs"
 	"github.com/ActiveState/cli/internal/installation"
+	"github.com/ActiveState/cli/internal/installation/storage"
 	"github.com/ActiveState/cli/internal/svcctl"
 	"github.com/ActiveState/cli/internal/testhelpers/e2e"
 	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
@@ -17,7 +18,7 @@ import (
 
 var SvcEnsureStartMaxTime = 1000 * time.Millisecond // https://activestatef.atlassian.net/browse/DX-935
 var SvcRequestMaxTime = 50 * time.Millisecond
-var SvcStopMaxTime = 10 * time.Millisecond
+var SvcStopMaxTime = 50 * time.Millisecond
 
 type PerformanceSvcIntegrationTestSuite struct {
 	tagsuite.Suite
@@ -33,6 +34,13 @@ func (suite *PerformanceIntegrationTestSuite) TestSvcPerformance() {
 	suite.OnlyRunForTags(tagsuite.Performance)
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
+
+	// This integration test is a bit special because it bypasses the spawning logic
+	// so in order to get the right log files when debugging we manually provide the config dir
+	var err error
+	ts.Dirs.Config, err = storage.AppDataPath()
+	suite.Require().NoError(err)
+
 	ipcClient := svcctl.NewDefaultIPCClient()
 	var svcPort string
 
@@ -93,7 +101,7 @@ func (suite *PerformanceIntegrationTestSuite) TestSvcPerformance() {
 		duration := time.Since(t)
 
 		if duration.Nanoseconds() > SvcStopMaxTime.Nanoseconds() {
-			suite.Fail(fmt.Sprintf("Service request took too long: %s (should be under %s)", duration.String(), SvcEnsureStartMaxTime.String()))
+			suite.Fail(fmt.Sprintf("Service request took too long: %s (should be under %s)", duration.String(), SvcStopMaxTime.String()))
 		}
 	})
 }
