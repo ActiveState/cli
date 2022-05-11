d9120e43744b2414477e46bcd3ede11f4bde1f9c
Nathan Rijksen <n.rijksen@gmail.com>
2022-05-11 09:15:39 -0700

    Make poller test more robust

--- internal/poller/poller_test.go
+++ internal/poller/poller_test.go
@@ -7,24 +7,40 @@ import (
 
 func TestPoller(t *testing.T) {
 	x := 0
-	interval := time.Millisecond * 100
+	interval := time.Millisecond * 50
 	p := New(interval, func() (interface{}, error) {
 		defer func() { x++ }()
 		return x, nil
 	})
 	defer p.Close()
 
-	time.Sleep(time.Millisecond)
-	for i := 0; i < 100; i++ {
-		v, ok := p.ValueFromCache().(int)
-		if !ok {
-			t.Fatalf("expected int, got %T", v)
-		}
+	time.Sleep(time.Millisecond * 10)
 
-		if v != i {
-			t.Fatalf("expected %d, got %d", i, v)
-		}
+	timer := time.NewTicker(interval)
+	defer timer.Stop()
 
-		time.Sleep(interval)
-	}
+	done := make(chan struct{})
+
+	go func() {
+		i := 0
+		for {
+			select {
+			case <-timer.C:
+				i++
+				v, ok := p.ValueFromCache().(int)
+				if !ok {
+					t.Fatalf("expected int, got %T", v)
+				}
+
+				if v != i {
+					t.Fatalf("expected %d, got %d", i, v)
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	time.Sleep(time.Second)
+	done <- struct{}{}
 }
