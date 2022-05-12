27e7160aba463ed2813c1bf27c2d4eb9b52c6ace
Nathan Rijksen <n.rijksen@gmail.com>
2022-05-12 15:08:30 -0700

    Retry on EOF

--- internal/svcctl/svcctl.go
+++ internal/svcctl/svcctl.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"time"
@@ -211,6 +212,9 @@ func waitDown(ctx context.Context, ipComm IPCommunicator) error {
 			// We don't need to sleep for this type of error because,
 			// by definition, this is a timeout, and time has already elapsed.
 			if errors.Is(err, ctlErrRequestTimeout) {
+				continue
+			}
+			if errors.Is(err, io.EOF) {
 				continue
 			}
 			if errors.Is(err, ctlErrNotUp) {
