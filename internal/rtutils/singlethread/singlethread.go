5c19141bea7269a25288fa87b0f6bb8cb3cceb6c
Nathan Rijksen <n.rijksen@gmail.com>
2022-05-13 09:34:06 -0700

    Debug hanging threads

--- internal/rtutils/singlethread/singlethread.go
+++ internal/rtutils/singlethread/singlethread.go
@@ -3,6 +3,8 @@ package singlethread
 import (
 	"fmt"
 	"sync"
+
+	"github.com/ActiveState/cli/internal/osutils/stacktrace"
 )
 
 type callback struct {
@@ -14,6 +16,17 @@ type Thread struct {
 	callback chan (callback)
 	closed   bool
 	mutex    *sync.Mutex
+	stack    string
+}
+
+var history []*Thread
+
+func PrintNotClosedThreads() {
+	for _, thread := range history {
+		if !thread.closed {
+			fmt.Printf("Thread not closed: %s", thread.stack)
+		}
+	}
 }
 
 func New() *Thread {
@@ -21,8 +34,10 @@ func New() *Thread {
 		make(chan (callback)),
 		false,
 		&sync.Mutex{},
+		stacktrace.Get().String(),
 	}
 	go t.run()
+	history = append(history, t)
 	return t
 }
 
