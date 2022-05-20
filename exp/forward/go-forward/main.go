a4d2625fbd36ad0570b70e8a747643bc3c4823e1
daved <daved@codemodus.com>
2022-05-20 14:06:32 -0700

    Connect go executor test to svc

--- exp/forward/go-forward/main.go
+++ exp/forward/go-forward/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("hello, world")
+	if len(os.Args) < 2 {
+		log.Fatalln("first arg should be patth to socket file")
+	}
+
+	path := os.Args[1]
+
+	conn, err := net.Dial("unix", path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if _, err := conn.Write([]byte("http-addr")); err != nil {
+		log.Fatalln(err)
+	}
+
+	buf := make([]byte, 256)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(buf[:n]))
 }
