f6385c5847fb7698f91281d602fb6af96e9a56c8
mdrakos <mike.drakos@gmail.com>
2022-06-24 10:21:05 -0700

    Ensure installer stays open with no args

--- cmd/state-installer/cmd.go
+++ cmd/state-installer/cmd.go
@@ -204,17 +204,16 @@ func main() {
 		if !errs.IsSilent(err) {
 			out.Error(err.Error())
 		}
-
-		// Installer was likely started via a double click so we keep the terminal window open
-		if noArgs() {
-			out.Print(locale.Tl("installer_pause", "Press ENTER to exit..."))
-			fmt.Scanln()
-		}
-
-		return
+	} else {
+		an.Event(AnalyticsFunnelCat, "success")
 	}
 
-	an.Event(AnalyticsFunnelCat, "success")
+	// Installer was likely started via a double click so we keep the terminal window open
+	if noArgs() {
+		out.Print(locale.Tl("installer_pause", "Press ENTER to exit..."))
+		fmt.Scanln()
+	}
+
 }
 
 func execute(out output.Outputer, cfg *config.Instance, an analytics.Dispatcher, args []string, params *Params) error {
