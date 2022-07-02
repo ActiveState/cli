0538427de7a38798b0ab89923b6d484b9f7ce037
daved <daved@codemodus.com>
2022-07-01 18:04:23 -0700

    Clean up state-exec error handling (wip)

--- cmd/state-exec/state-exec.zig
+++ cmd/state-exec/state-exec.zig
@@ -12,21 +12,36 @@ const os = std.os;
 const process = std.process;
 const Thread = std.Thread;
 
-const ArgError = error{
-    InvalidArgOne,
-    InvalidArgTwo,
+const Error = error{
+    ArgIterator,
+    ArgInvalidOne,
+    ArgMissingOne,
+    ArgInvalidTwo,
+    ArgMissingTwo,
+    ArgCollector,
+    ArgCollectRunt,
+    ArgCollectUsr,
+    InspectSelfPath,
+    ThreadSpawn,
+    ChildProcInit,
+    ChildProcSpawn,
 };
-
-const RunError = process.ArgIterator.InitError || ArgError;
 
 pub fn main() !void {
     const stderr = io.getStdErr().writer();
 
     run() catch |err| {
         switch (err) {
-            RunError.InitError => try stderr.print("oops", .{}),
-            ArgError.InvalidArgOne => try stderr.print("first arg should be a socket file\n", .{}),
-            ArgError.InvalidArgTwo => try stderr.print("second arg should be a language runtime\n", .{}),
+            Error.ArgIterator => try stderr.print("cannot process args", .{}),
+            Error.ArgInvalidOne, Error.ArgMissingOne => try stderr.print("first arg should be a socket file\n", .{}),
+            Error.ArgInvalidTwo, Error.ArgMissingTwo => try stderr.print("second arg should be a language runtime\n", .{}),
+            Error.ArgCollector => try stderr.print("cannot setup arg collector", .{}),
+            Error.ArgCollectRunt => try stderr.print("cannot collect runtime arg", .{}),
+            Error.ArgCollectUsr => try stderr.print("cannot collect user args", .{}),
+            Error.InspectSelfPath => try stderr.print("cannot obtain path to this executable", .{}),
+            Error.ThreadSpawn => try stderr.print("cannot spawn thread for heartbeat", .{}),
+            Error.ChildProcInit => try stderr.print("cannot initialize child process for runtime", .{}),
+            Error.ChildProcSpawn => try stderr.print("cannot spawn child process for runtime", .{}),
         }
         process.exit(1);
     };
@@ -45,40 +60,37 @@ fn sendMsgToServer(a: mem.Allocator, path: []const u8, pid: i32, exec: []const u
     _ = try conn.read(buf[0..]);
 }
 
-pub fn run() RunError!void {
+pub fn run() Error!void {
     var arena = heap.ArenaAllocator.init(heap.page_allocator);
     defer arena.deinit();
     const a = arena.allocator();
 
-    var argIt = try process.argsWithAllocator(a);
+    var argIt = process.argsWithAllocator(a) catch return Error.ArgIterator;
     defer argIt.deinit();
 
     _ = argIt.skip();
-    const path = try argIt.next(a) orelse {
-        return ArgError.InvalidArgOne;
-    };
-
-    const runt = try argIt.next(a) orelse {
-        return ArgError.InvalidArgTwo;
-    };
+    const path = argIt.next(a) orelse return Error.ArgMissingOne catch return Error.ArgInvalidOne;
+    const runt = argIt.next(a) orelse return Error.ArgMissingTwo catch return Error.ArgInvalidTwo;
 
     var pid: i32 = @truncate(i32, @bitCast(i64, Thread.getCurrentId()));
 
-    const exec = try fs.selfExePathAlloc(a);
+    const exec = fs.selfExePathAlloc(a) catch return Error.InspectSelfPath;
 
-    const clientThread = try Thread.spawn(.{}, sendMsgToServer, .{ a, path, pid, exec });
+    const clientThread = Thread.spawn(.{}, sendMsgToServer, .{ a, path, pid, exec }) catch {
+        return Error.ThreadSpawn;
+    };
     clientThread.join();
 
-    var usrArgs = try process.argsAlloc(a);
+    var usrArgs = process.argsAlloc(a) catch return Error.ArgCollector;
     defer process.argsFree(a, usrArgs);
 
     var cmdArgs = ArrayList([]const u8).init(a);
     defer cmdArgs.deinit();
-    try cmdArgs.append(runt);
-    try cmdArgs.appendSlice(usrArgs[3..]);
+    cmdArgs.append(runt) catch return Error.ArgCollectRunt;
+    cmdArgs.appendSlice(usrArgs[3..]) catch return Error.ArgCollectUsr;
 
-    const childProc = try ChildProcess.init(cmdArgs.items, a);
+    const childProc = ChildProcess.init(cmdArgs.items, a) catch return Error.ChildProcInit;
     defer childProc.deinit();
-    var term = try childProc.spawnAndWait();
+    var term = childProc.spawnAndWait() catch return Error.ChildProcSpawn;
     os.exit(term.Exited);
 }
