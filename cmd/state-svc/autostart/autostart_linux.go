7a91b1947a0032f85efa1e47b7e4a567a5effb22
mdrakos <mike.drakos@gmail.com>
2022-07-21 15:46:27 -0700

    Fix autostart on Linux Also disable tray autostart on Linux

--- cmd/state-svc/autostart/autostart_linux.go
+++ cmd/state-svc/autostart/autostart_linux.go
@@ -3,7 +3,7 @@ package autostart
 import "github.com/ActiveState/cli/internal/constants"
 
 func init() {
-	Options.LaunchFileName = constants.SvcAppName
+	Options.LaunchFileName = constants.ServiceLaunchFileName
 	Options.IconFileName = constants.ServiceIconFileName
 	Options.IconFileSource = constants.IconFileSource
 	Options.GenericName = constants.ServiceGenericName
--- cmd/state-tray/autostart/autostart_linux.go
+++ cmd/state-tray/autostart/autostart_linux.go
@@ -3,7 +3,7 @@ package autostart
 import "github.com/ActiveState/cli/internal/constants"
 
 func init() {
-	Options.LaunchFileName = constants.TrayAppName
+	Options.LaunchFileName = constants.TrayLaunchFileName
 	Options.IconFileName = constants.TrayIconFileName
 	Options.IconFileSource = constants.IconFileSource
 	Options.GenericName = constants.TrayGenericName
