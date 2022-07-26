837dc4824c6d2eac37f65b99f9eba58a3b923f72
mitchell <mitchellb@activestate.com>
2022-07-26 15:58:32 -0400

    `state reset -n` should default to "Yes".

--- cmd/state/internal/cmdtree/cmdtree.go
+++ cmd/state/internal/cmdtree/cmdtree.go
@@ -176,7 +176,7 @@ func New(prime *primer.Values, args ...string) *CmdTree {
 		newProtocolCommand(prime),
 		newExecCommand(prime, args...),
 		newRevertCommand(prime),
-		newResetCommand(prime),
+		newResetCommand(prime, globals),
 		secretsCmd,
 		branchCmd,
 		newLearnCommand(prime),
