af213a4ef42e051df8f1bd44f81317c671ac6cf2
mitchell <mitchellb@activestate.com>
2022-07-27 20:45:32 -0400

    Attempt to fix randomly hanging integration test on Windows CI.

--- test/integration/checkout_int_test.go
+++ test/integration/checkout_int_test.go
@@ -71,7 +71,7 @@ func (suite *CheckoutIntegrationTestSuite) TestCheckoutWithFlags() {
 		e2e.WithArgs("checkout", "ActiveState-CLI/Python3#6d9280e7-75eb-401a-9e71-0d99759fbad3", "--path", python3Dir),
 		e2e.AppendEnv("ACTIVESTATE_CLI_DISABLE_RUNTIME=false"),
 	)
-	cp.ExpectExitCode(0)
+	cp.Expect("Checked out")
 
 	suite.Require().True(fileutils.DirExists(python3Dir), "state checkout should have created "+python3Dir)
 	asy := filepath.Join(python3Dir, constants.ConfigFileName)
