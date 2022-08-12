f8652325a2e79ead5c59d9496d3469a82a652e00
mdrakos <mike.drakos@gmail.com>
2022-08-12 13:22:25 -0700

    Remove unused code

--- pkg/platform/runtime/model/buildplanner.go
+++ pkg/platform/runtime/model/buildplanner.go
@@ -14,7 +14,6 @@ import (
 	"github.com/ActiveState/cli/pkg/platform/api/inventory/inventory_models"
 	"github.com/go-openapi/strfmt"
 	"github.com/machinebox/graphql"
-	"github.com/thoas/go-funk"
 )
 
 type BuildPlanner struct {
@@ -49,64 +48,6 @@ func (b *BuildPlanner) FetchBuildResult(commitID strfmt.UUID, _, _ string) (*Bui
 	if model.BuildPlanStatusEnum(resp.Execute.Status) != model.Ready {
 		return nil, locale.NewError("err_buildplanner_not_ready", "Build plan is not ready")
 	}
-
-	// logging.Debug("Number of artifacts: %d", len(resp.Execute.Artifacts))
-	// logging.Debug("Number of steps: %d", len(resp.Execute.Steps))
-
-	// targetIDs are the IDs of the artifacts that the user requested
-	var targetIDs []string
-	for _, terminal := range resp.Terminals {
-		// TODO: This should filter by platform in the tag.
-		// Should the build graph even return this if it's not for a platform we're on?
-		// Can we filter this out in the results in our build query?
-		// Using the tag to filter this out is esentially string matching, which we should avoid
-		targetIDs = append(targetIDs, terminal.TargetIDs...)
-	}
-
-	var runtimeDeps []model.Artifact
-	var targetArtifacts []model.Artifact
-	for _, tID := range targetIDs {
-		for _, artifact := range resp.Artifacts {
-			if artifact.TargetID == tID {
-				targetArtifacts = append(targetArtifacts, artifact)
-				runtimeDeps = runtimeDependencies(artifact.TargetID, resp.Artifacts)
-			}
-		}
-	}
-
-	var seen []string
-	var uniqueDeps []model.Artifact
-	for _, dep := range runtimeDeps {
-		if !funk.Contains(seen, dep.TargetID) {
-			seen = append(seen, dep.TargetID)
-			uniqueDeps = append(uniqueDeps, dep)
-		}
-	}
-	runtimeDeps = uniqueDeps
-
-	// logging.Debug("Target artifacts: %v", targetArtifacts)
-	// logging.Debug("Runtime dependencies: %v", runtimeDeps)
-
-	names := make(map[string]model.Artifact)
-
-	for _, t := range targetArtifacts {
-		name, err := getArtifactName(t.GeneratedBy, resp.Steps, resp.Sources)
-		if err != nil {
-			return nil, locale.WrapError(err, "err_get_artifact_name", "Could not get artifact name")
-		}
-		names[name] = t
-	}
-
-	for _, d := range runtimeDeps {
-		name, err := getArtifactName(d.GeneratedBy, resp.Steps, resp.Sources)
-		if err != nil {
-			return nil, locale.WrapError(err, "err_get_artifact_name", "Could not get artifact name")
-		}
-		names[name] = d
-	}
-
-	// logging.Debug("Names: %+v", names)
-	// logging.Debug("response: %#v", resp)
 
 	return &BuildResult{
 		BuildEngine: Alternative,
