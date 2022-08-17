03f92c195c760de03f59c6043f7c949f480e871e
mdrakos <mike.drakos@gmail.com>
2022-08-17 11:51:52 -0700

    Add switch command

--- cmd/state/internal/cmdtree/branch.go
+++ cmd/state/internal/cmdtree/branch.go
@@ -45,27 +45,3 @@ func newBranchAddCommand(prime *primer.Values) *captain.Command {
 			return runner.Run(params)
 		})
 }
-
-func newBranchSwitchCommand(prime *primer.Values) *captain.Command {
-	runner := branch.NewSwitch(prime)
-
-	params := branch.SwitchParams{}
-
-	return captain.NewCommand(
-		"switch",
-		locale.Tl("switch_title", "Switching branches"),
-		locale.Tl("switch_description", "Switch to the given branch name"),
-		prime,
-		[]*captain.Flag{},
-		[]*captain.Argument{
-			{
-				Name:        locale.Tl("branch_switch_arg_name", "switch"),
-				Description: locale.Tl("branch_switch_arg_name_description", "Branch to switch to"),
-				Value:       &params.Name,
-				Required:    true,
-			},
-		},
-		func(_ *captain.Command, _ []string) error {
-			return runner.Run(params)
-		})
-}
--- cmd/state/internal/cmdtree/switch.go
+++ cmd/state/internal/cmdtree/switch.go
@@ -2,9 +2,31 @@ package cmdtree
 
 import (
 	"github.com/ActiveState/cli/internal/captain"
+	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/primer"
+	"github.com/ActiveState/cli/internal/runners/swtch"
 )
 
 func newSwitchCommand(prime *primer.Values) *captain.Command {
-	return nil
+	runner := swtch.New(prime)
+
+	params := swtch.SwitchParams{}
+
+	return captain.NewCommand(
+		"switch",
+		locale.Tl("switch_title", "Switching"),
+		locale.Tl("switch_description", "Switch to a branch, commit, or tag"),
+		prime,
+		[]*captain.Flag{},
+		[]*captain.Argument{
+			{
+				Name:        locale.Tl("switch_arg_name", "name"),
+				Description: locale.Tl("switch_arg_name_description", "Branch, commit, or tag to switch to"),
+				Value:       &params.Identifier,
+				Required:    true,
+			},
+		},
+		func(_ *captain.Command, _ []string) error {
+			return runner.Run(params)
+		}).SetGroup(PlatformGroup).SetUnstable(true)
 }
