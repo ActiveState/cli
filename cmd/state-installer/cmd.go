e16fe24c13ca3c0c99d039f5dfb857bd32c7b25a
Nathan Rijksen <nathanr@activestate.com>
2022-08-23 09:54:22 -0700

    Don't run subshell after install if env var is set

--- cmd/state-installer/cmd.go
+++ cmd/state-installer/cmd.go
@@ -366,7 +366,7 @@ func postInstallEvents(out output.Outputer, cfg *config.Instance, an analytics.D
 			an.EventWithLabel(AnalyticsFunnelCat, "forward-activate-default-err", err.Error())
 			return errs.Silence(errs.Wrap(err, "Could not activate %s, error returned: %s", params.activateDefault.String(), errs.JoinMessage(err)))
 		}
-	case !params.isUpdate:
+	case !params.isUpdate && os.Getenv(constants.InstallerNoSubshell) != "true":
 		ss := subshell.New(cfg)
 		ss.SetEnv(envMap(binPath))
 		if err := ss.Activate(nil, cfg, out); err != nil {
