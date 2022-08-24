bcc918546d9a84bd2f4039f86f59b7c08be48e76
daved <daved@codemodus.com>
2022-08-23 18:14:59 -0700

    Mature meta data handling in state-exec

--- cmd/state-exec/state-exec.zig
+++ cmd/state-exec/state-exec.zig
@@ -14,7 +14,6 @@ const process = std.process;
 const Thread = std.Thread;
 
 const execName = "state-exec";
-const metaName = "meta.as";
 
 const Error = error{
     InspectSelfPath,
@@ -51,6 +50,60 @@ pub fn main() !void {
     os.exit(exitCode);
 }
 
+const MetaData = struct {
+    pub const filename = "meta.as";
+    pub const sockDelim = "::sock::";
+    pub const binDelim = "::bin::";
+    pub const envDelim = "::env::";
+
+    sock: []u8,
+    bin: []u8,
+    env: [][]u8,
+};
+
+fn makeMetaData(a: mem.Allocator, stderr: fs.File.Writer, execDir: []const u8) !MetaData {
+    var sockBuf: [256]u8 = undefined;
+    var sock: []u8 = undefined;
+    var binBuf: [256]u8 = undefined;
+    var bin: []u8 = undefined;
+
+    const metaPath = try path.join(a, &[_][]const u8{ execDir, MetaData.filename });
+    const metaFile = try fs.openFileAbsolute(metaPath, .{ .read = true });
+    defer metaFile.close();
+
+    const metaReader = metaFile.reader();
+    var metaBuf: [32760]u8 = undefined;
+    var lineCt: i32 = 0;
+    while (try metaReader.readUntilDelimiterOrEof(&metaBuf, '\n')) |line| : (lineCt += 1) {
+        switch (lineCt) {
+            0 => {
+                const trimmedLine = mem.trimLeft(u8, line, MetaData.sockDelim);
+                sock = sockBuf[0..trimmedLine.len];
+                mem.copy(u8, sock, trimmedLine);
+                try stderr.print("case 0: {s}\n", .{sock});
+            },
+            1 => {
+                const trimmedLine = mem.trimLeft(u8, line, MetaData.binDelim);
+                bin = binBuf[0..trimmedLine.len];
+                mem.copy(u8, bin, trimmedLine);
+                try stderr.print("case 1: {s}\n", .{bin});
+            },
+            2 => {
+                break;
+            },
+            else => {
+                break;
+            },
+        }
+    }
+
+    return MetaData{
+        .sock = sock,
+        .bin = bin,
+        .env = undefined,
+    };
+}
+
 const MsgData = struct {
     pub const fmt = "heart<{d}<{s}";
 
@@ -58,9 +111,17 @@ const MsgData = struct {
     exec: []const u8,
 };
 
+fn makeMsgData(a: mem.Allocator) !MsgData {
+    return MsgData{
+        .pid = @truncate(i32, @bitCast(i64, Thread.getCurrentId())),
+        .exec = try fs.selfExePathAlloc(a),
+    };
+}
+
 fn sendMsgToServer(a: mem.Allocator, stderr: fs.File.Writer, sock: []const u8, d: MsgData) !void {
     const conn = net.connectUnixSocket(sock) catch |err| {
         try stderr.print("{s}: Cannot connect to socket: {s}.\n", .{ execName, err });
+        try stderr.print("{s}\n", .{sock});
         return;
     };
     defer conn.close();
@@ -83,35 +144,25 @@ fn run(stderr: fs.File.Writer) Error!u8 {
     defer arena.deinit();
     const a = arena.allocator();
 
-    var pid: i32 = @truncate(i32, @bitCast(i64, Thread.getCurrentId()));
+    const msgData = makeMsgData(a) catch return Error.InspectSelfPath;
+    const execDir = path.dirname(msgData.exec) orelse return Error.DirOfSelfPath;
+    const metaData = makeMetaData(a, stderr, execDir) catch return Error.InspectSelfPath;
+    const runt = path.join(a, &[_][]const u8{ metaData.bin, path.basename(msgData.exec) }) catch return Error.InspectSelfPath;
 
-    const exec = fs.selfExePathAlloc(a) catch return Error.InspectSelfPath;
+    stderr.print("runt: {s}\n", .{runt}) catch return error.InspectSelfPath;
 
-    const execDir = path.dirname(exec) orelse return Error.DirOfSelfPath;
-    const metaPath = path.join(a, &[_][]const u8{ execDir, metaName }) catch return Error.PathOfMeta;
-    const metaFile = fs.openFileAbsolute(metaPath, .{ .read = true }) catch return Error.MetaOpen;
-    defer metaFile.close();
-    const metaReader = metaFile.reader();
-    var metaBuf: [32760]u8 = undefined;
-    while (metaReader.readUntilDelimiterOrEof(&metaBuf, '\n') catch return Error.InspectSelfPath) |line| {
-        // do something with 'line'
-        stderr.print("{s}\n", .{line}) catch return Error.InspectSelfPath;
-    }
-    const sock = "test";
-    const runt = "runt";
-
-    const data = MsgData{
-        .pid = pid,
-        .exec = exec,
-    };
-    const clientThread = Thread.spawn(.{}, sendMsgToServer, .{ a, stderr, sock, data }) catch {
+    const clientThread = Thread.spawn(.{}, sendMsgToServer, .{ a, stderr, metaData.sock, msgData }) catch {
         return Error.ThreadSpawn;
     };
     defer clientThread.join();
 
+    var usrArgs = process.argsAlloc(a) catch return Error.InspectSelfPath;
+    defer process.argsFree(a, usrArgs);
+
     var cmdArgs = ArrayList([]const u8).init(a);
     defer cmdArgs.deinit();
     cmdArgs.append(runt) catch return Error.InspectSelfPath;
+    cmdArgs.appendSlice(usrArgs[6..]) catch return Error.InspectSelfPath;
 
     const childProc = ChildProcess.init(cmdArgs.items, a) catch return Error.ChildProcInit;
     defer childProc.deinit();
