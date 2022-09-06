e08aa95f5aaa6c3d221ec0177f7ed82288fe6468
Frederick Price <rprice@pricemail.ca>
2022-09-06 18:21:06 -0400

    Move the license stuff away from the TOC stuff

--- pkg/cmdlets/prompts/License.go
+++ pkg/cmdlets/prompts/License.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ActiveState/cli/internal/errs"
 	"github.com/ActiveState/cli/internal/output"
+	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/prompt"
 )
 
@@ -21,7 +22,7 @@ func newOfflineLicense(filepath string) *offlineLicense {
 func (license *offlineLicense) GetLegalText() (string, error) {
     b, err := os.ReadFile(license.filepath)
 	if err != nil {
-		return "", errs.Wrap(err, "Unable to open TOS file")
+		return "", errs.Wrap(err, "Unable to open License file")
 	}
 
 	return string(b), nil
@@ -29,5 +30,38 @@ func (license *offlineLicense) GetLegalText() (string, error) {
 
 func PromptOfflineLicense(out output.Outputer, prompt prompt.Prompter, filepath string)(bool,error) {
     legalText := newOfflineLicense(filepath)
-    return PromptTOS(legalText,out,prompt)
+    return PromptLicense(legalText,out,prompt)
+}
+
+func PromptLicense(lic LegalText, out output.Outputer, prompt prompt.Prompter) (bool, error) {
+	choices := []string{
+		locale.T("lic_accept"),
+		locale.T("lic_not_accept"),
+		locale.T("lic_show_full"),
+	}
+
+	defaultChoice := locale.T("lic_accept")
+	choice, err := prompt.Select(locale.Tl("license", "Accept License"), locale.T("lic_acceptance"), choices, &defaultChoice)
+	if err != nil {
+		return false, err
+	}
+
+	switch choice {
+	case locale.T("lic_accept"):
+		return true, nil
+	case locale.T("lic_not_accept"):
+		return false, nil
+	case locale.T("lic_show_full"):
+		tosText, err := lic.GetLegalText()
+		if err != nil {
+			return false, locale.WrapError(err, "err_get_license_text", "Could not get license text.")
+		}
+
+		out.Print(tosText)
+
+		tosConfirmDefault := true
+		return prompt.Confirm("", locale.T("lic_acceptance"), &tosConfirmDefault)
+	}
+
+	return false, nil
 }
