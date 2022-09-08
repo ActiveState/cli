04e11f6897e5f6c9353a97de314ada2413e256a3
Nathan Rijksen <nathanr@activestate.com>
2022-09-08 14:31:14 -0700

    Fix prompt for bash and zsh

--- internal/assets/contents/shells/bashrc_global.sh
+++ internal/assets/contents/shells/bashrc_global.sh
@@ -1,6 +1,12 @@
-if [ -z "$PROMPT_COMMAND" ]; then
-  export PS1="[{{.Project}}] $PS1"
+{{if ne .Project ""}}
+__state_prompt() {
+  echo "[{{.Project}}]"
+}
+if [ "$PROMPT_COMMAND" != "" ]; then
+  PROMPT_COMMAND+=" "
 fi
+PROMPT_COMMAND+='__state_prompt'
+{{end}}
 
 {{- range $K, $V := .Env}}
 {{- if eq $K "PATH"}}
