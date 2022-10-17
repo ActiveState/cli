9b6a7aa32afe659f38ced1d40727a8791da43f2f
Mike Drakos <mike.drakos@gmail.com>
2022-10-17 12:23:58 -0700

    Remove module

--- cmd/state-svc/main.go
+++ cmd/state-svc/main.go
@@ -27,7 +27,6 @@ import (
 	"github.com/ActiveState/cli/internal/runbits/panics"
 	"github.com/ActiveState/cli/internal/svcctl"
 	"github.com/ActiveState/cli/pkg/platform/authentication"
-	"github.com/apenwarr/fixconsole"
 	"github.com/inconshreveable/mousetrap"
 )
 
@@ -67,12 +66,6 @@ func main() {
 	}
 	rollbar.SetupRollbar(constants.StateServiceRollbarToken)
 	rollbar.SetConfig(cfg)
-
-	err = fixconsole.FixConsoleIfNeeded()
-	if err != nil {
-		exitCode = 1
-		return
-	}
 
 	if os.Getenv("VERBOSE") == "true" {
 		logging.CurrentHandler().SetVerbose(true)
