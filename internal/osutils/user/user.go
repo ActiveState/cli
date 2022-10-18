d9d94fd96021c1375bb3c982f140de9d8982e7ad
mitchell <mitchellb@activestate.com>
2022-10-18 16:10:28 -0400

    Recognize ACTIVESTATE_HOME as an alternative to HOME and show error if user home cannot be found.

--- internal/osutils/user/user.go
+++ internal/osutils/user/user.go
@@ -2,14 +2,43 @@ package user
 
 import (
 	"os"
+
+	"github.com/ActiveState/cli/internal/constants"
 )
+
+// HomeDirNotFoundError is an error that implements the ErrorLocalier and ErrorInput interfaces
+// from locale/errors.go because importing locale for NewInputError creates an import cycle.
+// Instead, return this error that looks like a LocalizedError.
+type HomeDirNotFoundError struct {
+}
+
+const homeDirNotFoundErrorMessage = "Could not proceed because your HOME environment variable is unset. " +
+	"Please ensure that your HOME environment variable is set. " +
+	"Alternatively if you do not or cannot set this variable you can instead use the ACTIVESTATE_HOME variable." +
+	"\n\n" +
+	"This variable is used by the State Tool to determine things like the installation directory, config directory and cache directory."
+
+func (e *HomeDirNotFoundError) Error() string {
+	return homeDirNotFoundErrorMessage
+}
+
+func (e *HomeDirNotFoundError) UserError() string {
+	return homeDirNotFoundErrorMessage
+}
+
+func (e *HomeDirNotFoundError) InputError() bool {
+	return true
+}
 
 // HomeDir returns the user's homedir
 func HomeDir() (string, error) {
 	dir, err := os.UserHomeDir()
 	if err != nil {
-		return "", err
+		var exists bool
+		dir, exists = os.LookupEnv(constants.HomeEnvVarName)
+		if !exists {
+			return "", &HomeDirNotFoundError{}
+		}
 	}
-
 	return dir, nil
 }
--- pkg/cmdlets/checkout/path.go
+++ pkg/cmdlets/checkout/path.go
@@ -3,15 +3,12 @@ package checkout
 import (
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/errs"
 	"github.com/ActiveState/cli/internal/fileutils"
 	"github.com/ActiveState/cli/internal/locale"
 	"github.com/ActiveState/cli/internal/logging"
-	"github.com/ActiveState/cli/internal/osutils"
-	"github.com/ActiveState/cli/internal/osutils/user"
 	"github.com/ActiveState/cli/pkg/project"
 )
 
@@ -69,22 +66,4 @@ func validatePath(ns *project.Namespaced, path string) error {
 	}
 
 	return nil
-}
-
-func getSafeWorkDir() (string, error) {
-	dir, err := osutils.Getwd()
-	if err != nil {
-		return "", errs.Wrap(err, "Could not get working directory")
-	}
-
-	if !strings.HasPrefix(strings.ToLower(dir), `c:\windows`) {
-		return dir, nil
-	}
-
-	dir, err = user.HomeDir()
-	if err != nil {
-		return "", errs.Wrap(err, "Could not get home directory")
-	}
-
-	return dir, nil
 }
