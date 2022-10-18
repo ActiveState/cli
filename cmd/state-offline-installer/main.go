87a2ae3704da3deba35663ca1a87d5b9e4e4ec72
mdrakos <mike.drakos@gmail.com>
2022-10-18 12:58:25 -0700

    Update other executables to report input errors

--- cmd/state-offline-installer/main.go
+++ cmd/state-offline-installer/main.go
@@ -124,5 +124,11 @@ func run(prime *primer.Values) error {
 		},
 	)
 
-	return cmd.Execute(os.Args[1:])
+	err := cmd.Execute(os.Args[1:])
+	if err != nil {
+		errors.ReportError(err, cmd.ActiveFlags(), prime.Analytics())
+		return err
+	}
+
+	return nil
 }
