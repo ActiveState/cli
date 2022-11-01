f8ed8704b409007041f5b0883507c8968a2daa59
mitchell <mitchellb@activestate.com>
2022-11-01 16:26:51 -0400

    Fixed BashifyPathEnv when there are spaces present.

--- internal/osutils/osutils.go
+++ internal/osutils/osutils.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/ActiveState/cli/internal/errs"
 	"github.com/ActiveState/cli/internal/fileutils"
-	"github.com/ActiveState/cli/internal/multilog"
+	"github.com/ActiveState/cli/internal/logging"
 )
 
 // CmdString returns a human-readable description of c.
@@ -43,12 +43,18 @@ func BashifyPath(absolutePath string) (string, error) {
 		return "", errs.New("Unrecognized absolute path format: %s", absolutePath)
 	}
 
+	if strings.Contains(absolutePath, ";") {
+		return "", errs.New("Path is a list: %s", absolutePath)
+	}
+
 	winPath, err := winPathToLinPath(absolutePath)
 	if err == nil {
 		winPath = strings.Replace(winPath, ` `, `\ `, -1) // escape space
 		return winPath, nil
 	}
-	multilog.Error("Failed to bashify path using installed bash executable, falling back to slash replacement: %v", err)
+
+	// Do not log to rollbar because the path might not exist (yet).
+	logging.Error("Failed to bashify path using installed bash executable, falling back to slash replacement: %v", err)
 
 	vol := filepath.VolumeName(absolutePath)
 	absolutePath = absolutePath[len(vol):]
@@ -75,19 +81,19 @@ func winPathToLinPath(name string) (string, error) {
 // BashifyPath takes a windows %PATH% list and turns it into a bash style PATH list.
 // e.g. C:\foo;C:\bar becomes /c/foo:/c/bar
 func BashifyPathEnv(pathList string) (string, error) {
-  if runtime.GOOS != "windows" {
-    return pathList, nil // already bashified
-  }
+	if runtime.GOOS != "windows" {
+		return pathList, nil // already bashified
+	}
 
-  dirs := strings.Split(pathList, ";")
-  for i, dir := range dirs {
-    path, err := BashifyPath(dir)
-    if err != nil {
-      return "", errs.Wrap(err, "Unable to bashify path: %v", dir)
-    }
-    dirs[i] = path
-  }
-  return strings.Join(dirs, ":"), nil // bash uses ':' while Windows uses ';'
+	dirs := strings.Split(pathList, ";")
+	for i, dir := range dirs {
+		path, err := BashifyPath(dir)
+		if err != nil {
+			return "", errs.Wrap(err, "Unable to bashify path: %v", dir)
+		}
+		dirs[i] = strings.Replace(path, `\ `, " ", -1)
+	}
+	return strings.Join(dirs, ":"), nil // bash uses ':' while Windows uses ';'
 }
 
 // Getwd is an alias of osutils.Getwd which wraps the error in our localized error message and FailGetWd, which is user facing (doesn't get logged)
