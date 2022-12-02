07182fb462c246464ccb993d796bedbbaf0bcfbd
mdrakos <mike.drakos@gmail.com>
2022-12-01 16:18:29 -0800

    Add back build fragment

--- pkg/platform/api/graphql/request/saveAndBuild.go
+++ pkg/platform/api/graphql/request/saveAndBuild.go
@@ -1,6 +1,10 @@
 package request
 
-import model "github.com/ActiveState/cli/pkg/platform/api/graphql/model/buildplan"
+import (
+	"fmt"
+
+	model "github.com/ActiveState/cli/pkg/platform/api/graphql/model/buildplan"
+)
 
 func SaveAndBuild(owner, project, parentCommit, branchRef, description string, graph *model.BuildGraph) *buildPlanBySaveAndBuild {
 	return &buildPlanBySaveAndBuild{map[string]interface{}{
@@ -18,111 +22,14 @@ type buildPlanBySaveAndBuild struct {
 }
 
 func (b *buildPlanBySaveAndBuild) Query() string {
-	return `
+	return fmt.Sprintf(`
 mutation ($organization: String!, $project: String!, $parentCommit: String!, $graph: BuildGraph!, $branchRef: String!, $description:String!) {
   saveAndBuild(organization: $organization, project: $project, parentCommit: $parentCommit, graph: $graph, branchRef: $branchRef, description:$description) {
     ... on Commit {
       __typename
       graph
-	  commitId
-      build {
-        __typename
-        ... on Build {
-          status
-          terminals {
-            tag
-            targetIDs
-          }
-          sources: targets {
-            ... on Source {
-              targetID
-              name
-              namespace
-              version
-            }
-          }
-          steps: targets {
-            ... on Step {
-              targetID
-              inputs {
-                tag
-                targetIDs
-              }
-              outputs
-            }
-          }
-          artifacts: targets {
-            __typename
-            targetID
-            ... on ArtifactSucceeded {
-              mimeType
-              generatedBy
-              runtimeDependencies
-              status
-              logURL
-              url
-              checksum
-            }
-            ... on ArtifactUnbuilt {
-              mimeType
-              generatedBy
-              runtimeDependencies
-              status
-            }
-            ... on ArtifactBuilding {
-              mimeType
-              generatedBy
-              runtimeDependencies
-              status
-            }
-            ... on ArtifactTransientlyFailed {
-              mimeType
-              generatedBy
-              runtimeDependencies
-              status
-              logURL
-              errors
-              attempts
-              nextAttemptAt
-            }
-            ... on ArtifactPermanentlyFailed {
-              mimeType
-              generatedBy
-              runtimeDependencies
-              status
-              logURL
-              errors
-            }
-          }
-        }
-        ... on PlanningError {
-          subErrors {
-            __typename
-            ... on GenericSolveError {
-              path
-              message
-              isTransient
-              validationErrors {
-                jsonPath
-              }
-            }
-            ... on RemediableSolveError {
-              path
-              message
-              isTransient
-              errorType
-              validationErrors {
-                jsonPath
-              }
-              suggestedRemediations {
-                remediationType
-                command
-                parameters
-              }
-            }
-          }
-        }
-      }
+      commitId
+      %s
     }
     ... on CommitNotFound {
       message
@@ -132,7 +39,7 @@ mutation ($organization: String!, $project: String!, $parentCommit: String!, $gr
     }
   }
 }
-`
+`, buildResultFragment)
 }
 
 func (b *buildPlanBySaveAndBuild) Vars() map[string]interface{} {
