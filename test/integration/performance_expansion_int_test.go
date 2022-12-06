f7b65b849f8abc9bb6956f70db6e59f629a5bdef
mdrakos <mike.drakos@gmail.com>
2022-12-06 15:59:30 -0800

    Move require

--- test/integration/performance_expansion_int_test.go
+++ test/integration/performance_expansion_int_test.go
@@ -56,8 +56,9 @@ func (suite *PerformanceExpansionIntegrationTestSuite) TestExpansionPerformance(
 		})
 		variance := float64(avg) + (float64(avg) * DefaultVariance)
 		baseline = time.Duration(variance)
-		suite.Require().NotEqual(baseline, DefaultMaxTime)
 	})
+
+	suite.Require().NotEqual(baseline, DefaultMaxTime)
 
 	suite.Run("CallScriptFromMerged", func() {
 		additionalYamls := make(map[string]projectfile.Project)
