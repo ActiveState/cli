88b47450f1c83c945a0ae470bfd670c1958f852b
mdrakos <mike.drakos@gmail.com>
2022-12-15 13:49:57 -0800

    Remove debug logging

--- pkg/platform/runtime/setup/setup.go
+++ pkg/platform/runtime/setup/setup.go
@@ -2,7 +2,6 @@ package setup
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"net/url"
@@ -236,12 +235,6 @@ func (s *Setup) updateArtifacts() ([]artifact.ArtifactID, error) {
 		if err != nil {
 			return errs.Wrap(err, "Could not collect env info for artifact")
 		}
-
-		data, err := json.MarshalIndent(envDef, "", "  ")
-		if err != nil {
-			return errs.Wrap(err, "Could not marshal environment definition")
-		}
-		logging.Debug("Environment definition: %s", string(data))
 
 		// Move files to installation path, ensuring file operations are synchronized
 		mutex.Lock()
