64d60fd526bfa3619445d3d25ba9a23a00d4e16d
mitchell <mitchellb@activestate.com>
2022-12-21 11:44:45 -0500

    Fixed Windows build errors.

--- cmd/state-installer/internal/legacytray/legacytray_windows.go
+++ cmd/state-installer/internal/legacytray/legacytray_windows.go
@@ -5,14 +5,15 @@ import (
 	"path/filepath"
 
 	"github.com/ActiveState/cli/internal/errs"
+	"github.com/ActiveState/cli/internal/fileutils"
 	"github.com/ActiveState/cli/internal/osutils/shortcut"
 )
 
 func osSpecificRemoveTray(installPath, trayExec string) error {
-	shortcutDir = filepath.Join(os.Getenv("USERPROFILE"), "AppData", "Roaming", "Microsoft", "Windows", "Start Menu", "Programs", "ActiveState")
+	shortcutDir := filepath.Join(os.Getenv("USERPROFILE"), "AppData", "Roaming", "Microsoft", "Windows", "Start Menu", "Programs", "ActiveState")
 	sc := shortcut.New(shortcutDir, trayAppName, trayExec)
 	if shortcutFile := filepath.Dir(sc.Path()); fileutils.FileExists(shortcutFile) {
-		err = os.Remove(shortcutFile)
+		err := os.Remove(shortcutFile)
 		if err != nil {
 			errs.Wrap(err, "Unable to remove shortcut file")
 		}
