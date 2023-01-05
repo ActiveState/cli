697906960ed3695718bd47d428b7f6facffc7d4b
Mike Drakos <mike.drakos@gmail.com>
2023-01-05 11:24:55 -0800

    Increase variance

--- test/integration/performance_expansion_int_test.go
+++ test/integration/performance_expansion_int_test.go
@@ -25,7 +25,7 @@ const (
 
 	DefaultMaxTime        = 1000 * time.Millisecond
 	DefaultSamples        = 10
-	DefaultVariance       = 0.75
+	DefaultVariance       = 0.80
 	DefaultSecretsMaxTime = 3500 * time.Millisecond
 	// Add other configuration values on per-test basis if needed
 )
