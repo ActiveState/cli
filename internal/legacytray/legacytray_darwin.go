a71ea6b36d4d6b06d2aaf23ea2269f66c30ab0d3
mdrakos <mike.drakos@gmail.com>
2023-01-10 15:49:44 -0800

    Attempt to fix macOS integration tests

--- internal/legacytray/legacytray_darwin.go
+++ internal/legacytray/legacytray_darwin.go
@@ -12,7 +12,7 @@ import (
 const macOSApplicationName = "ActiveState Desktop (Preview).app"
 
 func osSpecificRemoveTray(installPath, trayExec string) error {
-	if launcherPath, err := installation.LauncherInstallPath(); err == nil {
+	if launcherPath, err := installation.ApplicationInstallPath(); err == nil {
 		if appPath := filepath.Join(launcherPath, macOSApplicationName); fileutils.DirExists(appPath) {
 			err = os.RemoveAll(appPath)
 			if err != nil {
