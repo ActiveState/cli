0852606fd5ada93c28bf68da8f25516d2214934e
mdrakos <mike.drakos@gmail.com>
2023-02-06 11:13:00 -0800

    Fix another Linux build error

--- internal/osutils/autostart/autostart_linux.go
+++ internal/osutils/autostart/autostart_linux.go
@@ -114,7 +114,7 @@ func disable(exec string, opts Options) error {
 		return nil
 	}
 
-	path, err := autostartPath(opts.LaunchFileName)
+	path, err := autostartPath(exec, opts.LaunchFileName)
 	if err != nil {
 		return err
 	}
