72f5ab99705867faee86178e5930c43061c5bc18
mitchell <mitchellb@activestate.com>
2023-02-16 10:08:41 -0500

    Do not require authentication when reverting a commit.
    Local reversions are okay. Only require authentication to push.

--- internal/runners/revert/revert.go
+++ internal/runners/revert/revert.go
@@ -72,10 +72,6 @@ func (r *Revert) Run(params *Params) error {
 	if !strfmt.IsUUID(params.CommitID) {
 		return locale.NewInputError("err_invalid_commit_id", "Invalid commit ID")
 	}
-	if !r.auth.Authenticated() {
-		// Authentication is required in order to make a revert commit. Fail now rather than later.
-		return locale.NewInputError("err_api_not_authenticated")
-	}
 	r.out.Notice(locale.Tl("operating_message", "", r.project.NamespaceString(), r.project.Dir()))
 	commitID := strfmt.UUID(params.CommitID)
 
