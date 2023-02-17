077acb843459e2ec6b7430f6a1ae0f2000d69fe9
Nathan Rijksen <nathanr@activestate.com>
2023-02-17 11:54:05 -0800

    Commit generated constants for now
    Will be properly addressed in DX-1596

---
+++ internal/constants/generated.go
@@ -0,0 +1,16 @@
+// Do NOT manually edit this file. It is generated using scripts/constants-generator using data from constants/preprocess.
+// This script is automatically ran by `make build`, so you shouldn't have to run this manually.
+
+package constants
+
+const RemoteInstallerVersion = "0.1.0"
+const OnCI = ""
+const BuildNumber = "15628"
+const Version = "0.36.0-SHA78b50b7e3"
+const RevisionHashShort = "78b50b7e3"
+const VersionNumber = "0.36.0"
+const Date = "Fri Feb 17 2023 11:53:38 -0800 PST"
+const UserAgent = "state/0.36.0-SHA78b50b7e3; DX-1485"
+const APITokenName = "activestate-platform-cli-DX-1485"
+const BranchName = "DX-1485"
+const RevisionHash = "78b50b7e3bffd61ec5f4e59f96d41ab65f01de8f"
