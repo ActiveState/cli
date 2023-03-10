76aa44f5bf028834ff1f346e9555f92a76f396b7
daved <daved@codemodus.com>
2023-03-10 13:03:14 -0800

    Add more logging to constants generator script

--- scripts/constants-generator/main.go
+++ scripts/constants-generator/main.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"time"
 
 	"github.com/dave/jennifer/jen"
 
@@ -14,31 +14,49 @@ import (
 
 var inTest = false
 
+var (
+	start = time.Now()
+)
+
+func log(format string, as ...any) {
+	d := time.Since(start)
+	fmt.Fprintf(os.Stderr, "[%08dμs] ", d.Microseconds())
+	fmt.Fprintf(os.Stderr, format, as...)
+	fmt.Fprintln(os.Stderr)
+}
+
+func logFatal(format string, as ...any) {
+	log(format, as...)
+	os.Exit(1)
+}
+
 func main() {
-	if ! inTest {
+	if !inTest {
 		run(os.Args)
 	}
 }
 
 func run(args []string) {
+	log("Starting run logic")
 	if len(args) < 2 || (args[1] == "--" && len(args) < 3) {
-		log.Fatalf("Usage: %s <target-file>", args[0])
+		logFatal("Usage: %s <target-file>", args[0])
 	}
 
+	log("Storing constants data in file buffer")
 	f := jen.NewFile("constants")
-
 	f.HeaderComment("Do NOT manually edit this file. It is generated using scripts/constants-generator using data from constants/preprocess.")
 	f.HeaderComment("This script is automatically ran by `make build`, so you shouldn't have to run this manually.")
 
 	for k, v := range preprocess.Constants {
-		fmt.Printf("Adding Constant: %s, Value: %s\n", k, v())
+		log("Adding Constant: %s, Value: %s", k, v())
 		f.Const().Id(k).Op("=").Lit(v())
 	}
 
+	log("Rendering file buffer")
 	buf := &bytes.Buffer{}
 	err := f.Render(buf)
 	if err != nil {
-		log.Fatalf("Rendering failed: %v", err)
+		logFatal("Rendering failed: %v", err)
 	}
 
 	target := args[1]
@@ -47,8 +65,8 @@ func run(args []string) {
 	}
 
 	wd, _ := os.Getwd()
-	fmt.Printf("Writing generated constants to: %s (pwd: %s)\n", target, wd)
+	log("Writing generated constants to: %s (pwd: %s)", target, wd)
 	ioutil.WriteFile(target, buf.Bytes(), 0666)
 
-	fmt.Println("Constants generated")
+	log("Constants file generated")
 }
