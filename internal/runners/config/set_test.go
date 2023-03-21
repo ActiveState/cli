248a3a95af88417a957841ef0408488b55646cef
mdrakos <mike.drakos@gmail.com>
2023-03-21 16:25:13 -0700

    Fix unit test build error

--- internal/runners/config/set_test.go
+++ internal/runners/config/set_test.go
@@ -15,7 +15,7 @@ func TestSetUnknownKey(t *testing.T) {
 	cfg.Set("unknown", nil)
 
 	outputer := outputhelper.NewCatcher()
-	set := Set{outputer, cfg, nil}
+	set := Set{outputer, cfg, nil, nil}
 	params := SetParams{"unknown", "true"}
 
 	// Trying to set an unknown config key should error.
