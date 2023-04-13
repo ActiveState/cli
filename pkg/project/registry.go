23bea1b349468609492066a5ce75715be45477d4
daved <daved@codemodus.com>
2023-04-13 16:45:41 -0700

    Start use of reflect in project expander

--- pkg/project/registry.go
+++ pkg/project/registry.go
@@ -1,6 +1,8 @@
 package project
 
 import (
+	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/ActiveState/cli/internal/errs"
@@ -12,18 +14,51 @@ var expanderRegistry = map[string]ExpanderFunc{}
 var (
 	ErrExpandBadName = errs.New("Bad expander name")
 	ErrExpandNoFunc  = errs.New("Expander has no handler")
+	topLevelLookup   = make(map[string]string)
 )
 
-const TopLevelExpanderName = "toplevel"
+const (
+	TopLevelExpanderName = "toplevel"
+)
 
 func init() {
 	expanderRegistry = map[string]ExpanderFunc{
 		"events":             EventExpander,
 		"scripts":            ScriptExpander,
 		"constants":          ConstantExpander,
-		"project":            ProjectExpander,
 		TopLevelExpanderName: TopLevelExpander,
 	}
+}
+
+func RegisterStruct(val interface{}) error {
+	v := reflect.ValueOf(val)
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	to := v.Type()
+	fields := reflect.VisibleFields(to) // we know this is a struct
+
+	for _, f := range fields { // Vars.(OS).Version.Name
+		sv := v.FieldByIndex(f.Index)
+		if sv.Kind() == reflect.Ptr {
+			sv = sv.Elem()
+		}
+		sto := sv.Type()
+
+		switch sto.Kind() {
+		case reflect.Struct:
+			m := makeStringMapMap(sto, sv)
+			RegisterExpander(strings.ToLower(f.Name), MakeExpanderFuncFromMap(m))
+
+		case reflect.Func:
+			//c.RegisterFunc(f.Name, sv.Interface())
+
+		default:
+			topLevelLookup[strings.ToLower(f.Name)] = fmt.Sprintf("%v", sv.Interface())
+		}
+	}
+
+	return nil
 }
 
 // RegisterExpander registers an Expander Func for some given handler value. The handler value
@@ -52,4 +87,34 @@ func RegisteredExpander(handle string) ExpanderFunc {
 func IsRegistered(handle string) bool {
 	_, ok := expanderRegistry[handle]
 	return ok
+}
+
+func makeStringMapMap(structure reflect.Type, value reflect.Value) map[string]map[string]string {
+	m := make(map[string]map[string]string)
+	fields := reflect.VisibleFields(structure)
+
+	for _, f := range fields { // Vars.OS.(Version).Name
+		subValue := value.FieldByIndex(f.Index)
+		if subValue.Kind() == reflect.Ptr {
+			subValue = subValue.Elem()
+		}
+		subType := subValue.Type()
+
+		switch subType.Kind() {
+		case reflect.Struct:
+			innerMap := make(map[string]string)
+			subFields := reflect.VisibleFields(subType)
+
+			for _, sf := range subFields { // Vars.OS.Version.(Name)
+				subSubValue := subValue.FieldByIndex(sf.Index)
+				innerMap[strings.ToLower(sf.Name)] = fmt.Sprintf("%v", subSubValue.Interface())
+			}
+			m[strings.ToLower(f.Name)] = innerMap
+
+		default:
+			m[strings.ToLower(f.Name)] = map[string]string{"": fmt.Sprintf("%v", value.Interface())}
+		}
+	}
+
+	return m
 }
