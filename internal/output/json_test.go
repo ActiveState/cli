56922fccf56617cd23225bd24320c9f36369c7df
mitchell <mitchellb@activestate.com>
2023-05-02 17:27:43 -0400

    Do not write nul byte in JSON output.
    Also log multiple calls to JSON outputer as errors.

--- internal/output/json_test.go
+++ internal/output/json_test.go
@@ -21,13 +21,13 @@ func TestJSON_Print(t *testing.T) {
 		{
 			"simple string",
 			args{"hello"},
-			`"hello"` + "\x00\n",
+			`"hello"`,
 			"",
 		},
 		{
 			"error string",
 			args{errors.New("hello")},
-			`"hello"` + "\x00\n",
+			`"hello"`,
 			"",
 		},
 		{
@@ -41,7 +41,7 @@ func TestJSON_Print(t *testing.T) {
 					"value1", "value2", "value3",
 				},
 			},
-			`{"Field1":"value1","Field2":"value2"}` + "\x00\n",
+			`{"Field1":"value1","Field2":"value2"}`,
 			"",
 		},
 		{
@@ -55,7 +55,7 @@ func TestJSON_Print(t *testing.T) {
 					"value1", "value2", "value3",
 				},
 			},
-			`{"RealField1":"value1","RealField2":"value2"}` + "\x00\n",
+			`{"RealField1":"value1","RealField2":"value2"}`,
 			"",
 		},
 	}
@@ -64,12 +64,12 @@ func TestJSON_Print(t *testing.T) {
 			outWriter := &bytes.Buffer{}
 			errWriter := &bytes.Buffer{}
 
-			f := &JSON{&Config{
+			f := &JSON{cfg: &Config{
 				OutWriter:   outWriter,
 				ErrWriter:   errWriter,
 				Colored:     false,
 				Interactive: false,
-			}, true}
+			}}
 
 			f.Print(tt.args.value)
 			assert.Equal(t, tt.expectedOut, outWriter.String(), "Output did not match")
@@ -100,59 +100,14 @@ func TestJSON_Notice(t *testing.T) {
 			outWriter := &bytes.Buffer{}
 			errWriter := &bytes.Buffer{}
 
-			f := &JSON{&Config{
+			f := &JSON{cfg: &Config{
 				OutWriter:   outWriter,
 				ErrWriter:   errWriter,
 				Colored:     false,
 				Interactive: false,
-			}, true}
+			}}
 
 			f.Notice(tt.args.value)
-			assert.Equal(t, tt.expectedOut, outWriter.String(), "Output did not match")
-			assert.Equal(t, tt.expectedErr, errWriter.String(), "Errors did not match")
-		})
-	}
-}
-
-func TestJSON_Nullbyte(t *testing.T) {
-	type args struct {
-		value interface{}
-	}
-	tests := []struct {
-		name        string
-		nulByte     bool
-		args        args
-		expectedOut string
-		expectedErr string
-	}{
-		{
-			"no nulbyte",
-			false,
-			args{"hello"},
-			`"hello"` + "\n",
-			"",
-		},
-		{
-			"nulbyte",
-			true,
-			args{"hello"},
-			`"hello"` + "\x00\n",
-			"",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			outWriter := &bytes.Buffer{}
-			errWriter := &bytes.Buffer{}
-
-			f := &JSON{&Config{
-				OutWriter:   outWriter,
-				ErrWriter:   errWriter,
-				Colored:     false,
-				Interactive: false,
-			}, tt.nulByte}
-
-			f.Print(tt.args.value)
 			assert.Equal(t, tt.expectedOut, outWriter.String(), "Output did not match")
 			assert.Equal(t, tt.expectedErr, errWriter.String(), "Errors did not match")
 		})
@@ -172,25 +127,25 @@ func TestJSON_Error(t *testing.T) {
 		{
 			"error string",
 			args{errors.New("hello")},
-			`{"errors":["hello"],"code":1}` + "\x00\n",
+			`{"errors":["hello"],"code":1}`,
 			"",
 		},
 		{
 			"simple string",
 			args{"hello"},
-			`{"errors":["hello"],"code":1}` + "\x00\n",
+			`{"errors":["hello"],"code":1}`,
 			"",
 		},
 		{
 			"unrecognized",
 			args{1},
-			`{"errors":["Not a recognized error format: 1"],"code":1}` + "\x00\n",
+			`{"errors":["Not a recognized error format: 1"],"code":1}`,
 			"",
 		},
 		{
 			"raw JSON",
 			args{[]byte(`"hello"`)},
-			`"hello"` + "\x00\n",
+			`"hello"`,
 			"",
 		},
 	}
@@ -199,12 +154,12 @@ func TestJSON_Error(t *testing.T) {
 			outWriter := &bytes.Buffer{}
 			errWriter := &bytes.Buffer{}
 
-			f := &JSON{&Config{
+			f := &JSON{cfg: &Config{
 				OutWriter:   outWriter,
 				ErrWriter:   errWriter,
 				Colored:     false,
 				Interactive: false,
-			}, true}
+			}}
 
 			f.Error(tt.args.value)
 			assert.Equal(t, tt.expectedOut, outWriter.String(), "Output did not match")
