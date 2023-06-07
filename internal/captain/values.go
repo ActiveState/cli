7958a0c6c1f97f66eec9c49a43b610c9d0b68f98
Nathan Rijksen <nathanr@activestate.com>
2023-06-07 09:59:02 -0700

    Fix missing directive

--- internal/captain/values.go
+++ internal/captain/values.go
@@ -175,7 +175,7 @@ func (p *PackageValueNoVersion) Set(s string) error {
 		return errs.Wrap(err, "PackageValue.Set failed")
 	}
 	if p.Version != "" {
-		return fmt.Errorf("Specifying a version is not supported, package format should be '[<namespace>/]<name>'", s)
+		return fmt.Errorf("Specifying a version is not supported, package format should be '[<namespace>/]<name>'")
 	}
 	return nil
 }
