bec74ef0bfb335f4a3d4a9454dd9d31a8bf9ed30
daved <daved@codemodus.com>
2023-06-23 15:04:01 -0700

    Add l10n to buildscript sync failure in stage cmd

--- internal/runners/stage/stage.go
+++ internal/runners/stage/stage.go
@@ -42,17 +42,27 @@ func New(p primeable) *Stage {
 
 func (s *Stage) Run() error {
 	if err := buildscript.Sync(s.proj, nil, s.out, s.auth); err != nil {
-		return err
+		return locale.WrapError(
+			err, "err_stage_sync_buildscript",
+			"Could not synchronize the buildscript.",
+		)
 	}
 
-	rti, err := runtime.NewFromProject(s.proj, target.TriggerStage, s.analytics, s.svcModel, s.out, s.auth)
+	trigger := target.TriggerStage
+	rti, err := runtime.NewFromProject(s.proj, trigger, s.analytics, s.svcModel, s.out, s.auth)
 	if err != nil {
-		return locale.WrapInputError(err, "err_stage_runtime_new", "Could not update runtime for this project.")
+		return locale.WrapInputError(
+			err, "err_stage_runtime_new",
+			"Could not update runtime for this project.",
+		)
 	}
 
 	execDir := setup.ExecDir(rti.Target().Dir())
 	s.out.Print(output.Prepare(
-		locale.Tl("refresh_project_statement", "", s.proj.NamespaceString(), s.proj.Dir(), execDir),
+		locale.Tl(
+			"refresh_project_statement",
+			"", s.proj.NamespaceString(), s.proj.Dir(), execDir,
+		),
 		&struct {
 			Namespace   string `json:"namespace"`
 			Path        string `json:"path"`
