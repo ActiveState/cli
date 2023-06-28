ef750a30667fc046e9f15ecff569b123619d106f
daved <daved@codemodus.com>
2023-06-27 17:27:41 -0700

    Add stage cmd e2e test

---
+++ test/integration/stage_int_test.go
@@ -0,0 +1,55 @@
+package integration
+
+import (
+	"bytes"
+	"path/filepath"
+	"testing"
+
+	"github.com/ActiveState/cli/internal/constants"
+	"github.com/ActiveState/cli/internal/fileutils"
+	"github.com/ActiveState/cli/internal/testhelpers/e2e"
+	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
+	"github.com/ActiveState/cli/pkg/platform/runtime/buildscript"
+	"github.com/stretchr/testify/suite"
+)
+
+type StageIntegrationTestSuite struct {
+	tagsuite.Suite
+}
+
+func (suite *StageIntegrationTestSuite) TestMergeBuildScript() {
+	suite.OnlyRunForTags(tagsuite.Stage)
+	suite.T().Skip("Buildplanner's StageCommit does not currently work")
+	ts := e2e.New(suite.T(), false)
+	defer ts.Close()
+
+	ts.LoginAsPersistentUser()
+
+	cp := ts.Spawn("checkout", "ActiveState-CLI/Stage-Test-A#7a1b416e-c17f-4d4a-9e27-cbad9e8f5655", ".")
+	cp.Expect("Skipping runtime setup")
+	cp.Expect("Checked out")
+	cp.ExpectExitCode(0)
+
+	_, err := buildscript.NewScriptFromProjectDir(ts.Dirs.Work)
+	suite.Require().NoError(err) // verify validity
+
+	cp = ts.Spawn("stage")
+	cp.Expect("No change")
+	cp.ExpectExitCode(0)
+
+	_, err = buildscript.NewScriptFromProjectDir(ts.Dirs.Work)
+	suite.Require().NoError(err) // verify validity
+
+	scriptPath := filepath.Join(ts.Dirs.Work, constants.BuildScriptFileName)
+	data := fileutils.ReadFileUnsafe(scriptPath)
+	bytes.ReplaceAll(data, []byte("casestyle"), []byte("case"))
+	suite.Require().NoError(fileutils.WriteFile(scriptPath, data), "Update buildscript")
+
+	cp = ts.Spawn("stage")
+	cp.Expect("Runtime updated")
+	cp.ExpectNotExitCode(0)
+}
+
+func TestStageIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(StageIntegrationTestSuite))
+}
