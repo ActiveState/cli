c9a9aa394259a240c696b23ebc53ca33bfdc1890
mdrakos <mike.drakos@gmail.com>
2023-07-05 13:09:46 -0700

    Add back project query

---
+++ pkg/platform/api/buildplanner/request/project.go
@@ -0,0 +1,164 @@
+package request
+
+func BuildPlanByProject(organization, project, commitID string) *buildPlanByProject {
+	bp := &buildPlanByProject{map[string]interface{}{
+		"organization": organization,
+		"project":      project,
+		"commitID":     commitID,
+	}}
+
+	return bp
+}
+
+type buildPlanByProject struct {
+	vars map[string]interface{}
+}
+
+func (b *buildPlanByProject) Query() string {
+	return `
+query ($commitID: String!, $organization: String!, $project: String!) {
+  project(organization: $organization, project: $project) {
+    ... on Project {
+      commit(vcsRef: $commitID) {
+        ... on Commit {
+          __typename
+          expr
+          build {
+            __typename
+            ... on BuildCompleted {
+              buildLogIds {
+                ... on AltBuildId {
+                  id
+                }
+              }
+            }
+            ... on BuildStarted {
+              buildLogIds {
+                ... on AltBuildId {
+                  id
+                }
+              }
+            }
+            ... on Build {
+              status
+              terminals {
+                tag
+                nodeIds
+              }
+              sources: nodes {
+                ... on Source {
+                  nodeId
+                  name
+                  namespace
+                  version
+                }
+              }
+              steps: steps {
+                ... on Step {
+                  stepId
+                  inputs {
+                    tag
+                    nodeIds
+                  }
+                  outputs
+                }
+              }
+              artifacts: nodes {
+                ... on ArtifactSucceeded {
+                  __typename
+                  nodeId
+                  mimeType
+                  generatedBy
+                  runtimeDependencies
+                  status
+                  logURL
+                  url
+                  checksum
+                }
+                ... on ArtifactUnbuilt {
+                  __typename
+                  nodeId
+                  mimeType
+                  generatedBy
+                  runtimeDependencies
+                  status
+                }
+                ... on ArtifactStarted {
+                  __typename
+                  nodeId
+                  mimeType
+                  generatedBy
+                  runtimeDependencies
+                  status
+                }
+                ... on ArtifactTransientlyFailed {
+                  __typename
+                  nodeId
+                  mimeType
+                  generatedBy
+                  runtimeDependencies
+                  status
+                  logURL
+                  errors
+                  attempts
+                  nextAttemptAt
+                }
+                ... on ArtifactPermanentlyFailed {
+                  __typename
+                  nodeId
+                  mimeType
+                  generatedBy
+                  runtimeDependencies
+                  status
+                  logURL
+                  errors
+                }
+              }
+            }
+            ... on PlanningError {
+              subErrors {
+                __typename
+                ... on GenericSolveError {
+                  path
+                  message
+                  isTransient
+                  validationErrors {
+                    jsonPath
+                  }
+                }
+                ... on RemediableSolveError {
+                  path
+                  message
+                  isTransient
+                  errorType
+                  validationErrors {
+                    jsonPath
+                  }
+                  suggestedRemediations {
+                    remediationType
+                    command
+                    parameters
+                  }
+                }
+              }
+            }
+          }
+        }
+        ... on NotFound {
+          __typename
+          message
+        }
+      }
+    }
+    ... on NotFound {
+      __typename
+      message
+    }
+  }
+}
+`
+}
+
+func (b *buildPlanByProject) Vars() map[string]interface{} {
+	return b.vars
+}
