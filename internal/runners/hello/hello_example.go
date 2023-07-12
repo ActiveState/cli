0a822786a59cd2284b65e9b5b1682774d3aeb3d1
daved <daved@codemodus.com>
2023-07-11 20:34:25 -0700

    Add comments to and cleanup hello runners

--- internal/runners/hello/hello_example.go
+++ internal/runners/hello/hello_example.go
@@ -1,3 +1,10 @@
+// Package hello provides "hello command" logic and associated types. By
+// convention, the construction function for the primary type (also named
+// "hello") is simply named "New". For other construction functions, the name
+// of the type is used as a suffix. For instance, "hello.NewInfo()" for the
+// type "hello.Info". Similarly, "Info" would be used as a prefix or suffix for
+// the info-related types like "InfoRunParams". Each "group of types" is usually
+// expressed in its own file.
 package hello
 
 import (
@@ -7,32 +14,46 @@ import (
 	"github.com/ActiveState/cli/internal/runbits"
 )
 
+// primeable describes the app-level dependencies that a runner will need.
 type primeable interface {
 	primer.Outputer
 }
 
+// RunParams defines the parameters needed to execute a given runner. These
+// values are typically collected from flags and arguments entered into the
+// cli, but there is no reason that they couldn't be set in another manner.
 type RunParams struct {
 	Named string
 }
 
+// NewRunParams contains a scope in which default or construction-time values
+// can be set. If no default or construction-time values are necessary, direct
+// construction of RunParams is fine, and this construction func may be dropped.
 func NewRunParams() *RunParams {
 	return &RunParams{
 		Named: "Friend",
 	}
 }
 
+// Hello defines the app-level dependencies that are accessible within the Run
+// function.
 type Hello struct {
 	out output.Outputer
 }
 
+// New contains the scope in which an instance of Hello is constructed from an
+// implementation of primeable.
 func New(p primeable) *Hello {
 	return &Hello{
 		out: p.Output(),
 	}
 }
 
+// Run contains the scope in which the hello runner logic is executed.
 func (h *Hello) Run(params *RunParams) error {
+	// Reusable runner logic is contained within the runbits package.
 	if err := runbits.SayHello(h.out, params.Named); err != nil {
+		// Errors should nearly always be localized.
 		return locale.WrapError(
 			err, "hello_cannot_say", "Cannot say hello without a name",
 		)
--- internal/runners/hello/info_example.go
+++ internal/runners/hello/info_example.go
@@ -35,50 +35,85 @@ func NewInfo(p infoPrimeable) *Info {
 	}
 }
 
+// Run contains the scope in which the info runner logic is executed. It can be
+// helpful to group stages of the runner logic into subhandlers/seclusions so
+// that the behavior of the runner is immediately apparent.
 func (i *Info) Run(params *InfoRunParams) error {
-	if err := runbits.SayHello(i.out, i.auth.WhoAmI()); err != nil {
-		err = locale.WrapError(
-			err, "hello_info_err_say", "Cannot say hello without a name",
-		)
-		if !i.auth.Authenticated() {
-			err = errs.AddTips(err, locale.Tl("hello_info_suggest_login", "Try logging in"))
-		}
+	if err := runOutputHello(i.out, i.auth); err != nil {
+		// Because the "run" subhandlers are purely for seculding
+		// logic, it is not necessarily helpful to add localized error
+		// context.
 		return err
 	}
 
-	projMsg := locale.Tl(
-		"hello_info_warn_no_project", "Not in a project directory.",
-	)
-	if i.pj != nil {
-		projMsg = locale.Tl(
-			"hello_info_project",
-			"Project: {{.V0}}",
-			i.pj.Namespace().String(),
+	runOutputProjectMessage(i.out, i.pj)
+
+	return runOutputExtra(i.out, i.pj, params)
+}
+
+func runOutputHello(out output.Outputer, auth *authentication.Auth) error {
+	if err := runbits.SayHello(out, auth.WhoAmI()); err != nil {
+		// As in the hello runner, errors should nearly always be
+		// localized at this level.
+		err = locale.WrapError(
+			err, "hello_info_err_say", "Cannot say hello without a name",
 		)
-	}
-	i.out.Print(projMsg)
-
-	if params.Extra {
-		commitMsg, err := currentCommitMessage(i.pj)
-		if err != nil {
-			err = locale.WrapError(
-				err, "hello_info_err_get_commit_msg", " Cannot get commit message",
+		// It's also useful to offer users reasonable tips on recourses.
+		if !auth.Authenticated() {
+			err = errs.AddTips(
+				err, locale.Tl("hello_info_suggest_login", "Try logging in"),
 			)
-			return errs.AddTips(err, locale.Tl(
-				"hello_info_suggest_ensure_commit", "Ensure project has commits",
-			))
 		}
-
-		i.out.Print(locale.Tl(
-			"hello_info_extra",
-			"Current commit message: {{.V0}}",
-			commitMsg,
-		))
+		return err
 	}
 
 	return nil
 }
 
+func runOutputProjectMessage(out output.Outputer, pj *project.Project) {
+	projMsg := locale.Tl(
+		"hello_info_warn_no_project", "Not in a project directory.",
+	)
+	if pj != nil {
+		projMsg = locale.Tl(
+			"hello_info_project",
+			"Project: {{.V0}}",
+			pj.Namespace().String(),
+		)
+	}
+
+	out.Print(projMsg)
+}
+
+func runOutputExtra(out output.Outputer, pj *project.Project, params *InfoRunParams) error {
+	if !params.Extra {
+		return nil
+	}
+
+	commitMsg, err := currentCommitMessage(pj)
+	if err != nil {
+		err = locale.WrapError(
+			err, "hello_info_err_get_commit_msg", " Cannot get commit message",
+		)
+		return errs.AddTips(
+			err,
+			locale.Tl("hello_info_suggest_ensure_commit", "Ensure project has commits"),
+		)
+	}
+
+	out.Print(locale.Tl(
+		"hello_info_extra",
+		"Current commit message: {{.V0}}",
+		commitMsg,
+	))
+
+	return nil
+}
+
+// currentCommitMessage contains the scope in which the current commit message
+// is obtained. Since it is a sort of construction function that has some
+// complexity, it is helpful to provide localized error context. Secluding this
+// sort of logic is helpful to keep the subhandlers clean.
 func currentCommitMessage(pj *project.Project) (string, error) {
 	if pj == nil || pj.CommitUUID() == "" {
 		return "", errs.New("Cannot determine which commit UUID to use")
