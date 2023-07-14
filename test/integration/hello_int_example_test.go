556c117228bd68136c2f8b4ac66572f35818caee
daved <daved@codemodus.com>
2023-07-14 16:40:54 -0700

    Update hello e2e

--- test/integration/hello_int_example_test.go
+++ test/integration/hello_int_example_test.go
@@ -18,7 +18,11 @@ func (suite *HelloIntegrationTestSuite) TestHello() {
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
 
-	cp := ts.Spawn("hello")
+	cp := ts.Spawn("checkout", "ActiveState-CLI/small-python", ".")
+	cp.Expect("Checked out project")
+	cp.ExpectExitCode(0)
+
+	cp = ts.Spawn("hello")
 	cp.Expect("Hello, Friend!")
 	cp.ExpectExitCode(0)
 
@@ -31,21 +35,13 @@ func (suite *HelloIntegrationTestSuite) TestHello() {
 	cp.Expect("No name provided")
 	cp.ExpectNotExitCode(0)
 
-	cp = ts.Spawn("checkout", "ActiveState-CLI/small-python", ".")
-	cp.Expect("Checked out project")
-	cp.ExpectExitCode(0)
-
-	cp = ts.Spawn("hello", "info", "--extra")
+	cp = ts.Spawn("hello", "--extra")
 	cp.Expect("Project: ActiveState-CLI/small-python")
 	cp.Expect("Current commit message:")
 	cp.ExpectExitCode(0)
 
-	cp = ts.Spawn("hello", "--namespace=example")
-	cp.Expect("Invalid value")
-	cp.ExpectNotExitCode(0)
-
-	cp = ts.Spawn("hello", "--namespace=org/project")
-	cp.Expect("org/project")
+	cp = ts.Spawn("hello", "--echo", "example")
+	cp.Expect("Echoing: example")
 	cp.ExpectExitCode(0)
 }
 
