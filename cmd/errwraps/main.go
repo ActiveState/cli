69261abaa698b5f22040c310c64ebaf1b6c13f69
daved <daved@codemodus.com>
2023-07-18 18:00:17 -0700

    Finish off formatting and commenting

--- cmd/errwraps/main.go
+++ cmd/errwraps/main.go
@@ -3,47 +3,93 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/ActiveState/cli/internal/errs"
 )
 
-func printChain(err error) {
-	fmt.Println(err)
-	fmt.Println()
+func printChain(indent int, err error) int {
+	ind := strings.Repeat("    ", indent)
+	indent++
+
+	msg := strings.ReplaceAll(err.Error(), "\n", "\\n")
+
+	fmt.Printf("%s%s {\n", ind, msg)
 
 	switch x := err.(type) {
 	case interface{ Unwrap() error }:
-		printChain(x.Unwrap())
+		return printChain(indent, x.Unwrap())
 	case interface{ Unwrap() []error }:
 		for _, err := range x.Unwrap() {
-			printChain(err)
+			_ = printChain(indent, err)
 		}
-	default:
-		return
 	}
+	return indent
 }
 
-func printAll(msg string, err error) {
-	fmt.Println("---", msg, "---")
+func printAll(title, stdlibMsg, ourlibMsg string, err error) {
+	fmt.Println("---", title, "---")
 
-	fmt.Println("stdlib unwrapped...")
-	printChain(err)
-
+	if ourlibMsg != "" {
+		fmt.Println("***", ourlibMsg)
+	}
 	fmt.Println("ourlib joined msg...\n", errs.JoinMessage(err))
+	fmt.Println()
+
+	if stdlibMsg != "" {
+		fmt.Println("***", stdlibMsg)
+	}
+	fmt.Println("stdlib unwrapped...")
+	printChain(0, err)
 	fmt.Println()
 }
 
-func main() {
-	errx := errors.New("xxx")
-	errz := errors.New("zzz")
-	err := errors.Join(errx, errz)
-	err = fmt.Errorf("test1: %w", err)
-	printAll("stdlib error setup (join)", err)
+func stdlibChain(doubleVerb bool) error {
+	unreach := errors.New("Service unreachable")
+	cacheUnav := fmt.Errorf("Local cache unavailable: %w", unreach)
+	notFound := errors.New("HTTP 404")
 
-	err = fmt.Errorf("test2: %w, %w", errx, errz)
-	printAll("stdlib error setup (fmt.Errorf)", err)
+	joined := errors.Join(notFound, cacheUnav)
+	noAuth := fmt.Errorf("No authorization: %w", joined)
 
-	err = errs.Pack(errx, errz)
-	err = errs.Wrap(err, "test1")
-	printAll("ourlib error setup", err)
+	if doubleVerb {
+		noAuth = fmt.Errorf("No authorization: [%w | %w]", notFound, cacheUnav)
+	}
+
+	noAccess := fmt.Errorf("No project access: %w", noAuth)
+	noCheckout := fmt.Errorf("No checkout: %w", noAccess)
+
+	return noCheckout
 }
+
+func ourlibChain() error {
+	unreach := errs.New("Service unreachable")
+	cacheUnav := errs.Wrap(unreach, "Local cache unavailable")
+	notFound := errors.New("HTTP 404")
+
+	joined := errs.Pack(notFound, cacheUnav)
+	noAuth := errs.Wrap(joined, "No authorization")
+
+	noAccess := errs.Wrap(noAuth, "No project access")
+	noCheckout := errs.Wrap(noAccess, "No checkout")
+
+	return noCheckout
+}
+
+func main() {
+	err := stdlibChain(false)
+	printAll("stdlib error setup (errors.Join)", stdlibMsgA, ourlibMsgA, err)
+
+	err = stdlibChain(true)
+	printAll("stdlib error setup (fmt.Errorf 2x %w)", stdlibMsgB, "", err)
+
+	err = ourlibChain()
+	printAll("ourlib error setup", stdlibMsgC, "", err)
+}
+
+var (
+	stdlibMsgA = "In the stdlib error handling practices, the error message is its own message prepended to the wrapped errors messages. We only need to print the output of err.Error()."
+	ourlibMsgA = "In our own error handling practices, the complete error message text is dependent on unwrapping (e.g. errs.JoinMessage). However, this clashes with stdlib expectations."
+	stdlibMsgB = "Notice how joining errors using double %w verbs means that the default newline formatting of errs.Join does not show up. Many people dislike the newline as a default."
+	stdlibMsgC = "Again, we are dependent on unwrapping (i.e. we cannot simply print err.Error()), and errs.Pack creates an additional dependency on custom multierror handling practices where the multierror message itself should be skipped when outputting a complete error chain message."
+)
