39efdf8f14ae8803cb1abcc605f9eab75280017c
mitchell <mitchellb@activestate.com>
2023-08-17 17:06:59 -0400

    Improve readability of fish shell conditional.

--- internal/assets/contents/shells/fishrc_append.fish
+++ internal/assets/contents/shells/fishrc_append.fish
@@ -6,8 +6,7 @@ set -xg {{$K}} "{{$V}}:$PATH"
 set -xg {{$K}} "{{$V}}"
 {{- end}}
 {{- end}}
-if test ! -z "${{.ActivatedEnv}}"
-  and test -f "${{.ActivatedEnv}}/{{.ConfigFile}}"
+if test ! -z "${{.ActivatedEnv}}"; test -f "${{.ActivatedEnv}}/{{.ConfigFile}}"
   echo "State Tool is operating on project ${{.ActivatedNamespaceEnv}}, located at ${{.ActivatedEnv}}"
 end
 # {{.Stop}}
