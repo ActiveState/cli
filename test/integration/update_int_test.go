e1f70cf17417450af169225659570ce9cadb12f3
daved <daved@codemodus.com>
2023-08-16 20:58:20 -0700

    Trial env append change in updateavailable e2e test

--- test/integration/update_int_test.go
+++ test/integration/update_int_test.go
@@ -103,18 +103,7 @@ func (suite *UpdateIntegrationTestSuite) TestUpdateAvailable() {
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
 
-	// Technically state tool automatically starts the state-svc, but the update notification only happens if the svc
-	// happens to already be running and fails silently if not, so in this case we want to ensure the svc is running
-	cp := ts.SpawnCmd(ts.SvcExe, "stop")
-	cp.ExpectExitCode(0)
-
-	cp = ts.SpawnCmdWithOpts(ts.SvcExe, e2e.WithArgs("start"), e2e.AppendEnv(suite.env(false, true)...))
-	cp.ExpectExitCode(0)
-
-	// Give svc time to check for updates and cache the info
-	time.Sleep(2 * time.Second)
-
-	cp = ts.SpawnWithOpts(e2e.WithArgs("--version"))
+	cp := ts.SpawnWithOpts(e2e.WithArgs("--version"), e2e.AppendEnv(suite.env(false, true)...))
 	cp.Expect("Update Available")
 	cp.ExpectExitCode(0)
 }
