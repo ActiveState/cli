137cf1263ed4e3fc21eb8e7a731d2fdd5fb2a00f
mdrakos <mike.drakos@gmail.com>
2023-08-28 16:30:19 -0700

    Remove another expect

--- test/integration/package_int_test.go
+++ test/integration/package_int_test.go
@@ -614,7 +614,6 @@ func (suite *PackageIntegrationTestSuite) TestUpdate() {
 
 	cp = ts.Spawn("install", "pytest@7.3.2") // install
 	cp.ExpectExitCode(0)
-	cp.ExpectLongString("Run state push")
 
 	cp = ts.Spawn("history")
 	cp.Expect("pytest")
