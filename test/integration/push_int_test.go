690c75ebfe9ff4034bb4e5d4f319a865336c22e3
Nathan Rijksen <nathanr@activestate.com>
2023-10-05 10:13:47 -0700

    Add remaining tests

--- test/integration/push_int_test.go
+++ test/integration/push_int_test.go
@@ -379,6 +379,45 @@ func (suite *PushIntegrationTestSuite) TestPush_Aborted() {
 	}
 }
 
+func (suite *PushIntegrationTestSuite) TestPush_InvalidHistory() {
+	suite.OnlyRunForTags(tagsuite.Push)
+
+	ts := e2e.New(suite.T(), true)
+	defer ts.Close()
+
+	// Note the commit we're using here is for another project, in order to repro the error
+	ts.PrepareProject("ActiveState-CLI/small-python", "dbc0415e-91e8-407b-ad36-1de0cc5c0cbb")
+
+	ts.LoginAsPersistentUser()
+	// Target project already exists
+	cp := ts.SpawnWithOpts(e2e.OptArgs("push", "ActiveState-CLI/push-error-test"))
+	cp.Expect("commit history does not match")
+	cp.ExpectExitCode(1)
+
+	if strings.Count(cp.Snapshot(), " x ") != 1 {
+		suite.Fail("Expected exactly ONE error message, got: %s", cp.Snapshot())
+	}
+}
+
+func (suite *PushIntegrationTestSuite) TestPush_PullNeeded() {
+	suite.OnlyRunForTags(tagsuite.Push)
+
+	ts := e2e.New(suite.T(), true)
+	defer ts.Close()
+
+	ts.PrepareProject("ActiveState-CLI/push-error-test", "899c9b4c-d28d-441a-9c28-c84819ba8b1a")
+
+	ts.LoginAsPersistentUser()
+	// Target project already exists
+	cp := ts.SpawnWithOpts(e2e.OptArgs("push"))
+	cp.Expect("changes available that need to be merged")
+	cp.ExpectExitCode(1)
+
+	if strings.Count(cp.Snapshot(), " x ") != 1 {
+		suite.Fail("Expected exactly ONE error message, got: %s", cp.Snapshot())
+	}
+}
+
 func (suite *PushIntegrationTestSuite) TestPush_Outdated() {
 	suite.OnlyRunForTags(tagsuite.Push)
 	projectLine := "project: https://platform.activestate.com/ActiveState-CLI/cli?branch=main"
