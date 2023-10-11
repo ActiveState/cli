88d161c8eaf6578d2b388dd3c26f22b0fe52d748
mdrakos <mike.drakos@gmail.com>
2023-10-11 14:08:31 -0700

    Fix spawn args

--- test/integration/install_scripts_int_test.go
+++ test/integration/install_scripts_int_test.go
@@ -144,10 +144,10 @@ func (suite *InstallScriptsIntegrationTestSuite) TestInstall() {
 			}
 
 			if runtime.GOOS != "windows" {
-				cp = ts.SpawnCmd("env | grep PATH")
+				cp = ts.SpawnCmd("env", " | grep PATH")
 				cp.Expect(installPath)
 			} else {
-				cp = ts.SpawnCmd("echo %PATH%")
+				cp = ts.SpawnCmd("echo", "%PATH%")
 				cp.Expect(installPath)
 			}
 			cp.SendLine(statePath + " --version")
