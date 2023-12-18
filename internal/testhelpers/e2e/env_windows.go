f7bfe0929771d89b859966c4526cacbc047bb838
mdrakos <mike.drakos@gmail.com>
2023-12-18 15:18:10 -0800

    Try appending msys bash

--- internal/testhelpers/e2e/env_windows.go
+++ internal/testhelpers/e2e/env_windows.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	testPath             = `C:\Windows\System32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\PowerShell\7\;`
+	testPath             = `C:\msys64\usr\bin;C:\Windows\System32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\PowerShell\7\;`
 	systemHomeEnvVarName = "USERPROFILE"
 )
 
