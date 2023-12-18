e78d022dd564d45181650dd62119f5fe66820851
mdrakos <mike.drakos@gmail.com>
2023-12-18 10:40:11 -0800

    Revert install scripts changes from previous PR

--- test/integration/install_scripts_int_test.go
+++ test/integration/install_scripts_int_test.go
@@ -66,7 +66,9 @@ func (suite *InstallScriptsIntegrationTestSuite) TestInstall() {
 			suite.Require().NoError(fileutils.WriteFile(script, b))
 
 			// Construct installer command to execute.
+			installDir := filepath.Join(ts.Dirs.Work, "install")
 			argsPlain := []string{script}
+			argsPlain = append(argsPlain, "-t", installDir)
 			if tt.Channel != "" {
 				argsPlain = append(argsPlain, "-b", tt.Channel)
 			}
@@ -116,20 +118,6 @@ func (suite *InstallScriptsIntegrationTestSuite) TestInstall() {
 				cp.Expect("ActiveState")
 			}
 
-			// We get the default install path and use that to directly invoke
-			// the state tool. This is to avoid inadvertently using the state
-			// tool that is already on the PATH.
-			installPath, err := installation.InstallPathForBranch(constants.BranchName)
-			suite.NoError(err)
-
-			binPath := filepath.Join(installPath, "bin")
-
-			if runtime.GOOS != "windows" {
-				cp.SendLine("echo $PATH")
-			} else {
-				cp.SendLine("echo %PATH%")
-			}
-			cp.Expect(binPath)
 			cp.SendLine("state --version")
 			cp.Expect("Version " + constants.Version)
 			cp.Expect("Branch " + constants.BranchName)
@@ -138,12 +126,12 @@ func (suite *InstallScriptsIntegrationTestSuite) TestInstall() {
 
 			cp.ExpectExitCode(0)
 
-			stateExec, err := installation.StateExecFromDir(ts.Dirs.HomeDir)
+			stateExec, err := installation.StateExecFromDir(installDir)
 			suite.NoError(err)
 			suite.FileExists(stateExec)
 
-			suite.assertBinDirContents(binPath)
-			suite.assertCorrectVersion(ts, binPath, tt.Version, tt.Channel)
+			suite.assertBinDirContents(filepath.Join(installDir, "bin"))
+			suite.assertCorrectVersion(ts, installDir, tt.Version, tt.Channel)
 			suite.assertAnalytics(ts)
 			suite.DirExists(ts.Dirs.Config)
 
