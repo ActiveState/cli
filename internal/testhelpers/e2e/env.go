16d4f1445ba1e3b1d88bdd244d74d672a411f78f
mdrakos <mike.drakos@gmail.com>
2023-12-18 08:37:13 -0800

    Try setting more env vars

--- internal/testhelpers/e2e/env.go
+++ internal/testhelpers/e2e/env.go
@@ -45,6 +45,15 @@ func sandboxedTestEnvironment(t *testing.T, dirs *Dirs, updatePath bool, extraEn
 			"ALLUSERSPROFILE=C:\\ProgramData",
 			"ProgramData=C:\\ProgramData",
 			"COMSPEC=C:\\Windows\\System32\\cmd.exe",
+			"PROGRAMFILES=C:\\Program Files",
+			"CommonProgramW6432=C:\\Program Files\\Common Files",
+			`PSModulePath=C:\\Modules\azurerm_2.1.0;C:\\Modules\azure_2.1.0;C:\Users\packer\Documents\WindowsPowerShell\Modules;C:\Program Files\WindowsPowerShell\Modules;C:\Windows\system32\WindowsPowerShell\v1.0\Modules;C:\Program Files\Microsoft SQL Server\130\Tools\PowerShell\Modules\;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\platform\PowerShell`,
+			"SHLVL=1",
+			fmt.Sprintf("HOME=%s", dirs.HomeDir),
+			fmt.Sprintf("HOMEPATH=%s", dirs.HomeDir),
+			`PSModuleAnalysisCachePath=C:\PSModuleAnalysisCachePath\ModuleAnalysisCache`,
+			`WINDIR=C:\Windows`,
+			`PUBLIC=C:\Users\Public`,
 		}
 		env = append(env, windowsEnv...)
 	}
