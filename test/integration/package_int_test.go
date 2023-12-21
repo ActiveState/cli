0b1fa90706370a3b5c88cec1f8d3a4a409ec718e
mdrakos <mike.drakos@gmail.com>
2023-12-21 13:30:43 -0800

    Only enable for Linux

--- test/integration/package_int_test.go
+++ test/integration/package_int_test.go
@@ -596,8 +596,7 @@ func (suite *PackageIntegrationTestSuite) TestUpdate() {
 func (suite *PackageIntegrationTestSuite) TestRuby() {
 	// TODO: Re-enable this test when Ruby is supported on the Platform
 	// https://activestatef.atlassian.net/browse/DX-2384
-	suite.T().Skip("Ruby is currently not building on the Platform")
-	if runtime.GOOS == "darwin" {
+	if runtime.GOOS == "darwin" || runtime.GOOS == "windows" {
 		return // Ruby support is not yet enabled on the Platform
 	}
 	suite.OnlyRunForTags(tagsuite.Package)
