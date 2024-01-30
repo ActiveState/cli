39e961fab9bb0d8c87430aa388551b0a36b9b171
Nathan Rijksen <nathanr@activestate.com>
2024-01-30 14:18:02 -0800

    Implement new migration flow for local orders

---
+++ internal/runbits/legacy/projectmigration/migrate.nix.bash
@@ -0,0 +1,2 @@
+sed -E -i 's/(\?|&)commitID=[a-zA-Z0-9-]*//' "$project.path()/activestate.yaml"
+echo "Project successfully migrated. You can now delete the migrate-to-buildscripts script from your activestate.yaml"
--- test/integration/project_migration_int_test.go
+++ test/integration/project_migration_int_test.go
@@ -1,7 +1,7 @@
 package integration
 
 import (
-	"fmt"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -9,6 +9,7 @@ import (
 	"github.com/ActiveState/cli/internal/fileutils"
 	"github.com/ActiveState/cli/internal/testhelpers/e2e"
 	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
+	"github.com/ActiveState/cli/pkg/projectfile"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -17,12 +18,12 @@ type ProjectMigrationIntegrationTestSuite struct {
 }
 
 func (suite *ProjectMigrationIntegrationTestSuite) TestPromptMigration() {
-	suite.OnlyRunForTags(tagsuite.Critical)
+	suite.OnlyRunForTags(tagsuite.Critical, tagsuite.Migrations)
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
 
-	commitID := "9090c128-e948-4388-8f7f-96e2c1e00d98"
-	ts.PrepareActiveStateYAML(`project: https://platform.activestate.com/ActiveState-CLI/test?commitID=` + commitID)
+	commitID := "c2b3f176-4788-479c-aad3-8359d28ba3ce"
+	ts.PrepareActiveStateYAML(`project: https://platform.activestate.com/ActiveState-CLI/Python3-Alternative?commitID=` + commitID)
 	suite.Require().NoError(fileutils.Mkdir(filepath.Join(ts.Dirs.Work, ".git")), "could not mimic this being a git repo")
 
 	// Verify the user is prompted to migrate an unmigrated project.
@@ -33,21 +34,41 @@ func (suite *ProjectMigrationIntegrationTestSuite) TestPromptMigration() {
 	cp.Expect("migrate")
 	cp.Expect("? (y/N)")
 	cp.SendEnter()
-	cp.Expect("declined")
+	cp.Expect("Understood, you can manually upgrade later")
+	cp.Expect("outdated format") // Warning that project is not fully upgraded
 
 	// Verify that read-only actions still work for unmigrated projects.
-	cp.Expect("pylint")
 	cp.Expect("pytest")
 	cp.ExpectExitCode(0)
 
-	// Verify activestate.yaml remains unchanged and a .activestate/commit was not created, nor was a
-	// .gitignore created.
+	// Verify activestate.yaml remains unchanged
 	bytes := fileutils.ReadFileUnsafe(filepath.Join(ts.Dirs.Work, constants.ConfigFileName))
-	suite.Assert().Contains(string(bytes), commitID, "as.yaml was migrated and does not still contain commitID")
+	suite.Assert().Contains(string(bytes), commitID, "as.yaml was not migrated and still contains commitID")
+
+	// Verify that we have the local commit file, since even when declined we still do a partial migration
 	projectConfigDir := filepath.Join(ts.Dirs.Work, constants.ProjectConfigDirName)
-	suite.Assert().NoDirExists(projectConfigDir, ".activestate dir was created")
+	suite.Assert().DirExists(projectConfigDir, ".activestate dir was not created")
+	localCommitFile := filepath.Join(projectConfigDir, constants.CommitIdFileName)
+	suite.Assert().Equal(commitID, string(fileutils.ReadFileUnsafe(localCommitFile)), "local commit file was not created")
 	gitignoreFile := filepath.Join(ts.Dirs.Work, ".gitignore")
-	suite.Assert().NoFileExists(gitignoreFile, ".gitignore was created")
+	suite.Assert().FileExists(gitignoreFile, ".gitignore was not created")
+
+	// Make a new commit
+	cp = ts.SpawnWithOpts(
+		e2e.OptArgs("install", "hello-world"),
+		e2e.OptAppendEnv(constants.DisableRuntime+"=false"),
+	)
+	cp.ExpectExitCode(0, e2e.RuntimeSourcingTimeoutOpt)
+
+	// Verify both local commit and activestate.yaml are updated
+	newCommitID := string(fileutils.ReadFileUnsafe(localCommitFile))
+	suite.NotEqual(newCommitID, commitID, "Should have new commit ID")
+	pjf, err := projectfile.Parse(filepath.Join(ts.Dirs.Work, constants.ConfigFileName))
+	suite.Require().NoError(err)
+	suite.Equal(newCommitID, pjf.LegacyCommitID(), "commit ID in activestate.yaml was not updated")
+
+	// Delete local commit file so we can try the prompt again
+	suite.Require().NoError(os.Remove(localCommitFile))
 
 	// Verify that migration works.
 	cp = ts.SpawnWithOpts(
@@ -58,25 +79,76 @@ func (suite *ProjectMigrationIntegrationTestSuite) TestPromptMigration() {
 	cp.Expect("? (y/N)")
 	cp.SendLine("Y")
 	cp.Expect("success")
-
-	cp.Expect("pylint")
 	cp.Expect("pytest")
 	cp.ExpectExitCode(0)
 
-	// Verify .activestate/commit and .gitignore were created.
-	suite.Require().True(fileutils.DirExists(projectConfigDir), ",migration should have created "+projectConfigDir)
-	commitIDFile := filepath.Join(projectConfigDir, constants.CommitIdFileName)
-	suite.Assert().True(fileutils.FileExists(commitIDFile), "commit file not created")
-	suite.Assert().Contains(string(fileutils.ReadFileUnsafe(commitIDFile)), commitID, "migration did not populate .activestate/commit")
-	suite.Assert().True(fileutils.FileExists(gitignoreFile), "migration did not create .gitignore")
-	suite.Assert().Contains(string(fileutils.ReadFileUnsafe(gitignoreFile)), fmt.Sprintf("%s/%s", constants.ProjectConfigDirName, constants.CommitIdFileName), "commit file not added to .gitignore")
+	// Verify activestate.yaml no longer has commitID
+	bytes = fileutils.ReadFileUnsafe(filepath.Join(ts.Dirs.Work, constants.ConfigFileName))
+	suite.Assert().NotContains(string(bytes), "&commitID=", "as.yaml was migrated and does not still contain commitID")
+	suite.Assert().NotContains(string(bytes), "migrate-to-buildscripts", "should not have created migration script")
 
 	// Verify no prompt for migrated project.
 	cp = ts.SpawnWithOpts(
 		e2e.OptArgs("packages"),
 		e2e.OptAppendEnv(constants.DisableProjectMigrationPrompt+"=false"),
 	)
-	cp.Expect("pylint")
+	cp.Expect("pytest")
+	cp.ExpectExitCode(0)
+}
+
+func (suite *ProjectMigrationIntegrationTestSuite) TestScriptMigration() {
+	suite.OnlyRunForTags(tagsuite.Critical)
+	ts := e2e.New(suite.T(), false)
+	defer ts.Close()
+
+	commitID := "c2b3f176-4788-479c-aad3-8359d28ba3ce"
+	ts.PrepareActiveStateYAML(`project: https://platform.activestate.com/ActiveState-CLI/Python3-Alternative?commitID=` + commitID)
+	suite.Require().NoError(fileutils.Mkdir(filepath.Join(ts.Dirs.Work, ".git")), "could not mimic this being a git repo")
+
+	// Verify the user is prompted to migrate an unmigrated project.
+	cp := ts.SpawnWithOpts(
+		e2e.OptArgs("packages"),
+		e2e.OptAppendEnv(constants.DisableProjectMigrationPrompt+"=false"),
+	)
+	cp.Expect("migrate")
+	cp.Expect("? (y/N)")
+	cp.SendEnter()
+	cp.Expect("Understood, you can manually upgrade later")
+	cp.Expect("outdated format") // Warning that project is not fully upgraded
+
+	// Verify that read-only actions still work for unmigrated projects.
+	cp.Expect("pytest")
+	cp.ExpectExitCode(0)
+
+	// Verify activestate.yaml remains unchanged
+	bytes := fileutils.ReadFileUnsafe(filepath.Join(ts.Dirs.Work, constants.ConfigFileName))
+	suite.Assert().Contains(string(bytes), commitID, "as.yaml was not migrated and still contains commitID")
+
+	// Verify that we have the local commit file, since even when declined we still do a partial migration
+	projectConfigDir := filepath.Join(ts.Dirs.Work, constants.ProjectConfigDirName)
+	suite.Assert().DirExists(projectConfigDir, ".activestate dir was not created")
+	localCommitFile := filepath.Join(projectConfigDir, constants.CommitIdFileName)
+	suite.Assert().Equal(commitID, string(fileutils.ReadFileUnsafe(localCommitFile)), "local commit file was not created")
+	gitignoreFile := filepath.Join(ts.Dirs.Work, ".gitignore")
+	suite.Assert().FileExists(gitignoreFile, ".gitignore was not created")
+
+	// Verify that script migration works.
+	cp = ts.SpawnWithOpts(
+		e2e.OptArgs("run", "migrate-to-buildscripts"),
+		e2e.OptAppendEnv(constants.DisableProjectMigrationPrompt+"=false"),
+	)
+	cp.Expect("Project successfully migrated")
+	cp.ExpectExitCode(0)
+
+	// Verify activestate.yaml no longer has commitID
+	bytes = fileutils.ReadFileUnsafe(filepath.Join(ts.Dirs.Work, constants.ConfigFileName))
+	suite.Assert().NotContains(string(bytes), "&commitID=", "as.yaml was migrated and does not still contain commitID")
+
+	// Verify no prompt for migrated project.
+	cp = ts.SpawnWithOpts(
+		e2e.OptArgs("packages"),
+		e2e.OptAppendEnv(constants.DisableProjectMigrationPrompt+"=false"),
+	)
 	cp.Expect("pytest")
 	cp.ExpectExitCode(0)
 }
