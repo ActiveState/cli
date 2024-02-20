0a173b735ccb177dfa9458b220268041915b88ba
mitchell <mitchellb@activestate.com>
2024-02-20 14:53:27 -0500

    Revert input error change, but show error message from API.

--- internal/runners/publish/publish.go
+++ internal/runners/publish/publish.go
@@ -247,7 +247,7 @@ func (r *Runner) Run(params *Params) error {
 	result := graphModel.PublishResult{}
 
 	if err := r.client.Run(pr, &result); err != nil {
-		return locale.WrapInputError(err, "err_uploadingredient_publish", "", err.Error())
+		return locale.WrapError(err, "err_uploadingredient_publish", "", err.Error())
 	}
 
 	if result.Publish.Error != "" {
