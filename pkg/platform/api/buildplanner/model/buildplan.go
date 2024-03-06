933509745b8ffb111e83921f2649f9563fccaa65
mdrakos <mike.drakos@gmail.com>
2024-03-06 14:34:55 -0800

    Change JSON key

--- pkg/platform/api/buildplanner/model/buildplan.go
+++ pkg/platform/api/buildplanner/model/buildplan.go
@@ -542,7 +542,7 @@ type buildTarget struct {
 }
 
 type BuildTargetResult struct {
-	Evaluate *buildTarget `json:"evaluate"`
+	Build *buildTarget `json:"build"`
 }
 
 // Error contains an error message.
