5bd511bb75717ac39171b074694580877806065e
Nathan Rijksen <nathanr@activestate.com>
2024-03-08 09:02:16 -0800

    Add back import

--- test/integration/commit_int_test.go
+++ test/integration/commit_int_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/ActiveState/cli/internal/constants"
+	"github.com/ActiveState/cli/internal/errs"
 	"github.com/ActiveState/cli/internal/fileutils"
 	"github.com/ActiveState/cli/internal/testhelpers/e2e"
 	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
