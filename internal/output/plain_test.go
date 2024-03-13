b6472f9a787242b201aa641e19525198a831a123
mdrakos <mike.drakos@gmail.com>
2024-03-13 15:36:45 -0700

    Fix test

--- internal/output/plain_test.go
+++ internal/output/plain_test.go
@@ -78,7 +78,7 @@ func TestPlain_Print(t *testing.T) {
 				"int":    int(1),
 				"float":  float32(9.1),
 			}},
-			" float: 9.10 \n int: 1 \n string: hello \n",
+			"\n float: 9.10 \n int: 1 \n string: hello \n",
 			"",
 		},
 		{
