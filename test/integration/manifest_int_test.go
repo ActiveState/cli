234e1332be927f8191744965b740de70f6884530
mdrakos <mike.drakos@gmail.com>
2024-04-15 14:09:00 -0700

    Add simple integration test

---
+++ test/integration/manifest_int_test.go
@@ -0,0 +1,36 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/ActiveState/cli/internal/testhelpers/e2e"
+	"github.com/ActiveState/cli/internal/testhelpers/suite"
+	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
+)
+
+type ManifestIntegrationTestSuite struct {
+	tagsuite.Suite
+}
+
+func (suite *ManifestIntegrationTestSuite) TestManifest() {
+	suite.OnlyRunForTags(tagsuite.Manifest)
+	ts := e2e.New(suite.T(), false)
+	defer ts.Close()
+
+	ts.LoginAsPersistentUser()
+	ts.PrepareProject("ActiveState/cli", "9eee7512-b2ab-4600-b78b-ab0cf2e817d8")
+
+	cp := ts.Spawn("manifest")
+	cp.Expect("Operating on project: ActiveState/cli")
+	cp.Expect("Name")
+	cp.Expect("python")
+	cp.Expect("3.9.13")
+	cp.Expect("PSF-2.0")
+	cp.Expect("1 Critical,")
+	cp.Expect("pexpect")
+	cp.Expect("None detected")
+}
+
+func TestManifestIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(ManifestIntegrationTestSuite))
+}
