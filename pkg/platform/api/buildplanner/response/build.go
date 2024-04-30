2f8447bcecdd56f8924254a84f243a4b67d92bc3
Nathan Rijksen <nathanr@activestate.com>
2024-04-30 15:27:02 -0700

    Work around silly json lib limitation

--- pkg/platform/api/buildplanner/response/build.go
+++ pkg/platform/api/buildplanner/response/build.go
@@ -20,5 +20,22 @@ type BuildResponse struct {
 	Status    string             `json:"status"`
 	*Error
 	*PlanningError
-	json.RawMessage
+	RawMessage json.RawMessage
+}
+
+// UnmarshalJSON lets us record both the raw json message as well as unmarshal the parts we care about
+// because without this function only the RawMessage itself would be set, the rest of the field would be empty.
+// This is effectively working around a silly json library limitation.
+func (b *BuildResponse) UnmarshalJSON(data []byte) error {
+	type Alias BuildResponse
+	aux := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(b),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	b.RawMessage = data
+	return nil
 }
