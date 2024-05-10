be74093ffe90caa8965e319f278de94cc15a1640
mdrakos <mike.drakos@gmail.com>
2024-05-10 15:03:14 -0700

    Add back runtime sourcing

--- test/integration/package_int_test.go
+++ test/integration/package_int_test.go
@@ -460,6 +460,7 @@ func (suite *PackageIntegrationTestSuite) TestJSON() {
 
 	cp = ts.SpawnWithOpts(
 		e2e.OptArgs("checkout", "ActiveState-CLI/Packages-Perl", "."),
+		e2e.OptAppendEnv("ACTIVESTATE_CLI_DISABLE_RUNTIME=false"),
 	)
 	cp.Expect("Checked out project")
 	cp.ExpectExitCode(0)
