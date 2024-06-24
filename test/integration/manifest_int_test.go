72d3c5d74cfae6f1dca121f9ff54c92d4f8968e4
mitchell <mitchellb@activestate.com>
2024-06-24 14:42:21 -0400

    Refactor manifest integration test to to not re-enable runtimes.
    Also use prepare instead of checkout when possible.

--- test/integration/manifest_int_test.go
+++ test/integration/manifest_int_test.go
@@ -3,7 +3,6 @@ package integration
 import (
 	"testing"
 
-	"github.com/ActiveState/cli/internal/constants"
 	"github.com/ActiveState/cli/internal/testhelpers/e2e"
 	"github.com/ActiveState/cli/internal/testhelpers/suite"
 	"github.com/ActiveState/cli/internal/testhelpers/tagsuite"
@@ -20,17 +19,11 @@ func (suite *ManifestIntegrationTestSuite) TestManifest() {
 
 	ts.LoginAsPersistentUser()
 
-	cp := ts.SpawnWithOpts(
-		e2e.OptArgs("checkout", "ActiveState/cli#9eee7512-b2ab-4600-b78b-ab0cf2e817d8", "."),
-		e2e.OptAppendEnv(constants.DisableRuntime+"=false"),
-	)
+	cp := ts.Spawn("checkout", "ActiveState/cli#9eee7512-b2ab-4600-b78b-ab0cf2e817d8", ".")
 	cp.Expect("Checked out project", e2e.RuntimeSourcingTimeoutOpt)
 
-	cp = ts.SpawnWithOpts(
-		e2e.OptArgs("manifest"),
-		e2e.OptAppendEnv(constants.DisableRuntime+"=false"),
-	)
-	cp.Expect("Operating on project: ActiveState/cli", e2e.RuntimeSourcingTimeoutOpt)
+	cp = ts.Spawn("manifest")
+	cp.Expect("Operating on project: ActiveState/cli")
 	cp.Expect("Name")
 	cp.Expect("python")
 	cp.Expect("3.9.13")
@@ -48,14 +41,10 @@ func (suite *ManifestIntegrationTestSuite) TestManifest_JSON() {
 
 	ts.LoginAsPersistentUser()
 
-	cp := ts.SpawnWithOpts(
-		e2e.OptArgs("checkout", "ActiveState/cli#9eee7512-b2ab-4600-b78b-ab0cf2e817d8", "."),
-	)
+	cp := ts.Spawn("checkout", "ActiveState/cli#9eee7512-b2ab-4600-b78b-ab0cf2e817d8", ".")
 	cp.Expect("Checked out project", e2e.RuntimeSourcingTimeoutOpt)
 
-	cp = ts.SpawnWithOpts(
-		e2e.OptArgs("manifest", "--output", "json"),
-	)
+	cp = ts.Spawn("manifest", "--output", "json")
 	cp.ExpectExitCode(0)
 	AssertValidJSON(suite.T(), cp)
 	cp.Expect(`"requirements":`)
