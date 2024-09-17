067dd4cb9fb3bffcf4660f9dd4d6a0557ccac31d
mitchell <mitchellb@activestate.com>
2024-09-17 10:53:41 -0400

    Remove shell PS1/prompt modifications.

--- internal/assets/contents/shells/bashrc.sh
+++ internal/assets/contents/shells/bashrc.sh
@@ -1,9 +1,5 @@
 if [ -f ~/.bashrc ]; then source ~/.bashrc; fi
 
-{{if and (ne .Owner "") (not .PreservePs1) }}
-export PS1="[{{.Owner}}/{{.Name}}] $PS1"
-{{end}}
-
 cd "{{.WD}}"
 
 {{- range $K, $V := .Env}}
