7a0c29daf63770dd70e89af6840997df42f1fb80
Nathan Rijksen <nathanr@activestate.com>
2024-09-19 09:15:17 -0700

    Fix test; not guaranteed to return django

--- test/integration/install_int_test.go
+++ test/integration/install_int_test.go
@@ -42,7 +42,7 @@ func (suite *InstallIntegrationTestSuite) TestInstallSuggest() {
 	cp = ts.Spawn("install", "djang")
 	cp.Expect("No results found", e2e.RuntimeSolvingTimeoutOpt)
 	cp.Expect("Did you mean")
-	cp.Expect("language/python/django")
+	cp.Expect("language/python/djang")
 	cp.ExpectExitCode(1)
 }
 
