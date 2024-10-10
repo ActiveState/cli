c50b3e3fa9daa377bcc8c8defd8ddc024a8d7cc0
Nathan Rijksen <nathanr@activestate.com>
2024-10-10 15:13:49 -0700

    Turned publish logic into model function for reusability

--- pkg/platform/model/buildplanner/publish.go
+++ pkg/platform/model/buildplanner/publish.go
@@ -1,1 +1,25 @@
 package buildplanner
+
+import (
+	"github.com/ActiveState/cli/internal/errs"
+	graphModel "github.com/ActiveState/cli/pkg/platform/api/graphql/model"
+	"github.com/ActiveState/cli/pkg/platform/api/graphql/request"
+)
+
+func (b *BuildPlanner) Publish(vars request.PublishVariables, filepath string) (*graphModel.PublishResult, error) {
+	pr, err := request.Publish(vars, filepath)
+	if err != nil {
+		return nil, errs.Wrap(err, "Could not create publish request")
+	}
+	res := graphModel.PublishResponse{}
+
+	if err := b.client.Run(pr, &res); err != nil {
+		return nil, errs.Wrap(err, "", err.Error())
+	}
+
+	if res.Result.Error != "" {
+		return nil, errs.New("API responded with error: {{.V0}}", res.Result.Error)
+	}
+
+	return &res.Result, nil
+}
