6fba38b4b4705742f2f1d1f0d143fb980c41e646
mitchell <mitchellb@activestate.com>
2024-10-30 13:03:59 -0400

    Make access denied error an input error.

--- pkg/projectfile/yamlfield.go
+++ pkg/projectfile/yamlfield.go
@@ -59,7 +59,7 @@ func (y *yamlField) Save(path string) error {
 
 	if err := os.WriteFile(path, out, 0664); err != nil {
 		if osutils.IsAccessDeniedError(err) {
-			return locale.WrapError(err, "err_migrate_projectfile_access_denied",
+			return locale.WrapInputError(err, "err_migrate_projectfile_access_denied",
 				"Your project file at '{{.V0}}' is out of date, but State Tool does not have permission to update it. Please make it writeable or re-checkout the project to a writeable location.",
 				path)
 		}
