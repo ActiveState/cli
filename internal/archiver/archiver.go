312771ba5b13548882c9447c21d0c644a41023c7
Nathan Rijksen <nathanr@activestate.com>
2024-10-31 13:28:27 -0700

    Added archiver package

---
+++ internal/archiver/archiver.go
@@ -0,0 +1,67 @@
+package archiver
+
+import (
+	"os"
+	"strings"
+
+	"github.com/ActiveState/cli/internal/errs"
+	"github.com/ActiveState/cli/internal/fileutils"
+	"github.com/mholt/archiver/v3"
+)
+
+type FileMap struct {
+	Source string
+	Target string
+}
+
+func CreateTgz(filepath string, fileMaps []FileMap) error {
+	f, err := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return errs.Wrap(err, "Could not create temp file")
+	}
+	defer f.Close()
+	tgz := archiver.NewTarGz()
+	if err := tgz.Create(f); err != nil {
+		return errs.Wrap(err, "Could not create tar.gz")
+	}
+	defer tgz.Close()
+
+	for _, fileMap := range fileMaps {
+		file, err := os.Open(fileMap.Source)
+		if err != nil {
+			return errs.Wrap(err, "Could not open file")
+		}
+
+		fileInfo, err := file.Stat()
+		if err != nil {
+			return errs.Wrap(err, "Could not stat file")
+		}
+
+		// write it to the archive
+		err = tgz.Write(archiver.File{
+			FileInfo: archiver.FileInfo{
+				FileInfo:   fileInfo,
+				CustomName: fileMap.Target,
+			},
+			ReadCloser: file,
+		})
+		file.Close()
+		if err != nil {
+			return errs.Wrap(err, "Could not write file to tar.gz")
+		}
+	}
+
+	return nil
+}
+
+func FilesWithCommonParent(filepaths ...string) []FileMap {
+	var fileMaps []FileMap
+	common := fileutils.CommonParentPath(filepaths)
+	for _, filepath := range filepaths {
+		fileMaps = append(fileMaps, FileMap{
+			Source: filepath,
+			Target: strings.TrimPrefix(strings.TrimPrefix(filepath, common), "/"),
+		})
+	}
+	return fileMaps
+}
