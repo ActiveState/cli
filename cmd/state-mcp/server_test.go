c11c5c48752a78cef12deedde9a350a61dabe53a
Nathan Rijksen <nathanr@activestate.com>
2025-04-10 11:23:11 -0700

    Disable tests

--- cmd/state-mcp/server_test.go
+++ cmd/state-mcp/server_test.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"testing"
+
+	"github.com/ActiveState/cli/internal/environment"
 )
 
-func TestServer(t *testing.T) {
+func TestServerProjects(t *testing.T) {
+	t.Skip("Intended for manual testing")
 	mcpHandler := registerServer()
 	registerRawTools(mcpHandler)
 	
@@ -17,6 +20,25 @@ func TestServer(t *testing.T) {
 		"params": {
 			"name": "projects",
 			"arguments": {}
+		}
+	}`))
+	t.Fatalf("%+v", msg)
+}
+
+func TestServerPackages(t *testing.T) {
+	t.Skip("Intended for manual testing")
+	mcpHandler := registerServer()
+	registerRawTools(mcpHandler)
+	
+	msg := mcpHandler.mcpServer.HandleMessage(context.Background(), json.RawMessage(`{
+		"jsonrpc": "2.0",
+		"id": 1,
+		"method": "tools/call",
+		"params": {
+			"name": "packages",
+			"arguments": {
+				"project_directory": "`+environment.GetRootPathUnsafe()+`"
+			}
 		}
 	}`))
 	t.Fatalf("%+v", msg)
