33ddc91808e4a264a9aff6a6dc57a44170d44f74
Nathan Rijksen <nathanr@activestate.com>
2025-08-11 14:43:32 -0700

    Simplify assertion, avoiding encoding issues

--- test/integration/errors_int_test.go
+++ test/integration/errors_int_test.go
@@ -17,35 +17,35 @@ func (suite *ErrorsIntegrationTestSuite) TestTips() {
 	suite.OnlyRunForTags(tagsuite.Errors, tagsuite.Critical)
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
+	ts.IgnoreLogErrors()
 
 	cp := ts.Spawn("__test", "multierror")
 	cp.Expect("Need More Help?")
 	cp.Expect("Run →")
 	cp.Expect("Ask For Help →")
 	cp.ExpectExitCode(1)
-	ts.IgnoreLogErrors()
 }
 
 func (suite *ErrorsIntegrationTestSuite) TestMultiErrorWithInput() {
 	suite.OnlyRunForTags(tagsuite.Errors, tagsuite.Critical)
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
+	ts.IgnoreLogErrors()
 
 	cp := ts.Spawn("__test", "multierror-input")
-	cp.ExpectRe(`\s+x error1.\s+\s+x error2.\s+x error3.\s+x error4.\s+█\s+Need More Help`)
+	cp.ExpectRe(`\s+x error1.\s+\s+x error2.\s+x error3.\s+x error4.\s+`)
 	cp.ExpectExitCode(1)
-	ts.IgnoreLogErrors()
 }
 
 func (suite *ErrorsIntegrationTestSuite) TestMultiErrorWithoutInput() {
 	suite.OnlyRunForTags(tagsuite.Errors, tagsuite.Critical)
 	ts := e2e.New(suite.T(), false)
 	defer ts.Close()
+	ts.IgnoreLogErrors()
 
 	cp := ts.Spawn("__test", "multierror-noinput")
-	cp.ExpectRe(`\s+x error1.\s+\s+x error2.\s+x error3.\s+x error4.\s+█\s+Need More Help`)
+	cp.ExpectRe(`\s+x error1.\s+\s+x error2.\s+x error3.\s+x error4.\s+`)
 	cp.ExpectExitCode(1)
-	ts.IgnoreLogErrors()
 }
 
 func TestErrorsIntegrationTestSuite(t *testing.T) {
